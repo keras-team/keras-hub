a0607d0f9b7c08ddcf81459868b33761d8ed5bb2
Chen Qian <chenmoney@google.com>
2021-12-02 14:08:43 -0800

    Set up the dependency

---
+++ setup.py
@@ -0,0 +1,43 @@
+# Copyright 2021 The KerasNLP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Setup script."""
+
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name="keras-nlp",
+    description="High-level NLP libraries based on Keras",
+    url="https://github.com/keras-team/keras-nlp",
+    author="Keras team",
+    author_email="keras-nlp@google.com",
+    license="Apache License 2.0",
+    # tensorflow isn't a dependency because it would force the
+    # download of the gpu version or the cpu version.
+    # users should install it manually.
+    install_requires=["packaging", "tensorflow", "numpy"],
+    extras_require={"tests": ["flake8", "isort", "black",],},
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: Unix",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Software Development",
+    ],
+    packages=find_packages(exclude=("tests",)),
+)
