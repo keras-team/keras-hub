81fa8fa3aa007194ea83300fdca943a3e5f17814
Aflah <72096386+aflah02@users.noreply.github.com>
2022-04-06 07:56:13 +0530

    Testing

--- keras_nlp/tokenizers/word_piece_tokenizer.py
+++ keras_nlp/tokenizers/word_piece_tokenizer.py
@@ -256,7 +256,11 @@ class WordPieceTokenizer(tokenizer.Tokenizer):
         return config
 
     def tokenize(self, inputs):
-        scalar_input = tf.tensor(inputs).shape.rank == 0
+        """Check if inputs is a tensor"""
+        if isinstance(inputs, tf.Tensor):
+            scalar_input = tf.convert_to_tensor(inputs).shape.rank == 0
+        else:
+            scalar_input = inputs.shape.rank == 0
         if scalar_input:
             inputs = tf.expand_dims(inputs, 0)
         # Optionally normalize and split inputs.
