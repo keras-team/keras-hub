2a7a15c60d8ce40f6c630b3284a78b9fedb46213
Aflah <72096386+aflah02@users.noreply.github.com>
2022-04-07 13:02:06 +0530

    Removed Debug Statements

--- keras_nlp/tokenizers/word_piece_tokenizer.py
+++ keras_nlp/tokenizers/word_piece_tokenizer.py
@@ -259,12 +259,9 @@ class WordPieceTokenizer(tokenizer.Tokenizer):
         """Check if inputs is a tensor"""
         if not isinstance(inputs, tf.Tensor) or not isinstance(inputs, 
         tf.RaggedTensor):
-            print("here")
             scalar_input = tf.convert_to_tensor(inputs).shape.rank == 0
         else:
-            print("here2")
             scalar_input = inputs.shape.rank == 0
-        print(scalar_input)
         if scalar_input:
             inputs = tf.expand_dims(inputs, 0)
         # Optionally normalize and split inputs.
