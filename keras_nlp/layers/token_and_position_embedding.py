b1eec9f6fb0252999b49f4237f5396fab5ec73b3
Anurag Dhadse <anudhadse66@gmail.com>
2022-04-08 16:22:16 +0530

    Added ragged tensor test, and model save test for TokenAndPositionEmbedding and bug fixes

--- keras_nlp/layers/token_and_position_embedding.py
+++ keras_nlp/layers/token_and_position_embedding.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Layer that combines an Embedding Layer and Keras-NLP Positional Embedding Layer"""
+"""Creates an Embedding Layer and adds Positional Embeddings"""
 
 from tensorflow import keras
 
@@ -20,8 +20,7 @@ import keras_nlp.layers
 
 
 class TokenAndPositionEmbedding(keras.layers.Layer):
-    """Creates a layer which creates Embedding for tokens and adds Position Embedding
-    to the input sequence
+    """A layer which sums a token and position embedding.
 
     This class assumes that in the input tensor, the last dimension corresponds
     to the features, and the dimension before the last corresponds to the
@@ -32,21 +31,27 @@ class TokenAndPositionEmbedding(keras.layers.Layer):
         `max_length`: The maximum length of input sequence
         `embedding_dim`: The output dimension of the embedding layer
         `embeddings_initializer`: The initializer to use for the Embedding Layer
-        `embeddings_regularizer`: The regularizer to user for regularization of the Embedding Layer
-        `mask_zero`: Boolean, whether or not the input value 0 is a special "padding" value that should be masked out.
-            This is useful when using recurrent layers which may take variable length input. If this is True, then all
-            subsequent layers in the model need to support masking or an exception will be raised. If mask_zero is set
-            to True, as a consequence, index 0 cannot be used in the vocabulary
+        `position_embeddings_initializer`: The initializer to use for Position
+            Embedding Layer
+        `embeddings_regularizer`: The regularizer to user for regularization of the
+            Embedding Layer
+        `mask_zero`: Boolean, whether or not the input value 0 is a special "padding"
+            value that should be masked out.
+            This is useful when using recurrent layers which may take variable
+            length input. If this is True, then all subsequent layers in the
+            model need to support masking or an exception will be raised. If mask_zero
+            is set to True, as a consequence, index 0 cannot be used in the vocabulary
             (input_dim should equal size of vocabulary + 1).
+        `seq_axis`: The axis of the input tensor where we add the embeddings
 
     Example:
     ```python
-    token_and_position_embed = keras_nlp.layers.TokenAndPositionEmbedding(
+    embedding_layer = keras_nlp.layers.TokenAndPositionEmbedding(
         vocabulary_size=vocab_size,
         max_length=max_length,
         embedding_dim=embed_dim,
     )
-    outputs = token_and_position_embed(inputs)
+    outputs = embedding_layer(inputs)
     ```
     """
 
@@ -56,31 +61,38 @@ class TokenAndPositionEmbedding(keras.layers.Layer):
         max_length,
         embedding_dim,
         embeddings_initializer="glorot_uniform",
+        position_embeddings_initializer="glorot_uniform",
         embeddings_regularizer=None,
         mask_zero=False,
         **kwargs
     ):
-        super().__init__()
+        super().__init__(**kwargs)
         if vocabulary_size is None:
             raise ValueError(
-                "`vocabulary_size` must be an Integer, not `None`."
+                "`vocabulary_size` must be an Integer, received `None`."
             )
         if max_length is None:
-            raise ValueError("`max_length` must be an Integer, not `None`.")
+            raise ValueError(
+                "`max_length` must be an Integer, received `None`."
+            )
         if embedding_dim is None:
-            raise ValueError("`embedding_dim` must be an Integer, not `None`.")
+            raise ValueError(
+                "`embedding_dim` must be an Integer, received `None`."
+            )
         self.vocabulary_size = int(vocabulary_size)
         self.max_length = int(max_length)
         self.embedding_dim = int(embedding_dim)
-        self.token_embeddings = keras.layers.Embedding(
+        self.token_embedding = keras.layers.Embedding(
             vocabulary_size,
             embedding_dim,
             embeddings_initializer=embeddings_initializer,
             embeddings_regularizer=embeddings_regularizer,
             mask_zero=mask_zero,
         )
-        self.position_embeddings = keras_nlp.layers.PositionEmbedding(
+        self.position_embedding = keras_nlp.layers.PositionEmbedding(
             max_length=max_length,
+            initializer=position_embeddings_initializer,
+            **kwargs
         )
 
     def get_config(self):
@@ -90,17 +102,25 @@ class TokenAndPositionEmbedding(keras.layers.Layer):
                 "vocabulary_size": self.vocabulary_size,
                 "max_length": self.max_length,
                 "embedding_dim": self.embedding_dim,
+                "embeddings_initializer": keras.initializers.serialize(
+                    self.token_embedding.embeddings_initializer
+                ),
+                "embeddings_regularizer": keras.regularizers.serialize(
+                    self.token_embedding.embeddings_regularizer
+                ),
+                "position_embeddings_initializer": keras.initializers.serialize(
+                    self.position_embedding.initializer
+                ),
+                "mask_zero": self.token_embedding.mask_zero,
             },
-            **self.token_embeddings.get_config()
         )
-        del config["name"]
         return config
 
     def build(self, input_shape):
         super().build(input_shape)
 
     def call(self, inputs):
-        embedded_tokens = self.token_embeddings(inputs)
-        embedded_positions = self.position_embeddings(embedded_tokens)
+        embedded_tokens = self.token_embedding(inputs)
+        embedded_positions = self.position_embedding(embedded_tokens)
         outputs = embedded_tokens + embedded_positions
         return outputs
--- keras_nlp/layers/token_and_position_embedding_test.py
+++ keras_nlp/layers/token_and_position_embedding_test.py
@@ -13,10 +13,26 @@
 # limitations under the License.
 """Tests for TokenAndPositionEmbedding"""
 
+import os
+
 import tensorflow as tf
 from tensorflow import keras
 
 from keras_nlp.layers import TokenAndPositionEmbedding
+
+
+def custom_position_embed_init(shape, dtype=None):
+    count = 1
+    for length in shape:
+        count *= length
+    return tf.reshape(tf.range(count, dtype=dtype), shape)
+
+
+def custom_token_embed_init(shape, dtype=None):
+    count = 1
+    for length in shape:
+        count *= length
+    return tf.ones(shape, dtype=dtype)
 
 
 class TokenAndPositionEmbeddingTest(tf.test.TestCase):
@@ -33,6 +49,9 @@ class TokenAndPositionEmbeddingTest(tf.test.TestCase):
             "embeddings_initializer": keras.initializers.serialize(
                 keras.initializers.GlorotUniform()
             ),
+            "position_embeddings_initializer": keras.initializers.serialize(
+                keras.initializers.GlorotUniform()
+            ),
             "embeddings_regularizer": None,
             "mask_zero": False,
         }
@@ -47,3 +66,76 @@ class TokenAndPositionEmbeddingTest(tf.test.TestCase):
             restored_token_and_position_embed.get_config(),
             {**config, **expected_config_subset},
         )
+
+    def test_ragged_tensor_with_3_dimensions(self):
+        vocabulary_size = 5
+        sequence_length = 4
+        feature_size = 2
+        embedding_dim = 3
+        test_layer = TokenAndPositionEmbedding(
+            vocabulary_size=vocabulary_size,
+            max_length=sequence_length,
+            embedding_dim=embedding_dim,
+            embeddings_initializer=custom_token_embed_init,
+            position_embeddings_initializer=custom_position_embed_init,
+        )
+        # Create a 3-dimensional ragged input (the first dimension is implicit).
+        input_tensor = tf.keras.Input(
+            shape=(None, feature_size), dtype=tf.float32, ragged=True
+        )
+        output_tensor = test_layer(input_tensor)
+        model = tf.keras.Model(input_tensor, output_tensor)
+
+        input_data = tf.ragged.constant(
+            [
+                [[1.0, 1.0], [1.0, 1.0]],
+                [],
+                [[1.0, 1.0], [1.0, 1.0], [1.0, 1.0]],
+                [[1.0, 1.0]],
+            ],
+            ragged_rank=1,
+            inner_shape=(2,),
+        )
+        expected_output_data = tf.ragged.constant(
+            [
+                [
+                    [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]],
+                    [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]],
+                ],
+                [],
+                [
+                    [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]],
+                    [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]],
+                    [[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]],
+                ],
+                [[[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]]],
+            ],
+            ragged_rank=1,
+        )
+        output_data = model.predict(input_data)
+        self.assertAllClose(output_data, expected_output_data)
+
+    def test_save_model(self):
+        vocabulary_size = 5
+        sequence_length = 4
+        feature_size = 2
+        embedding_dim = 3
+        test_layer = TokenAndPositionEmbedding(
+            vocabulary_size=vocabulary_size,
+            max_length=sequence_length,
+            embedding_dim=embedding_dim,
+        )
+        inputs = keras.Input(shape=(sequence_length, feature_size))
+        outputs = test_layer(inputs)
+        model = keras.Model(inputs=inputs, outputs=outputs)
+
+        data = tf.zeros(shape=[2, sequence_length, feature_size])
+        model(data)
+
+        path = os.path.join(self.get_temp_dir(), "model")
+        model.save(path)
+        loaded_model = keras.models.load_model(path)
+
+        model_output = model.predict(data)
+        loaded_model_output = loaded_model.predict(data)
+        self.assertAllClose(model_output, loaded_model_output)
