2aff863e20c2d5edb5ec3bf00c0bcf6f06f50b19
Anurag Dhadse <anudhadse66@gmail.com>
2022-04-09 16:57:23 +0530

    Removed venv folder from lint.sh and format.sh

--- shell/format.sh
+++ shell/format.sh
@@ -2,14 +2,14 @@
 
 base_dir=$(dirname $(dirname $0))
 
-isort --sl ${base_dir}
-black --line-length 80 ${base_dir}
+isort --sl ${base_dir} --skip venv
+black --line-length 80 ${base_dir} --exclude venv
 
-for i in $(find ${base_dir} -name '*.py'); do
+for i in $(find ${base_dir} -name '*.py' -not -path "*/venv/*"); do
   if ! grep -q Copyright $i; then
     echo $i
     cat shell/copyright.txt $i >$i.new && mv $i.new $i
   fi
 done
 
-flake8 ${base_dir}
+flake8 ${base_dir} --exclude venv
