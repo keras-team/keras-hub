feebbd092cdb8afdb92c4bae76197aeea138ceeb
Abheesht <sharmabhee@gmail.com>
2022-04-13 06:46:33 +0530

    Add Perplexity Metric (#68)
    * Add class for perplexity
    
    * Fix doc-string example
    
    * Add shape checks
    
    * Small typo
    
    * Small typo - 2
    
    * Remove shape and rank checks - they don't work
    
    * Add UTs
    
    * Remove reshape ops
    
    * Address review comments - I
    
    * Add UT for no masking case
    
    * Minor change
    
    * Add a check for dtype
    
    * Format code
    
    * Address review comments - II, III
    
    * Fix formatting issue
    
    * Fix UTs (Attempt I)
    
    * Fix UTs (Attempt II)
    
    * Fix UTs (Attempt III)
    
    * Remove UTs for private members
    
    * Remove checks for private members in UTs
    
    * Address review comments - IV
    
    * Change sample_weight, mask functionality
    
    * Format + Lint

--- keras_nlp/metrics/__init__.py
+++ keras_nlp/metrics/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2021 The KerasNLP Authors
+# Copyright 2022 The KerasNLP Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,8 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from keras_nlp import layers
-from keras_nlp import metrics
-from keras_nlp import tokenizers
-
-__version__ = "0.1.1"
+from keras_nlp.metrics.perplexity import Perplexity
