f41ec829966df63abf3562fcb635d632002958a4
Anurag Dhadse <anudhadse66@gmail.com>
2022-04-22 01:27:15 +0530

    Whitelisted formatting and lint check targets (#126)
    * Fixed lint.sh and format.sh
    
    * Remove print
    
    Co-authored-by: Matt Watson <1389937+mattdangerw@users.noreply.github.com>

--- shell/lint.sh
+++ shell/lint.sh
@@ -1,23 +1,26 @@
 #!/bin/bash -e
 
 base_dir=$(dirname $(dirname $0))
+targets="${base_dir}/*.py ${base_dir}/examples/ ${base_dir}/keras_nlp/"
 
-isort --sl -c "${base_dir}"
+isort --sp "${base_dir}/setup.cfg" --sl -c ${targets}
 if ! [ $? -eq 0 ]; then
   echo "Please run \"./shell/format.sh\" to format the code."
   exit 1
 fi
-flake8 "${base_dir}"
+
+flake8 --config "${base_dir}/setup.cfg" ${targets}
 if ! [ $? -eq 0 ]; then
   echo "Please fix the code style issue."
   exit 1
 fi
-black --check --line-length 80 "${base_dir}"
+
+black --check --line-length 80 ${targets}
 if ! [ $? -eq 0 ]; then
   echo "Please run \"./shell/format.sh\" to format the code."
     exit 1
 fi
-for i in $(find "${base_dir}" -name '*.py'); do
+for i in $(find ${targets} -name '*.py'); do
   if ! grep -q Copyright $i; then
     echo "Please run \"./shell/format.sh\" to format the code."
     exit 1
