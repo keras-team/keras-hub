7baff244dc4e4209dda0ec930b4c2683e2fb55e8
Matt Watson <mattdangerw@gmail.com>
2022-04-27 22:14:24 -0700

    Don't accept a string dtype for unicode tokenizer
    This tokenizer cannot output strings.

--- keras_nlp/tokenizers/unicode_character_tokenizer.py
+++ keras_nlp/tokenizers/unicode_character_tokenizer.py
@@ -175,9 +175,9 @@ class UnicodeCharacterTokenizer(tokenizer.Tokenizer):
             kwargs["dtype"] = tf.int32
         else:
             dtype = tf.dtypes.as_dtype(kwargs["dtype"])
-            if not dtype.is_integer and dtype != tf.string:
+            if not dtype.is_integer:
                 raise ValueError(
-                    "Output dtype must be an integer type of a string. "
+                    "Output dtype must be an integer type. "
                     f"Received: dtype={dtype}"
                 )
 
