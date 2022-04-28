cbee8af32c0e87b2645ac79a8842f93951c34d3b
Matt Watson <mattdangerw@gmail.com>
2022-04-28 10:45:46 -0700

    cast the output to the layer dtype

--- keras_nlp/tokenizers/unicode_character_tokenizer.py
+++ keras_nlp/tokenizers/unicode_character_tokenizer.py
@@ -251,6 +251,7 @@ class UnicodeCharacterTokenizer(tokenizer.Tokenizer):
             replacement_char=self.replacement_char,
             input_encoding=self.input_encoding,
         )
+        tokens = tf.cast(tokens, self.compute_dtype)
 
         if self.sequence_length:
             output_shape = tokens.shape.as_list()
