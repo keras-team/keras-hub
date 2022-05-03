91c9e617f120a6113e359c444ba9fa4426dae28c
Aflah <72096386+aflah02@users.noreply.github.com>
2022-05-04 00:24:44 +0530

    Fixed Import Errors and Added __init__ file

--- keras_nlp/utils/__init__.py
+++ keras_nlp/utils/__init__.py
@@ -11,5 +11,3 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from keras_nlp.utils.tensor_utils import tensor_to_string_list
