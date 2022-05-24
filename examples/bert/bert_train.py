d80b8364ddb2c802620a439aa7d688d6a212ed90
Chen Qian <qianchen94era@gmail.com>
2022-05-24 16:04:42 -0700

    rebase

--- examples/bert/bert_train.py
+++ examples/bert/bert_train.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
 import os
 import shutil
 import sys
@@ -446,6 +445,7 @@ def main(_):
     pretraining_model.compile(
         optimizer=optimizer,
     )
+
     # TODO(mattdangerw): Add TPU strategy support.
     pretraining_model.fit(
         dataset,
