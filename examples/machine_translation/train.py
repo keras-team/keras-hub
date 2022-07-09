d382c45b6a17d595038e0a27addb6aba1f130d2a
Jonathan Bischof <bischof@google.com>
2022-07-09 00:25:20 +0000

    Remove unneeded import

--- examples/machine_translation/train.py
+++ examples/machine_translation/train.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import tensorflow as tf
 from absl import app
 from absl import flags
 from tensorflow import keras
