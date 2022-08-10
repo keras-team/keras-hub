1d8c7e46eec552362b3e59e9a8e9640cb39e25a1
jessechancy <jessec1@andrew.cmu.edu>
2022-08-10 13:57:08 -0700

    style fixes

--- keras_nlp/tokenizers/word_piece_tokenizer_trainer_test.py
+++ keras_nlp/tokenizers/word_piece_tokenizer_trainer_test.py
@@ -145,11 +145,11 @@ class WordPieceTokenizerTrainerTest(tf.test.TestCase):
         test_output = ["a", "b", "c", "m", "s", "##aa", "##a", "##b"]
 
         compute_word_piece_vocabulary(
-            test_text, 
-            8, 
-            vocabulary_output_file="test.txt", 
+            test_text,
+            8,
+            vocabulary_output_file="test.txt",
             lowercase=True,
-            reserved_tokens=[]
+            reserved_tokens=[],
         )
         vocab_from_file = []
         with open("test.txt", "r") as f:
