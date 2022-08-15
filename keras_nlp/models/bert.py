7b34f1a9717efc4ef71d37dc27c3f958f708e156
Jonathan Bischof <bischof@google.com>
2022-08-15 21:41:48 +0000

    Remove unnecessary comment

--- keras_nlp/models/bert.py
+++ keras_nlp/models/bert.py
@@ -11,7 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Bert model and layer implementations."""
 
 import tensorflow as tf
 from absl import logging
