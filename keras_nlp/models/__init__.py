fb812afb4879815d3833b64d10f42ad99b5ef48d
Jonathan Bischof <bischof@google.com>
2022-08-25 18:38:26 +0000

    Rename `models.Bert()` to `models.BertCustom()` Fixes #309

--- keras_nlp/models/__init__.py
+++ keras_nlp/models/__init__.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from keras_nlp.models.bert import Bert
 from keras_nlp.models.bert import BertBase
 from keras_nlp.models.bert import BertClassifier
+from keras_nlp.models.bert import BertCustom
