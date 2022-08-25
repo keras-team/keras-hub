7aff3a1f2dab266002500026997bec18dc1841d9
Jonathan Bischof <bischof@google.com>
2022-08-25 00:44:45 +0000

    Add black formatting for `network_tests`

--- network_tests/test_load_ckpts.py
+++ network_tests/test_load_ckpts.py
@@ -1,4 +1,3 @@
-
 # Copyright 2022 The KerasNLP Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,7 +14,9 @@
 """Tests for loading pretrained model checkpoints."""
 
 import tensorflow as tf
+
 import keras_nlp
+
 
 class BertCkptTest(tf.test.TestCase):
     def test_load_bert_base_uncased(self):
