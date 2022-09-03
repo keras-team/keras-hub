66dd054f1076b4add184b21ab8b57500ece709b8
abheesht17 <sharmabhee@gmail.com>
2022-09-03 07:47:08 +0530

    Retrigger tests

--- keras_nlp/models/bert.py
+++ keras_nlp/models/bert.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Bert model configurable class, preconfigured versions, and task heads."""
+"""BERT model configurable class, preconfigured versions, and task heads."""
 
 import tensorflow as tf
 from tensorflow import keras
