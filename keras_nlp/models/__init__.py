eb6225f16bae020cadca8e99b7b53ff8f7706642
abheesht17 <sharmabhee@gmail.com>
2022-09-03 07:52:15 +0530

    Minor fix

--- keras_nlp/models/__init__.py
+++ keras_nlp/models/__init__.py
@@ -16,7 +16,6 @@ from keras_nlp.models.bert import BertBase
 from keras_nlp.models.bert import BertClassifier
 from keras_nlp.models.bert import BertCustom
 from keras_nlp.models.bert import BertMedium
-from keras_nlp.models.bert import BertMini
 from keras_nlp.models.bert import BertSmall
 from keras_nlp.models.bert import BertTiny
 from keras_nlp.models.roberta import RobertaBase
