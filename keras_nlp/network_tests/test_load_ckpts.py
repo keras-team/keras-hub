a6fe520a3ef2c5db7366588947ad5bc153711599
abheesht17 <sharmabhee@gmail.com>
2022-09-30 00:14:34 +0530

    Fix tests

--- keras_nlp/network_tests/test_load_ckpts.py
+++ keras_nlp/network_tests/test_load_ckpts.py
@@ -15,14 +15,14 @@
 
 import pytest
 import tensorflow as tf
-from absl import parameterized
+from absl.testing import parameterized
 
 from keras_nlp.models.bert import bert_checkpoints
 from keras_nlp.models.bert import bert_models
 from keras_nlp.models.bert import bert_preprocessing
 from keras_nlp.models.bert import bert_tasks
-from keras_nlp.models.gpt2 import checkpoints as gpt2_checkpoints
-from keras_nlp.models.gpt2 import model_classes as gpt2_models
+from keras_nlp.models.gpt2 import gpt2_checkpoints
+from keras_nlp.models.gpt2 import gpt2_models
 
 
 @pytest.mark.slow
@@ -86,7 +86,6 @@ class BertCkptTest(tf.test.TestCase, parameterized.TestCase):
         )
         tokenizer("The quick brown fox.")
 
-
 @pytest.mark.slow
 class Gpt2CkptTest(tf.test.TestCase, parameterized.TestCase):
     @parameterized.named_parameters(
@@ -94,20 +93,18 @@ class Gpt2CkptTest(tf.test.TestCase, parameterized.TestCase):
             checkpoint,
             checkpoint,
             gpt2_models.model_class_by_name(
-                gpt2_checkpoints[checkpoint]["model"]
+                gpt2_checkpoints.checkpoints[checkpoint]["model"]
             ),
         )
         for checkpoint in gpt2_checkpoints.checkpoints
     )
-    def test_load(self, checkpoint, gpt2_class):
+    def test_load_gpt2(self, checkpoint, gpt2_class):
         model = gpt2_class(weights=checkpoint)
         input_data = {
             "token_ids": tf.random.uniform(
-                shape=(1, 1024),
-                dtype=tf.int64,
-                maxval=model.vocabulary_size,
+                shape=(1, 512), dtype=tf.int64, maxval=model.vocabulary_size
             ),
-            "padding_mask": tf.constant([1] * 1024, shape=(1, 1024)),
+            "padding_mask": tf.constant([1] * 512, shape=(1, 512)),
         }
         model(input_data)
 
@@ -115,7 +112,7 @@ class Gpt2CkptTest(tf.test.TestCase, parameterized.TestCase):
         (model_class, model_class)
         for model_class in set(
             [
-                gpt2_checkpoints[checkpoint]["model"]
+                gpt2_checkpoints.checkpoints[checkpoint]["model"]
                 for checkpoint in gpt2_checkpoints.checkpoints
             ]
         )
