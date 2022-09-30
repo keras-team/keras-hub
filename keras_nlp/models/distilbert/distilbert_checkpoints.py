95876535961359873fcd413731eb85f16564595e
abheesht17 <sharmabhee@gmail.com>
2022-09-30 23:41:24 +0530

    Remove ckpts and vocabs

--- keras_nlp/models/distilbert/distilbert_checkpoints.py
+++ keras_nlp/models/distilbert/distilbert_checkpoints.py
@@ -14,74 +14,13 @@
 
 from collections import defaultdict
 
-checkpoints = {
-    "distilbert_base_uncased_en": {
-        "model": "DistilBertBase",
-        "vocabulary": "uncased_en",
-        "description": (
-            "Base size of DistilBERT where all input is lowercased. "
-            "Trained on English Wikipedia + BooksCorpus."
-        ),
-        "weights_url": "https://storage.googleapis.com/keras-nlp/models/distilbert_base_uncased_en/model.h5",
-        "weights_hash": "6625a649572e74086d74c46b8d0b0da3",
-    },
-    "distilbert_base_cased_en": {
-        "model": "DistilBertBase",
-        "vocabulary": "cased_en",
-        "description": (
-            "Base size of DistilBERT where case is maintained. "
-            "Trained on English Wikipedia + BooksCorpus."
-        ),
-        "weights_url": "https://storage.googleapis.com/keras-nlp/models/distilbert_base_cased_en/model.h5",
-        "weights_hash": "fa36aa6865978efbf85a5c8264e5eb57",
-    },
-    "distilbert_base_multi_cased": {
-        "model": "DistilBertBase",
-        "vocabulary": "multi_cased",
-        "description": (
-            "Base size of DistilBERT. Trained on Wikipedias of 104 languages."
-        ),
-        "weights_url": "https://storage.googleapis.com/keras-nlp/models/distilbert_base_multi_cased/model.h5",
-        "weights_hash": "c0f11095e2a6455bd3b1a6d14800a7fa",
-    },
-}
+checkpoints = {}
 
 
 # Metadata for loading pretrained tokenizer vocabularies.
 # TODO: Vocabularies of DistilBERT are the same as BERT's, including the
 # hash. We can have a common folder for both BERT and DistilBERT vocabularies.
-vocabularies = {
-    "uncased_en": {
-        "description": (
-            "The vocabulary for DistilBERT models trained on "
-            "English Wikipedia + BooksCorpus where case is discarded."
-        ),
-        "vocabulary_url": "https://storage.googleapis.com/keras-nlp/models/distilbert_base_uncased_en/vocab.txt",
-        "vocabulary_hash": "64800d5d8528ce344256daf115d4965e",
-        "vocabulary_size": 30522,
-        "lowercase": True,
-    },
-    "cased_en": {
-        "description": (
-            "The vocabulary for DistilBERT models trained on "
-            "English Wikipedia + BooksCorpus where case is maintained."
-        ),
-        "vocabulary_url": "https://storage.googleapis.com/keras-nlp/models/distilbert_base_cased_en/vocab.txt",
-        "vocabulary_hash": "bb6ca9b42e790e5cd986bbb16444d0e0",
-        "vocabulary_size": 28996,
-        "lowercase": False,
-    },
-    "multi_cased": {
-        "description": (
-            "The vocabulary for DistilBERT models trained on Wikipedias "
-            "of 104 languages."
-        ),
-        "vocabulary_url": "https://storage.googleapis.com/keras-nlp/models/distilbert_base_multi_cased/vocab.txt",
-        "vocabulary_hash": "d9d865138d17f1958502ed060ecfeeb6",
-        "vocabulary_size": 119547,
-        "lowercase": False,
-    },
-}
+vocabularies = {}
 
 # Index checkpoints by arch compatibility and create lookup function
 checkpoints_per_arch = defaultdict(set)
