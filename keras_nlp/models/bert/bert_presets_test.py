4adda4125f1aa7331b5115b17d72225f54a23499
Matt Watson <1389937+mattdangerw@users.noreply.github.com>
2022-11-02 11:58:33 -0700

    Improve our continuous testing for model presets (#357)

--- keras_nlp/models/bert/bert_presets_test.py
+++ keras_nlp/models/bert/bert_presets_test.py
@@ -21,8 +21,64 @@ from keras_nlp.models.bert.bert_preprocessing import BertPreprocessor
 from keras_nlp.models.bert.bert_tasks import BertClassifier
 
 
-@pytest.mark.slow
+@pytest.mark.large
+class BertPresetSmokeTest(tf.test.TestCase):
+    """
+    A smoke test for BERT presets we run continuously.
+
+    This only tests the smallest weights we have available. Run with:
+    `pytest keras_nlp/models/bert/bert_presets_test.py --run_large`
+    """
+
+    def test_preprocessor_output(self):
+        tokenizer = BertPreprocessor.from_preset(
+            "bert_tiny_uncased_en",
+            sequence_length=4,
+        )
+        outputs = tokenizer("The quick brown fox.")["token_ids"]
+        expected_outputs = [101, 1996, 4248, 102]
+        self.assertAllEqual(outputs, expected_outputs)
+
+    def test_backbone_output(self):
+        input_data = {
+            "token_ids": tf.constant([[101, 1996, 4248, 102]]),
+            "segment_ids": tf.constant([[0, 0, 0, 0]]),
+            "padding_mask": tf.constant([[1, 1, 1, 1]]),
+        }
+        model = Bert.from_preset(
+            "bert_tiny_uncased_en",
+        )
+        outputs = model(input_data)["sequence_output"][0, 0, :5]
+        # Outputs from our preset checkpoints should be stable!
+        # We should only update these numbers if we are updating a weights file,
+        # or have found a bug where output did not match the upstream source.
+        expected_outputs = [-1.38173, 0.16598, -2.92788, -2.66958, -0.61556]
+        # Keep a high tolerance, so we are robust to different hardware.
+        self.assertAllClose(outputs, expected_outputs, atol=0.01, rtol=0.01)
+
+    def test_classifier_output(self):
+        input_data = {
+            "token_ids": tf.constant([[101, 1996, 4248, 102]]),
+            "segment_ids": tf.constant([[0, 0, 0, 0]]),
+            "padding_mask": tf.constant([[1, 1, 1, 1]]),
+        }
+        model = BertClassifier(
+            "bert_tiny_uncased_en",
+        )
+        # We don't assert output values, as the head weights are random.
+        model(input_data)
+
+
+@pytest.mark.extra_large
 class BertPresetTest(tf.test.TestCase):
+    """
+    Test the full enumeration of our preset.
+
+    This only tests all enumeration of our presets and is only run manually.
+    Run with:
+    `pytest keras_nlp/models/bert_presets_test.py --run_extra_large`
+    """
+
     def test_load_bert(self):
         for preset in Bert.presets:
             model = Bert.from_preset(preset, load_weights=True)
