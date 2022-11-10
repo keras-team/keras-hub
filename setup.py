2da5f34ddb43f778fb341d0e01e90b2054537bfc
Matt Watson <mattdangerw@gmail.com>
2022-11-10 15:32:11 -0800

    Remove dev prefix for 0.3.1 release

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     ),
     long_description=README,
     long_description_content_type="text/markdown",
-    version="0.3.1.dev1",
+    version="0.3.1",
     url="https://github.com/keras-team/keras-nlp",
     author="Keras team",
     author_email="keras-nlp@google.com",
