f5141ca3e5fcbb469c127f8606c0a8b7accefba0
abheesht17 <sharmabhee@gmail.com>
2022-12-01 18:03:32 +0530

    Add __init__.py files

--- tools/pretrained_tokenizers/word_piece_cleaning_script.py
+++ tools/pretrained_tokenizers/word_piece_cleaning_script.py
@@ -1,4 +1,18 @@
+# Copyright 2022 The KerasNLP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import os
+
 import tqdm
 
 # Clean these folders and saved parsed version of them.
@@ -6,7 +20,7 @@ clean_folders = ["bnwiki", "arwiki", "ruwiki", "ptwiki", "idwiki"]
 
 for i in range(len(clean_folders)):
     clean_folder = clean_folders[i]
-    output_folder = clean_folders[i]+"_parsed"
+    output_folder = clean_folders[i] + "_parsed"
     os.mkdir(output_folder)
     for folder in tqdm.tqdm(os.listdir(clean_folder)):
         path = os.path.join(clean_folder, folder)
@@ -21,4 +35,4 @@ for i in range(len(clean_folders)):
                         article.append(line)
             with open(os.path.join(output_folder, folder, file), "w+") as f:
                 for line in article:
-                    f.write(line+"\n")
+                    f.write(line + "\n")
