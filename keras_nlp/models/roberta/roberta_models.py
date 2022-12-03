aba5d64144dbad934a4e74fca290b56f94099ae5
Abheesht <sharmabhee@gmail.com>
2022-12-03 08:23:35 +0530

    Add RoBERTa Presets (#506)
    * Add RoBERTa presets
    
    * Fix imports
    
    * Add __init__.py files
    
    * Fix typos
    
    * Fix tokenizer
    
    * Small change
    
    * Correct hashes
    
    * Change preset name
    
    * FIx priset tests
    
    * Add comment for range

--- keras_nlp/models/roberta/roberta_models.py
+++ keras_nlp/models/roberta/roberta_models.py
@@ -14,11 +14,17 @@
 
 """RoBERTa backbone models."""
 
+import copy
+import os
+
 import tensorflow as tf
 from tensorflow import keras
 
 from keras_nlp.layers import TokenAndPositionEmbedding
 from keras_nlp.layers import TransformerEncoder
+from keras_nlp.models.roberta.roberta_presets import backbone_presets
+from keras_nlp.utils.python_utils import classproperty
+from keras_nlp.utils.python_utils import format_docstring
 
 
 def roberta_kernel_initializer(stddev=0.02):
@@ -174,11 +180,65 @@ class Roberta(keras.Model):
     def from_config(cls, config):
         return cls(**config)
 
+    @classproperty
+    def presets(cls):
+        return copy.deepcopy(backbone_presets)
+
     @classmethod
+    @format_docstring(names=", ".join(backbone_presets))
     def from_preset(
         cls,
         preset,
         load_weights=True,
         **kwargs,
     ):
-        raise NotImplementedError
+        """Instantiate RoBERTa model from preset architecture and weights.
+
+        Args:
+            preset: string. Must be one of {{names}}.
+            load_weights: Whether to load pre-trained weights into model.
+                Defaults to `True`.
+
+        Examples:
+        ```python
+        input_data = {
+            "token_ids": tf.random.uniform(
+                shape=(1, 12), dtype=tf.int64, maxval=model.vocabulary_size
+            ),
+            "padding_mask": tf.constant(
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0], shape=(1, 12)
+            ),
+        }
+
+        # Load architecture and weights from preset
+        model = keras_nlp.models.Roberta.from_preset("roberta_base")
+        output = model(input_data)
+
+        # Load randomly initalized model from preset architecture
+        model = keras_nlp.models.Roberta.from_preset(
+            "roberta_base", load_weights=False
+        )
+        output = model(input_data)
+        ```
+        """
+        if preset not in cls.presets:
+            raise ValueError(
+                "`preset` must be one of "
+                f"""{", ".join(cls.presets)}. Received: {preset}."""
+            )
+        metadata = cls.presets[preset]
+        config = metadata["config"]
+        model = cls.from_config({**config, **kwargs})
+
+        if not load_weights:
+            return model
+
+        weights = keras.utils.get_file(
+            "model.h5",
+            metadata["weights_url"],
+            cache_subdir=os.path.join("models", preset),
+            file_hash=metadata["weights_hash"],
+        )
+
+        model.load_weights(weights)
+        return model
--- keras_nlp/models/roberta/roberta_tasks.py
+++ keras_nlp/models/roberta/roberta_tasks.py
@@ -13,9 +13,15 @@
 # limitations under the License.
 """RoBERTa task specific models and heads."""
 
+import copy
+
 from tensorflow import keras
 
+from keras_nlp.models.roberta.roberta_models import Roberta
 from keras_nlp.models.roberta.roberta_models import roberta_kernel_initializer
+from keras_nlp.models.roberta.roberta_presets import backbone_presets
+from keras_nlp.utils.python_utils import classproperty
+from keras_nlp.utils.python_utils import format_docstring
 
 
 @keras.utils.register_keras_serializable(package="keras_nlp")
@@ -59,7 +65,7 @@ class RobertaClassifier(keras.Model):
     def __init__(
         self,
         backbone,
-        num_classes,
+        num_classes=2,
         hidden_dim=None,
         dropout=0.0,
         **kwargs,
@@ -112,3 +118,62 @@ class RobertaClassifier(keras.Model):
         if "backbone" in config:
             config["backbone"] = keras.layers.deserialize(config["backbone"])
         return cls(**config)
+
+    @classproperty
+    def presets(cls):
+        return copy.deepcopy(backbone_presets)
+
+    @classmethod
+    @format_docstring(names=", ".join(backbone_presets))
+    def from_preset(
+        cls,
+        preset,
+        load_weights=True,
+        **kwargs,
+    ):
+        """Create a classification model from a preset architecture and weights.
+
+        Args:
+            preset: string. Must be one of {{names}}.
+            load_weights: Whether to load pre-trained weights into model.
+                Defaults to `True`.
+
+        Examples:
+        ```python
+        input_data = {
+            "token_ids": tf.random.uniform(
+                shape=(1, 12), dtype=tf.int64, maxval=model.vocabulary_size
+            ),
+            "padding_mask": tf.constant(
+                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0], shape=(1, 12)
+            ),
+        }
+
+        # Load backbone architecture and weights from preset
+        classifier = keras_nlp.models.RobertaClassifier.from_preset(
+            "roberta_base",
+            num_classes=4,
+        )
+        output = classifier(input_data)
+
+        # Load randomly initalized model from preset architecture
+        classifier = keras_nlp.models.RobertaClassifier.from_preset(
+            "roberta_base",
+            load_weights=False,
+            num_classes=4,
+        )
+        output = classifier(input_data)
+        ```
+        """
+        # Check if preset is backbone-only model
+        if preset in Roberta.presets:
+            backbone = Roberta.from_preset(preset, load_weights)
+            return cls(backbone, **kwargs)
+
+        # Otherwise must be one of class presets
+        # Currently no classifier-level presets, so must throw.
+        if preset not in cls.presets:
+            raise ValueError(
+                "`preset` must be one of "
+                f"""{", ".join(cls.presets)}. Received: {preset}."""
+            )
