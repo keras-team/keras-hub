1e92fbc7e8d5766ef34ad7981bc9e74192e36535
Matt Watson <1389937+mattdangerw@users.noreply.github.com>
2022-12-15 14:31:25 -0800

    Make XLMRobertaClassifier operate directly on raw string inputs (#583)
    Fixes #529

--- keras_nlp/models/xlm_roberta/xlm_roberta_classifier.py
+++ keras_nlp/models/xlm_roberta/xlm_roberta_classifier.py
@@ -19,14 +19,28 @@ from tensorflow import keras
 
 from keras_nlp.models.roberta.roberta_backbone import roberta_kernel_initializer
 from keras_nlp.models.xlm_roberta.xlm_roberta_backbone import XLMRobertaBackbone
+from keras_nlp.models.xlm_roberta.xlm_roberta_preprocessor import (
+    XLMRobertaPreprocessor,
+)
 from keras_nlp.models.xlm_roberta.xlm_roberta_presets import backbone_presets
+from keras_nlp.utils.pipeline_model import PipelineModel
 from keras_nlp.utils.python_utils import classproperty
 from keras_nlp.utils.python_utils import format_docstring
 
 
 @keras.utils.register_keras_serializable(package="keras_nlp")
-class XLMRobertaClassifier(keras.Model):
-    """XLM-RoBERTa encoder model with a classification head.
+class XLMRobertaClassifier(PipelineModel):
+    """An end-to-end XLM-RoBERTa model for classification tasks.
+
+    This model attaches a classification head to a
+    `keras_nlp.model.XLMRobertaBackbone` model, mapping from the backbone
+    outputs to logit output suitable for a classification task. For usage of
+    this model with pre-trained weights, see the `from_preset()` method.
+
+    This model can optionally be configured with a `preprocessor` layer, in
+    which case it will automatically apply preprocessing to raw inputs during
+    `fit()`, `predict()`, and `evaluate()`. This is done by default when
+    creating the model with `from_preset()`.
 
     Disclaimer: Pre-trained models are provided on an "as is" basis, without
     warranties or conditions of any kind. The underlying model is provided by a
@@ -37,18 +51,23 @@ class XLMRobertaClassifier(keras.Model):
         backbone: A `keras_nlp.models.XLMRoberta` instance.
         num_classes: int. Number of classes to predict.
         hidden_dim: int. The size of the pooler layer.
+        preprocessor: A `keras_nlp.models.XLMRobertaPreprocessor` or `None`. If
+            `None`, this model will not apply preprocessing, and inputs should
+            be preprocessed before calling the model.
 
     Example usage:
     ```python
-    # Call classifier on the inputs.
-    input_data = {
-        "token_ids": tf.ones(shape=(1, 12), dtype=tf.int64),
+    preprocessed_features = {
+        "token_ids": tf.ones(shape=(2, 12), dtype=tf.int64),
+        "token_ids": tf.random.uniform(
+            shape=(2, 12), dtype=tf.int64, maxval=vocabulary_size),
         "padding_mask": tf.constant(
-            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0], shape=(1, 12)),
+            [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0]] * 2, shape=(1, 12)),
     }
+    labels = [0, 3]
 
     # Randomly initialized XLM-RoBERTa encoder
-    model = keras_nlp.models.XLMRobertaBackbone(
+    backbone = keras_nlp.models.XLMRobertaBackbone(
         vocabulary_size=250002,
         num_layers=12,
         num_heads=12,
@@ -56,11 +75,14 @@ class XLMRobertaClassifier(keras.Model):
         intermediate_dim=3072,
         max_sequence_length=12
     )
+
+    # Create a XLM-RoBERTa classifier and fit your data.
     classifier = keras_nlp.models.XLMRobertaClassifier(
-        backbone=model,
+        backbone,
         num_classes=4,
+        preprocessor=None,
     )
-    logits = classifier(input_data)
+    classifier.fit(x=preprocessed_features, y=labels, batch_size=2)
 
     # Access backbone programatically (e.g., to change `trainable`)
     classifier.backbone.trainable = False
@@ -73,6 +95,7 @@ class XLMRobertaClassifier(keras.Model):
         num_classes=2,
         hidden_dim=None,
         dropout=0.0,
+        preprocessor=None,
         **kwargs,
     ):
         inputs = backbone.input
@@ -95,24 +118,33 @@ class XLMRobertaClassifier(keras.Model):
         super().__init__(
             inputs=inputs,
             outputs=outputs,
+            include_preprocessing=preprocessor is not None,
             **kwargs,
         )
         # All references to `self` below this line
         self._backbone = backbone
+        self._preprocessor = preprocessor
         self.num_classes = num_classes
         self.hidden_dim = hidden_dim
         self.dropout = dropout
 
+    def preprocess_samples(self, x, y=None, sample_weight=None):
+        return self.preprocessor(x, y=y, sample_weight=sample_weight)
+
     @property
     def backbone(self):
-        """A `keras_nlp.models.XLMRoberta` instance providing the encoder
-        submodel.
-        """
+        """A `keras_nlp.models.XLMRobertaBackbone` submodel."""
         return self._backbone
+
+    @property
+    def preprocessor(self):
+        """A `keras_nlp.models.XLMRobertaPreprocessor` preprocessing layer."""
+        return self._preprocessor
 
     def get_config(self):
         return {
             "backbone": keras.layers.serialize(self.backbone),
+            "preprocessor": keras.layers.serialize(self.preprocessor),
             "num_classes": self.num_classes,
             "hidden_dim": self.hidden_dim,
             "dropout": self.dropout,
@@ -122,8 +154,14 @@ class XLMRobertaClassifier(keras.Model):
 
     @classmethod
     def from_config(cls, config):
-        if "backbone" in config:
+        if "backbone" in config and isinstance(config["backbone"], dict):
             config["backbone"] = keras.layers.deserialize(config["backbone"])
+        if "preprocessor" in config and isinstance(
+            config["preprocessor"], dict
+        ):
+            config["preprocessor"] = keras.layers.deserialize(
+                config["preprocessor"]
+            )
         return cls(**config)
 
     @classproperty
@@ -140,36 +178,85 @@ class XLMRobertaClassifier(keras.Model):
     ):
         """Create a classification model from a preset architecture and weights.
 
+        By default, this method will automatically create a `preprocessor`
+        layer to preprocess raw inputs during `fit()`, `predict()`, and
+        `evaluate()`. If you would like to disable this behavior, pass
+        `preprocessor=None`.
+
         Args:
             preset: string. Must be one of {{names}}.
             load_weights: Whether to load pre-trained weights into model.
                 Defaults to `True`.
 
         Examples:
+
+        Raw string inputs.
         ```python
-        input_data = {
-            "token_ids": tf.ones(shape=(1, 12), dtype=tf.int64),
+        # Create a dataset with raw string features in an `(x, y)` format.
+        features = ["The quick brown fox jumped.", "I forgot my homework."]
+        labels = [0, 3]
+
+        # Create a XLMRobertaClassifier and fit your data.
+        classifier = keras_nlp.models.XLMRobertaClassifier.from_preset(
+            "xlm_roberta_base",
+            num_classes=4,
+        )
+        classifier.compile(
+            loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        )
+        classifier.fit(x=features, y=labels, batch_size=2)
+        ```
+
+        Raw string inputs with customized preprocessing.
+        ```python
+        # Create a dataset with raw string features in an `(x, y)` format.
+        features = ["The quick brown fox jumped.", "I forgot my homework."]
+        labels = [0, 3]
+
+        # Use a shorter sequence length.
+        preprocessor = keras_nlp.models.XLMRobertaPreprocessor.from_preset(
+            "xlm_roberta_base",
+            sequence_length=128,
+        )
+
+        # Create a XLMRobertaClassifier and fit your data.
+        classifier = keras_nlp.models.XLMRobertaClassifier.from_preset(
+            "xlm_roberta_base",
+            num_classes=4,
+            preprocessor=preprocessor,
+        )
+        classifier.compile(
+            loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+        )
+        classifier.fit(x=features, y=labels, batch_size=2)
+        ```
+
+        Preprocessed inputs.
+        ```python
+        # Create a dataset with preprocessed features in an `(x, y)` format.
+        preprocessed_features = {
+            "token_ids": tf.ones(shape=(2, 12), dtype=tf.int64),
             "padding_mask": tf.constant(
-                [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0], shape=(1, 12)
+                [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0]] * 2, shape=(2, 12)
             ),
         }
+        labels = [0, 3]
 
-        # Load backbone architecture and weights from preset
+        # Create a XLMRobertaClassifier and fit your data.
         classifier = keras_nlp.models.XLMRobertaClassifier.from_preset(
             "xlm_roberta_base",
             num_classes=4,
+            preprocessor=None,
         )
-        output = classifier(input_data)
-
-        # Load randomly initialized model from preset architecture
-        classifier = keras_nlp.models.XLMRobertaClassifier.from_preset(
-            "xlm_roberta_base",
-            load_weights=False,
-            num_classes=4,
+        classifier.compile(
+            loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
         )
-        output = classifier(input_data)
+        classifier.fit(x=preprocessed_features, y=labels, batch_size=2)
         ```
         """
+        if "preprocessor" not in kwargs:
+            kwargs["preprocessor"] = XLMRobertaPreprocessor.from_preset(preset)
+
         # Check if preset is backbone-only model
         if preset in XLMRobertaBackbone.presets:
             backbone = XLMRobertaBackbone.from_preset(preset, load_weights)
