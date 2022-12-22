e541c0e8841bd57b130c994382a393a1eda131fe
Abheesht <sharmabhee@gmail.com>
2022-12-22 23:23:30 +0530

    File-level Doc-string Changes for Classifiers and Presets (#604)
    * Some file-level doc-string fixes
    
    * Remove defaults from dropout values in classifier

--- keras_nlp/models/xlm_roberta/xlm_roberta_classifier.py
+++ keras_nlp/models/xlm_roberta/xlm_roberta_classifier.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""XLM-RoBERTa task specific models and heads."""
+"""XLM-RoBERTa classification model."""
 
 import copy
 
@@ -51,8 +51,8 @@ class XLMRobertaClassifier(PipelineModel):
         backbone: A `keras_nlp.models.XLMRoberta` instance.
         num_classes: int. Number of classes to predict.
         hidden_dim: int. The size of the pooler layer.
-        dropout: float, defaults to 0.0. The dropout probability value, applied
-            to the pooled output, and after the first dense layer.
+        dropout: float. The dropout probability value, applied to the pooled
+            output, and after the first dense layer.
         preprocessor: A `keras_nlp.models.XLMRobertaPreprocessor` or `None`. If
             `None`, this model will not apply preprocessing, and inputs should
             be preprocessed before calling the model.
--- keras_nlp/models/xlm_roberta/xlm_roberta_classifier_test.py
+++ keras_nlp/models/xlm_roberta/xlm_roberta_classifier_test.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for XLM-RoBERTa task specific models and heads."""
+"""Tests for XLM-RoBERTa classification model."""
 
 import io
 import os
