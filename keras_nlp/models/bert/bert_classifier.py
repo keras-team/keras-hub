e5b6687900659075f2be7f91c5bb45dc2a4677ab
abheesht17 <sharmabhee@gmail.com>
2022-12-22 17:44:30 +0530

    Some file-level doc-string fixes

--- keras_nlp/models/bert/bert_classifier.py
+++ keras_nlp/models/bert/bert_classifier.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""BERT task specific models and heads."""
+"""BERT classification model."""
 
 import copy
 import os
