6cea03b8eba06435461eb63c4e847f73ed199499
Abheesht <sharmabhee@gmail.com>
2023-01-14 02:39:13 +0530

    Add FNet Preprocessor (#646)
    * Add FNet Preprocessor
    
    * Add imports to __init__.py
    
    * Remove padding_mask ref
    
    * Change fnet to f_net

--- keras_nlp/models/__init__.py
+++ keras_nlp/models/__init__.py
@@ -29,6 +29,8 @@ from keras_nlp.models.distil_bert.distil_bert_preprocessor import (
 from keras_nlp.models.distil_bert.distil_bert_tokenizer import (
     DistilBertTokenizer,
 )
+from keras_nlp.models.f_net.f_net_preprocessor import FNetPreprocessor
+from keras_nlp.models.f_net.f_net_tokenizer import FNetTokenizer
 from keras_nlp.models.roberta.roberta_backbone import RobertaBackbone
 from keras_nlp.models.roberta.roberta_classifier import RobertaClassifier
 from keras_nlp.models.roberta.roberta_preprocessor import RobertaPreprocessor
