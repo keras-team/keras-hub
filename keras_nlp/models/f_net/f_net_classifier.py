7cd46dfc168b3275887106c86e3a23a573d6a4e0
ADITYA DAS <das767034@gmail.com>
2023-01-18 10:21:35 +0530

    Revert "Update f_net_classifier.py"
    This reverts commit 992160dac106b79e8e2797d32ede1fa3485542d0.

--- keras_nlp/models/f_net/f_net_classifier.py
+++ keras_nlp/models/f_net/f_net_classifier.py
@@ -11,10 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """FNet classification model."""
 
-
+import copy
 import os
 
 from tensorflow import keras
@@ -24,13 +23,11 @@ from keras_nlp.models.f_net.f_net_backbone import f_net_kernel_initializer
 from keras_nlp.models.f_net.f_net_preprocessor import FNetPreprocessor
 from keras_nlp.utils.pipeline_model import PipelineModel
 from keras_nlp.utils.python_utils import classproperty
-
-
+from keras_nlp.utils.python_utils import format_docstring
 
 
 @keras.utils.register_keras_serializable(package="keras_nlp")
 class FnetClassifier(PipelineModel):
-
     """An end-to-end f_net model for classification tasks.
 
     This model attaches a classification head to a
@@ -94,8 +91,7 @@ class FnetClassifier(PipelineModel):
     classifier.backbone.trainable = False
     ```
     """
-
-
+    
     def __init__(
         self,
         backbone,
@@ -162,7 +158,7 @@ class FnetClassifier(PipelineModel):
 
     @classproperty
     def presets(cls):
-        return NotImplemented
+        return error
 
     @classmethod
     def from_preset(
