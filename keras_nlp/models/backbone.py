2d7ed18a0ead164bdee45b9ef0e4d5cb0ab14b39
Matt Watson <mattdangerw@gmail.com>
2023-01-18 16:03:05 -0800

    Add comments explaining get_config/from_config

--- keras_nlp/models/backbone.py
+++ keras_nlp/models/backbone.py
@@ -28,6 +28,8 @@ class Backbone(keras.Model):
         super().__init__(*args, **kwargs)
 
     def get_config(self):
+        # Don't chain to super here. The default `get_config()` for functional
+        # models is nested and cannot be passed to our Backbone constructors.
         return {
             "name": self.name,
             "trainable": self.trainable,
@@ -35,6 +37,8 @@ class Backbone(keras.Model):
 
     @classmethod
     def from_config(cls, config):
+        # The default `from_config()` for functional models will return a
+        # vanilla `keras.Model`. We override it to get a subclass instance back.
         return cls(**config)
 
     @classproperty
