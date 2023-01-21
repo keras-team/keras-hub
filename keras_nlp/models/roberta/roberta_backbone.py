a7442f3aa749c4ec26a47050eacd0de2e3e24796
abheesht17 <sharmabhee@gmail.com>
2023-01-21 21:34:15 +0530

    RoBERTa fix test

--- keras_nlp/models/roberta/roberta_backbone.py
+++ keras_nlp/models/roberta/roberta_backbone.py
@@ -108,14 +108,14 @@ class RobertaBackbone(Backbone):
         )
 
         # Embed tokens and positions.
-        token_and_position_embedding_layer = TokenAndPositionEmbedding(
+        embedding_layer = TokenAndPositionEmbedding(
             vocabulary_size=vocabulary_size,
             sequence_length=max_sequence_length,
             embedding_dim=hidden_dim,
             embeddings_initializer=roberta_kernel_initializer(),
             name="embeddings",
         )
-        embedding = token_and_position_embedding_layer(token_id_input)
+        embedding = embedding_layer(token_id_input)
 
         # Sum, normalize and apply dropout to embeddings.
         x = keras.layers.LayerNormalization(
@@ -150,9 +150,7 @@ class RobertaBackbone(Backbone):
             **kwargs,
         )
         # All references to `self` below this line
-        self._token_embedding = (
-            token_and_position_embedding_layer.token_embedding
-        )
+        self._token_embedding = embedding_layer.token_embedding
         self.vocabulary_size = vocabulary_size
         self.num_layers = num_layers
         self.num_heads = num_heads
