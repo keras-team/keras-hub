3e9edab1032d1525541e4f57c8e6e31309265465
Matt Watson <1389937+mattdangerw@users.noreply.github.com>
2023-02-01 17:36:55 -0800

    Add OPT Backbone and Tokenizer (#699)
    * Start OPT
    
    * fix docs
    
    * Add init file

--- keras_nlp/models/__init__.py
+++ keras_nlp/models/__init__.py
@@ -40,6 +40,8 @@ from keras_nlp.models.gpt2.gpt2_causal_lm_preprocessor import (
 )
 from keras_nlp.models.gpt2.gpt2_preprocessor import GPT2Preprocessor
 from keras_nlp.models.gpt2.gpt2_tokenizer import GPT2Tokenizer
+from keras_nlp.models.opt.opt_backbone import OPTBackbone
+from keras_nlp.models.opt.opt_tokenizer import OPTTokenizer
 from keras_nlp.models.roberta.roberta_backbone import RobertaBackbone
 from keras_nlp.models.roberta.roberta_classifier import RobertaClassifier
 from keras_nlp.models.roberta.roberta_masked_lm import RobertaMaskedLM
