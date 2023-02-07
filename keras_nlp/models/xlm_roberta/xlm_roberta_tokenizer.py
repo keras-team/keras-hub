8aae1b8425d7d3940036634b17cb627701d15c9e
Somasree <bishnupadamajumder32@gmail.com>
2023-02-07 22:08:42 +0530

    Adding

--- keras_nlp/models/xlm_roberta/xlm_roberta_tokenizer.py
+++ keras_nlp/models/xlm_roberta/xlm_roberta_tokenizer.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""XLM-RoBERTa tokenizer."""
+"""XLM RoBERTa tokenizer."""
 
 import copy
 import os
