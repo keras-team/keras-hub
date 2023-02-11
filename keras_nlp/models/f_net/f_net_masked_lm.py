80d6f9eb14bd159ea10b342d5671a4107f8c6927
apupneja <apupneja2002@gmail.com>
2023-02-11 07:35:16 +0530

    Formatting the code

--- keras_nlp/models/f_net/f_net_masked_lm.py
+++ keras_nlp/models/f_net/f_net_masked_lm.py
@@ -1,3 +1,16 @@
+# Copyright 2023 The KerasNLP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import copy
 
 from tensorflow import keras
@@ -8,10 +21,10 @@ from keras_nlp.models.f_net.f_net_backbone import f_net_kernel_initializer
 from keras_nlp.models.f_net.f_net_masked_lm_preprocessor import (
     FNetMaskedLMPreprocessor,
 )
-
 from keras_nlp.models.f_net.f_net_presets import backbone_presets
 from keras_nlp.models.task import Task
 from keras_nlp.utils.python_utils import classproperty
+
 
 @keras.utils.register_keras_serializable(package="keras_nlp")
 class FNetMaskedLM(Task):
@@ -85,6 +98,7 @@ class FNetMaskedLM(Task):
     masked_lm.fit(x=preprocessed_features, y=labels, batch_size=2)
     ```
     """
+
     def __init__(
         self,
         backbone,
