95fcebe4bfe55ccc60c1905dd67e7d3425ddd605
abheesht17 <sharmabhee@gmail.com>
2023-02-15 21:09:50 +0530

    Reformat

--- tools/checkpoint_conversion/convert_albert_checkpoints.py
+++ tools/checkpoint_conversion/convert_albert_checkpoints.py
@@ -11,11 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import json
 import os
+import shutil
 
 import numpy as np
-import requests
 import tensorflow as tf
 import transformers
 from absl import app
@@ -33,7 +32,6 @@ PRESET_MAP = {
     "albert_extra_extra_large_en_uncased": "albert-xxlarge-v2",
 }
 
-EXTRACT_DIR = "./{}"
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string(
@@ -41,76 +39,19 @@ flags.DEFINE_string(
 )
 
 
-def download_files(preset, hf_model_name):
-    print("-> Download original vocabulary and config.")
-
-    extract_dir = EXTRACT_DIR.format(preset)
-    if not os.path.exists(extract_dir):
-        os.makedirs(extract_dir)
-
-    # Config.
-    config_path = os.path.join(extract_dir, "config.json")
-    response = requests.get(
-        f"https://huggingface.co/{hf_model_name}/raw/main/config.json"
-    )
-    open(config_path, "wb").write(response.content)
-    print(f"`{config_path}`")
-
-    # Vocab.
-    spm_path = os.path.join(extract_dir, "spiece.model")
-    response = requests.get(
-        f"https://huggingface.co/{hf_model_name}/resolve/main/spiece.model"
-    )
-    open(spm_path, "wb").write(response.content)
-    print(f"`{spm_path}`")
-
-
-def define_preprocessor(preset, hf_model_name):
-    print("\n-> Define the tokenizers.")
-    extract_dir = EXTRACT_DIR.format(preset)
-    spm_path = os.path.join(extract_dir, "spiece.model")
-
-    keras_nlp_tokenizer = keras_nlp.models.AlbertTokenizer(
-        proto=spm_path,
-    )
-    keras_nlp_preprocessor = keras_nlp.models.AlbertPreprocessor(
-        keras_nlp_tokenizer
-    )
-
-    hf_tokenizer = transformers.AutoTokenizer.from_pretrained(hf_model_name)
-
-    print("\n-> Print MD5 checksum of the vocab files.")
-    print(f"`{spm_path}` md5sum: ", get_md5_checksum(spm_path))
-
-    return keras_nlp_preprocessor, hf_tokenizer
-
-
-def convert_checkpoints(preset, keras_nlp_model, hf_model):
+def convert_checkpoints(hf_model):
     print("\n-> Convert original weights to KerasNLP format.")
 
-    extract_dir = EXTRACT_DIR.format(preset)
-    config_path = os.path.join(extract_dir, "config.json")
-
-    # Build config.
-    cfg = {}
-    with open(config_path, "r") as hf_cfg_handler:
-        hf_cfg = json.load(hf_cfg_handler)
-    cfg["vocabulary_size"] = hf_cfg["vocab_size"]
-    cfg["num_layers"] = hf_cfg["num_hidden_layers"]
-    cfg["num_heads"] = hf_cfg["num_attention_heads"]
-    cfg["num_groups"] = hf_cfg["num_hidden_groups"]
-    cfg["num_inner_repetitions"] = hf_cfg["inner_group_num"]
-    cfg["embedding_dim"] = hf_cfg["embedding_size"]
-    cfg["hidden_dim"] = hf_cfg["hidden_size"]
-    cfg["intermediate_dim"] = hf_cfg["intermediate_size"]
-    cfg["dropout"] = hf_cfg["hidden_dropout_prob"]
-    cfg["max_sequence_length"] = hf_cfg["max_position_embeddings"]
-    cfg["num_segments"] = hf_cfg["type_vocab_size"]
-    print("Config:", cfg)
+    keras_nlp_model = keras_nlp.models.AlbertBackbone.from_preset(
+        FLAGS.preset, load_weights=False
+    )
 
     hf_wts = hf_model.state_dict()
     print("Original weights:")
     print(list(hf_wts.keys()))
+
+    num_heads = keras_nlp_model.num_heads
+    hidden_dim = keras_nlp_model.hidden_dim
 
     keras_nlp_model.get_layer("token_embedding").embeddings.assign(
         hf_wts["embeddings.word_embeddings.weight"]
@@ -145,7 +86,7 @@ def convert_checkpoints(preset, keras_nlp_model, hf_model):
                     f"encoder.albert_layer_groups.{i}.albert_layers.{j}.attention.query.weight"
                 ]
                 .transpose(1, 0)
-                .reshape((cfg["hidden_dim"], cfg["num_heads"], -1))
+                .reshape((hidden_dim, num_heads, -1))
                 .numpy()
             )
             keras_nlp_model.get_layer(
@@ -154,7 +95,7 @@ def convert_checkpoints(preset, keras_nlp_model, hf_model):
                 hf_wts[
                     f"encoder.albert_layer_groups.{i}.albert_layers.{j}.attention.query.bias"
                 ]
-                .reshape((cfg["num_heads"], -1))
+                .reshape((num_heads, -1))
                 .numpy()
             )
 
@@ -165,7 +106,7 @@ def convert_checkpoints(preset, keras_nlp_model, hf_model):
                     f"encoder.albert_layer_groups.{i}.albert_layers.{j}.attention.key.weight"
                 ]
                 .transpose(1, 0)
-                .reshape((cfg["hidden_dim"], cfg["num_heads"], -1))
+                .reshape((hidden_dim, num_heads, -1))
                 .numpy()
             )
             keras_nlp_model.get_layer(
@@ -174,7 +115,7 @@ def convert_checkpoints(preset, keras_nlp_model, hf_model):
                 hf_wts[
                     f"encoder.albert_layer_groups.{i}.albert_layers.{j}.attention.key.bias"
                 ]
-                .reshape((cfg["num_heads"], -1))
+                .reshape((num_heads, -1))
                 .numpy()
             )
 
@@ -185,7 +126,7 @@ def convert_checkpoints(preset, keras_nlp_model, hf_model):
                     f"encoder.albert_layer_groups.{i}.albert_layers.{j}.attention.value.weight"
                 ]
                 .transpose(1, 0)
-                .reshape((cfg["hidden_dim"], cfg["num_heads"], -1))
+                .reshape((hidden_dim, num_heads, -1))
                 .numpy()
             )
             keras_nlp_model.get_layer(
@@ -194,7 +135,7 @@ def convert_checkpoints(preset, keras_nlp_model, hf_model):
                 hf_wts[
                     f"encoder.albert_layer_groups.{i}.albert_layers.{j}.attention.value.bias"
                 ]
-                .reshape((cfg["num_heads"], -1))
+                .reshape((num_heads, -1))
                 .numpy()
             )
 
@@ -205,7 +146,7 @@ def convert_checkpoints(preset, keras_nlp_model, hf_model):
                     f"encoder.albert_layer_groups.{i}.albert_layers.{j}.attention.dense.weight"
                 ]
                 .transpose(1, 0)
-                .reshape((cfg["num_heads"], -1, cfg["hidden_dim"]))
+                .reshape((num_heads, -1, hidden_dim))
                 .numpy()
             )
             keras_nlp_model.get_layer(
@@ -288,14 +229,37 @@ def convert_checkpoints(preset, keras_nlp_model, hf_model):
     )
 
     # Save the model.
-    print(f"\n-> Save KerasNLP model weights to `{preset}.h5`.")
-    keras_nlp_model.save_weights(f"{preset}.h5")
+    print("\n-> Save KerasNLP model weights.")
+    keras_nlp_model.save_weights(os.path.join(FLAGS.preset, "model.h5"))
 
     return keras_nlp_model
 
 
+def extract_vocab(hf_tokenizer):
+    spm_path = os.path.join(FLAGS.preset, "spiece.model")
+    print(f"\n-> Save KerasNLP SPM vocabulary file to `{spm_path}`.")
+
+    shutil.copyfile(
+        transformers.utils.hub.get_file_from_repo(
+            hf_tokenizer.name_or_path, "spiece.model"
+        ),
+        spm_path,
+    )
+
+    keras_nlp_tokenizer = keras_nlp.models.AlbertTokenizer(
+        proto=spm_path,
+    )
+    keras_nlp_preprocessor = keras_nlp.models.AlbertPreprocessor(
+        keras_nlp_tokenizer
+    )
+
+    print("-> Print MD5 checksum of the vocab files.")
+    print(f"`{spm_path}` md5sum: ", get_md5_checksum(spm_path))
+
+    return keras_nlp_preprocessor
+
+
 def check_output(
-    preset,
     keras_nlp_preprocessor,
     keras_nlp_model,
     hf_tokenizer,
@@ -321,33 +285,26 @@ def check_output(
     print("Difference:", np.mean(keras_nlp_output - hf_output.detach().numpy()))
 
     # Show the MD5 checksum of the model weights.
-    print("Model md5sum: ", get_md5_checksum(f"./{preset}.h5"))
+    print(
+        "Model md5sum: ",
+        get_md5_checksum(os.path.join(FLAGS.preset, "model.h5")),
+    )
 
 
 def main(_):
     hf_model_name = PRESET_MAP[FLAGS.preset]
 
-    download_files(FLAGS.preset, hf_model_name)
-
-    keras_nlp_preprocessor, hf_tokenizer = define_preprocessor(
-        FLAGS.preset, hf_model_name
-    )
-
-    print("\n-> Load KerasNLP model.")
-    keras_nlp_model = keras_nlp.models.AlbertBackbone.from_preset(
-        FLAGS.preset, load_weights=False
-    )
-
-    print("\n-> Load HF model.")
+    print("\n-> Load HF model and HF tokenizer.")
     hf_model = transformers.AutoModel.from_pretrained(hf_model_name)
     hf_model.eval()
+    hf_tokenizer = transformers.AutoTokenizer.from_pretrained(hf_model_name)
 
-    keras_nlp_model = convert_checkpoints(
-        FLAGS.preset, keras_nlp_model, hf_model
-    )
+    print("\n-> Load KerasNLP model and convert checkpoints.")
+    keras_nlp_model = convert_checkpoints(hf_model)
+    print("\n -> Load KerasNLP preprocessor.")
+    keras_nlp_preprocessor = extract_vocab(hf_tokenizer)
 
     check_output(
-        FLAGS.preset,
         keras_nlp_preprocessor,
         keras_nlp_model,
         hf_tokenizer,
