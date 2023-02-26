3a1eb8a896e5ec7a31dbf3a84ec2297aaf136bea
Maaz Karim <MaazKarim02@gmail.com>
2023-02-26 11:08:57 +0530

    Reformat codew

--- keras_nlp/models/bert/bert_masked_lm.py
+++ keras_nlp/models/bert/bert_masked_lm.py
@@ -80,7 +80,8 @@ class BertMaskedLM(Task):
         "padding_mask": tf.constant(
             [[1, 1, 1, 1, 1, 1, 1, 1]] * 2, shape=(2, 8)
         ),
-        "mask_positions": tf.constant([[2, 4]] * 2, shape=(2, 2))
+        "mask_positions": tf.constant([[2, 4]] * 2, shape=(2, 2)),
+        "segment_ids": tf.constant([[0, 0, 0, 0, 0, 0, 0, 0]] * 2, shape=(2, 8))
     }
     # Labels are the original masked values.
     labels = [[3, 5]] * 2
