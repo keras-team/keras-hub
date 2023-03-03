822bff4bdf4b83fc459abb5a635aa37f2635791e
abheesht17 <sharmabhee@gmail.com>
2023-03-04 00:41:03 +0530

    Matt's simple solution!

--- keras_nlp/models/whisper/whisper_encoder.py
+++ keras_nlp/models/whisper/whisper_encoder.py
@@ -14,67 +14,19 @@
 """Whisper encoder block."""
 
 
-from tensorflow import keras
-
 from keras_nlp.layers.transformer_encoder import TransformerEncoder
-from keras_nlp.models.whisper.whisper_multi_head_attention import (
-    WhisperMultiHeadAttention,
-)
-from keras_nlp.utils.keras_utils import clone_initializer
 
 
 class WhisperEncoder(TransformerEncoder):
     """Whisper encoder.
 
     Inherits from `keras_nlp.layers.TransformerEncoder`, and overrides the
-    `_build` method to use the
-    `keras_nlp.models.whisper.whisper_multi_head_attention.WhisperMultiHeadAttention`
-    layer instead of `keras.layers.MultiHeadAttention`.
+    `_build` method so as to remove the bias term from the key projection layer.
     """
 
     def _build(self, input_shape):
-        # Create layers based on input shape.
-        self._built = True
-        self._input_shape = input_shape
-        # Infer the dimension of our hidden feature size from the build shape.
-        hidden_dim = input_shape[-1]
-        # Attention head size is `hidden_dim` over the number of heads.
-        key_dim = int(hidden_dim // self.num_heads)
+        super()._build(input_shape)
 
-        # Self attention layers.
-        self._self_attention_layer = WhisperMultiHeadAttention(
-            num_heads=self.num_heads,
-            key_dim=key_dim,
-            dropout=self.dropout,
-            kernel_initializer=clone_initializer(self.kernel_initializer),
-            bias_initializer=clone_initializer(self.bias_initializer),
-        )
-        self._self_attention_layer._build_from_signature(
-            query=input_shape,
-            value=input_shape,
-        )
-        self._self_attention_layernorm = keras.layers.LayerNormalization(
-            epsilon=self.layer_norm_epsilon,
-        )
-        self._self_attention_dropout = keras.layers.Dropout(
-            rate=self.dropout,
-        )
-
-        # Feedforward layers.
-        self._feedforward_layernorm = keras.layers.LayerNormalization(
-            epsilon=self.layer_norm_epsilon,
-        )
-        self._feedforward_intermediate_dense = keras.layers.Dense(
-            self.intermediate_dim,
-            activation=self.activation,
-            kernel_initializer=clone_initializer(self.kernel_initializer),
-            bias_initializer=clone_initializer(self.bias_initializer),
-        )
-        self._feedforward_output_dense = keras.layers.Dense(
-            hidden_dim,
-            kernel_initializer=clone_initializer(self.kernel_initializer),
-            bias_initializer=clone_initializer(self.bias_initializer),
-        )
-        self._feedforward_dropout = keras.layers.Dropout(
-            rate=self.dropout,
-        )
+        # Since there is no exposed option for this in MHA, we will reach into
+        # the internals of the layer for now.
+        self._self_attention_layer._key_dense.bias_axes = None
