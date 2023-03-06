d9ab8b397243879443ac02d176e16e957a91e070
Chen Qian <chenmoney@google.com>
2023-03-06 15:55:57 -0800

    Stop generation once end_token_id is seen (#769)
    * Stop generation once end_token_id is seen
    
    * Better code!
    
    * fix none end_token_id
    
    * fix test
    
    * fix tests
    
    * siimplify the end_token_id logic
    
    * even better
    
    * fix comments

--- keras_nlp/samplers/sampler.py
+++ keras_nlp/samplers/sampler.py
@@ -236,8 +236,9 @@ class Sampler:
             token_probability_fn,
             mask,
             max_length - shortest_prompt_len,
-            cache=cache,
             from_logits=from_logits,
+            end_token_id=end_token_id,
+            cache=cache,
         )
         # Mask out tokens after `end_token_id`.
         if end_token_id is not None:
@@ -269,6 +270,7 @@ class Sampler:
         mask,
         num_steps,
         from_logits=True,
+        end_token_id=None,
         cache=None,
     ):
         """Sampling logic implementation.
@@ -284,6 +286,9 @@ class Sampler:
             from_logits: bool, defaults to True. Indicate if the
                 `token_probability_fn` returns logits. If False,
                 `token_probability_fn` returns probability distributions.
+            end_token_id: int, defaults to None. The token marking the end of
+                the sequence, once encountered the generation is finished for
+                the exact sequence.
             cache: a dense int tensor, the cache used in decoding. The cache
                 stores the key and value of each
                 `keras_nlp.layers.CachedMultiHeadAttention` layer to make the
@@ -299,8 +304,9 @@ class Sampler:
         # The index of the last non-padding token in prompt. Since all sequences
         # are aligned to the right side, the index is the same for all.
         current_index = max_length - num_steps
+        original_padding_mask = tf.cast(tf.identity(mask), dtype=tf.int32)
 
-        def one_step(
+        def body(
             current_index,
             prompt,
             mask,
@@ -316,7 +322,10 @@ class Sampler:
                 )
                 next_token_probs = tf.squeeze(probs, axis=1)
             else:
-                probs = token_probability_fn(prompt, mask)
+                probs = token_probability_fn(
+                    prompt,
+                    mask,
+                )
                 next_token_probs = tf.gather(
                     probs,
                     tf.repeat(current_index - 1, batch_size),
@@ -343,24 +352,32 @@ class Sampler:
             current_index = tf.add(current_index, 1)
             if cache is None:
                 return current_index, prompt, mask
-            return [current_index, prompt, mask, cache]
+            return current_index, prompt, mask, cache
+
+        def cond(current_index, prompt, mask, cache=None):
+            if end_token_id is None:
+                return True
+            end_token_seen = (prompt == end_token_id) & (
+                original_padding_mask == 0
+            )
+            sequence_done = tf.reduce_any(end_token_seen, axis=-1)
+            all_done = tf.reduce_all(sequence_done)
+            return not all_done
 
         if cache is None:
             _, prompt, _ = tf.while_loop(
-                cond=lambda current_index, prompt, mask: tf.less(
-                    current_index, max_length
-                ),
-                body=one_step,
-                loop_vars=[current_index, prompt, mask],
+                cond=cond,
+                body=body,
+                loop_vars=(current_index, prompt, mask),
+                maximum_iterations=num_steps,
             )
             return prompt
         # Run a while loop till `max_length` of tokens has been generated.
         _, prompt, _, _ = tf.while_loop(
-            cond=lambda current_index, prompt, mask, cache: tf.less(
-                current_index, max_length
-            ),
-            body=one_step,
-            loop_vars=[current_index, prompt, mask, cache],
+            cond=cond,
+            body=body,
+            loop_vars=(current_index, prompt, mask, cache),
+            maximum_iterations=num_steps,
         )
         return prompt
 
