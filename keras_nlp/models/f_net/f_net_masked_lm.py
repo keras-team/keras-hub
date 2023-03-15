7d098015cf3dafdb42e0d13f44c8594fe746f684
Somasree Majumder <56045049+soma2000-lang@users.noreply.github.com>
2023-03-15 20:22:24 +0530

    Update f_net_masked_lm.py

--- keras_nlp/models/f_net/f_net_masked_lm.py
+++ keras_nlp/models/f_net/f_net_masked_lm.py
@@ -139,7 +139,7 @@ class FNetMaskedLM(Task):
         self.compile(
             loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
             optimizer=keras.optimizers.Adam(2e-5),
-            metrics=keras.metrics.SparseCategoricalAccuracy(),
+            weighted_metrics=keras.metrics.SparseCategoricalAccuracy(),
             jit_compile=is_xla_compatible(self),
         )
 
