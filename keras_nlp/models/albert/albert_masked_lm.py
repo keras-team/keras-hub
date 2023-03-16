44d79dfa20a6ce815754a0b5b1c386eb5f6e5aa2
Anshuman Mishra <51750587+shivance@users.noreply.github.com>
2023-03-16 03:27:46 -0700

    Update albert_masked_lm.py

--- keras_nlp/models/albert/albert_masked_lm.py
+++ keras_nlp/models/albert/albert_masked_lm.py
@@ -146,7 +146,7 @@ class AlbertMaskedLM(Task):
         self.compile(
             loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
             optimizer=keras.optimizers.Adam(5e-5),
-            metrics=keras.metrics.SparseCategoricalAccuracy(),
+            weighted_metrics=keras.metrics.SparseCategoricalAccuracy(),
             jit_compile=is_xla_compatible(self),
         )
 
