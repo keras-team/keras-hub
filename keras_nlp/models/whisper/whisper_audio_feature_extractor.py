95be44ab7f74052db1cd450fed06169e8237f2ee
abheesht17 <sharmabhee@gmail.com>
2023-03-18 00:21:27 +0530

    Remove librosa as a dep

--- keras_nlp/models/whisper/whisper_audio_feature_extractor.py
+++ keras_nlp/models/whisper/whisper_audio_feature_extractor.py
@@ -11,14 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-
-try:
-    import librosa
-except ImportError:
-    librosa = None
-
 
 NUM_MELS = 80
 
@@ -54,12 +50,6 @@ class WhisperAudioFeatureExtractor(keras.layers.Layer):
         chunk_length=30,
         **kwargs,
     ):
-        if librosa is None:
-            raise ImportError(
-                f"{self.__class__.__name__} requires the `librosa` "
-                "package. Please install it with `pip install librosa`."
-            )
-
         # Check dtype and provide a default.
         if "dtype" not in kwargs or kwargs["dtype"] is None:
             kwargs["dtype"] = tf.float32
@@ -80,15 +70,69 @@ class WhisperAudioFeatureExtractor(keras.layers.Layer):
 
         # After transposition, `self.mel_filters`'s shape is
         # `(num_fft_bins // 2 + 1, NUM_MELS).`
-        self.mel_filters = tf.constant(
-            librosa.filters.mel(
-                sr=sample_rate,
-                n_fft=num_fft_bins,
-                n_mels=NUM_MELS,
-            ),
-            dtype=self.dtype,
+        self.mel_filters = tf.transpose(tf.constant(self._get_mel_filters()))
+
+    def _get_mel_filters(self):
+        """
+        Adapted from Hugging Face
+        (https://github.com/huggingface/transformers/blob/v4.27.1/src/transformers/models/whisper/feature_extraction_whisper.py#L86)
+        """
+
+        # TODO: Convert to TensorFlow ops (if possible).
+
+        dtype = np.float32
+        # Initialize the weights
+        weights = np.zeros(
+            (NUM_MELS, int(1 + self.num_fft_bins // 2)), dtype=dtype
         )
-        self.mel_filters = tf.transpose(self.mel_filters)
+
+        # Center freqs of each FFT bin
+        fftfreqs = np.fft.rfftfreq(
+            n=self.num_fft_bins, d=1.0 / self.sample_rate
+        )
+
+        # 'Center freqs' of mel bands - uniformly spaced between limits
+        min_mel = 0.0
+        max_mel = 45.245640471924965
+
+        mels = np.linspace(min_mel, max_mel, NUM_MELS + 2)
+
+        mels = np.asanyarray(mels)
+
+        # Fill in the linear scale
+        f_min = 0.0
+        f_sp = 200.0 / 3
+        freqs = f_min + f_sp * mels
+
+        # And now the nonlinear scale
+        min_log_hz = 1000.0  # beginning of log region (Hz)
+        min_log_mel = (min_log_hz - f_min) / f_sp  # same (Mels)
+        logstep = np.log(6.4) / 27.0  # step size for log region
+
+        # If we have vector data, vectorize
+        log_t = mels >= min_log_mel
+        freqs[log_t] = min_log_hz * np.exp(
+            logstep * (mels[log_t] - min_log_mel)
+        )
+
+        mel_f = freqs
+
+        fdiff = np.diff(mel_f)
+        ramps = np.subtract.outer(mel_f, fftfreqs)
+
+        for i in range(NUM_MELS):
+            # lower and upper slopes for all bins
+            lower = -ramps[i] / fdiff[i]
+            upper = ramps[i + 2] / fdiff[i + 1]
+
+            # .. then intersect them with each other and zero
+            weights[i] = np.maximum(0, np.minimum(lower, upper))
+
+        # Slaney-style mel is scaled to be approx constant energy per channel
+        enorm = 2.0 / (mel_f[2 : NUM_MELS + 2] - mel_f[:NUM_MELS])
+        weights *= enorm[:, np.newaxis]
+
+        return weights
 
     def _extract_audio_features(self, audio):
         # Use "reflection" padding - `tf.signal.stft` uses symmetric padding
