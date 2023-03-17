bde4fe1172846391a1f56390dd49db30733ca6fe
Anshuman Mishra <51750587+shivance@users.noreply.github.com>
2023-03-17 09:19:32 +0530

    Update distil_bert_masked_lm.py

--- keras_nlp/models/distil_bert/distil_bert_masked_lm.py
+++ keras_nlp/models/distil_bert/distil_bert_masked_lm.py
@@ -146,7 +146,7 @@ class DistilBertMaskedLM(Task):
 
         self.compile(
             loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-            optimizer=keras.optimizers.Adam(1e-4),
+            optimizer=keras.optimizers.Adam(5e-5),
             weighted_metrics=keras.metrics.SparseCategoricalAccuracy(),
             jit_compile=is_xla_compatible(self),
         )
