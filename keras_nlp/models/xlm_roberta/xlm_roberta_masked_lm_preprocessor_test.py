d44390f4807f9b80ae41a7023651c0420725896b
Somasree Majumder <bishnupadamajumder32@gmail.com>
2023-03-18 14:05:17 +0000

    com

--- keras_nlp/models/xlm_roberta/xlm_roberta_masked_lm_preprocessor_test.py
+++ keras_nlp/models/xlm_roberta/xlm_roberta_masked_lm_preprocessor_test.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for ALBERT masked language model preprocessor layer."""
+"""Tests for  XLMRoBERTa masked language model preprocessor layer."""
 
 import io
 import os
