c070c94a9b41008bb64e56d1ede3a053bd4cb4cb
Somasree Majumder <56045049+soma2000-lang@users.noreply.github.com>
2023-03-20 15:20:33 +0530

    Update constrastive_search_sampler_test.py

--- keras_nlp/samplers/constrastive_search_sampler_test.py
+++ keras_nlp/samplers/constrastive_search_sampler_test.py
@@ -11,92 +11,37 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""Tests for sampler."""
 
-
+# import numpy as np
 import tensorflow as tf
-
-# from absl import logging
+from absl.testing import parameterized
 from tensorflow import keras
 
-# from keras_nlp.api_export import keras_nlp_export
-from keras_nlp.samplers.sampler import Sampler
-from keras_nlp.samplers.sampler import base_sampler_args_docstring
-from keras_nlp.samplers.sampler import call_args_docstring
-from keras_nlp.utils.python_utils import format_docstring
+from keras_nlp.samplers import ContrastiveSampler
 
 
-@format_docstring(
-    base_sampler_args=base_sampler_args_docstring, call_args=call_args_docstring
-)
-@keras.utils.register_keras_serializable(package="keras_nlp")
-class ContrastiveSampler(Sampler):
-    """Contrastive Sampler class.
+class ContrastiveSamplerTest(tf.test.TestCase, parameterized.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.vocab_size = 10
+        self.feature_size = 16
 
-    {Add docstring here}
-    """
-
-    def __init__(
-        self,
-        k=5,
-        seed=None,
-        jit_compile=True,
-        run_eagerly=False,
-    ):
-        self.k = k
-        self.seed = seed
-        super().__init__(jit_compile=jit_compile, run_eagerly=run_eagerly)
-
-    def get_next_token(self, next_token_probs):
-        pass
-
-    def sample(
-        self,
-        prompt,
-        token_probability_fn,
-        ask,
-        batch_size,
-        num_steps,
-        current_index,
-        from_logits=True,
-    ):
-        batch_size, max_length = tf.shape(prompt)[0], tf.shape(prompt)[1]
-        max_length = tf.cast(max_length, num_steps.dtype)
-        # The index of the last non-padding token in prompt. Since all sequences
-        # are aligned to the right side, the index is the same for all.
-        current_index = max_length - num_steps
-        return batch_size, current_index
-
-        def one_step(current_index, prompt, mask):
-            #################################################################
-            # 1. Get top-k tokens along with their probibility and representation.
-            #
-            # Your code goes here!
-            #################################################################
-
-            #################################################################
-            # 2. Compute the penalty for each token in the top-k selection.
-            #
-            # Your code goes here!
-            #################################################################
-
-            #################################################################
-            # 3. Update the corresponding index and mask.
-            #
-            # Your code goes here!
-            #################################################################
-
-            # Run a while loop till `max_length` of tokens has been generated.
-            current_index, prompt, mask = tf.while_loop(
-                cond=lambda current_index, prompt, mask: tf.less(
-                    current_index, max_length
+        # Create a dummy model to predict the next token.
+        model = keras.Sequential(
+            [
+                keras.Input(shape=[None]),
+                keras.layers.Embedding(
+                    input_dim=self.vocab_size,
+                    output_dim=self.feature_size,
                 ),
-                body=one_step,
-                loop_vars=(current_index, prompt, mask),
-            )
-            return prompt
+                keras.layers.Dense(self.vocab_size),
+                keras.layers.Softmax(),
+            ]
+        )
 
-        def get_config(self):
-            config = super().get_config()
+        def token_probability_fn(inputs, mask):
+            return model(inputs)
 
-            config.update({"num_beams": self.num_beams})
-            return config
+        self.token_probability_fn = token_probability_fn
+        self.sampler = ContrastiveSampler(k=2)
