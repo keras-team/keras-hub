dd1c1f14a6b29a4d617c9607357f61165e7d5ba8
Somasree Majumder <56045049+soma2000-lang@users.noreply.github.com>
2023-03-20 15:19:53 +0530

    Update contrastive_search_sampler.py

--- keras_nlp/samplers/contrastive_search_sampler.py
+++ keras_nlp/samplers/contrastive_search_sampler.py
@@ -11,37 +11,136 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for Top-K sampler."""
 
-# import numpy as np
+
 import tensorflow as tf
-from absl.testing import parameterized
+
+# from absl import logging
 from tensorflow import keras
 
-from keras_nlp.samplers import ContrastiveSampler
+# from keras_nlp.api_export import keras_nlp_export
+from keras_nlp.samplers.sampler import Sampler
+from keras_nlp.samplers.sampler import base_sampler_args_docstring
+from keras_nlp.samplers.sampler import call_args_docstring
+from keras_nlp.utils.python_utils import format_docstring
 
 
-class ContrastiveSamplerTest(tf.test.TestCase, parameterized.TestCase):
-    def setUp(self):
-        super().setUp()
-        self.vocab_size = 10
-        self.feature_size = 16
+@format_docstring(
+    base_sampler_args=base_sampler_args_docstring, call_args=call_args_docstring
+)
+@keras.utils.register_keras_serializable(package="keras_nlp")
+class ContrastiveSampler(Sampler):
+    """Contrastive Sampler class.
 
-        # Create a dummy model to predict the next token.
-        model = keras.Sequential(
-            [
-                keras.Input(shape=[None]),
-                keras.layers.Embedding(
-                    input_dim=self.vocab_size,
-                    output_dim=self.feature_size,
+    Contrastive sampler is a variant of top-k sampler,it randomly selects a token from the
+    tokens of top K probability, with  selection chance determined by the probability.
+    adds a penalty based on max similarity with previously seen tokens.
+
+      Args:
+          num_beams: int. The number of beams that should be kept at each
+              time-step. `num_beams` should be strictly positive.
+          {{base_sampler_args}}
+
+      Call Args:
+          {{call_args}}
+
+      Examples:
+      ```python
+      VOCAB_SIZE = 10
+
+      # Create a dummy model to predict the next token.
+      model = keras.Sequential(
+          [
+              keras.Input(shape=[None]),
+              keras.layers.Embedding(
+                  input_dim=VOCAB_SIZE,
+                  output_dim=16,
+              ),
+              keras.layers.Dense(VOCAB_SIZE, activation="softmax"),
+          ]
+      )
+
+      # Define a function that outputs the next token's probability for each token
+      # in the input sequence.
+      def token_probability_fn(inputs, mask):
+          return model(inputs)
+
+      prompt = tf.fill((8, 1), 1)
+
+      sampler = keras_nlp.samplers.BeamSampler(num_beams=3)
+      # Print the generated sequence (token ids).
+      print(sampler(prompt, token_probability_fn, max_length=10))
+      ```
+    """
+
+    def __init__(
+        self,
+        k=5,
+        seed=None,
+        jit_compile=True,
+        run_eagerly=False,
+    ):
+        self.k = k
+        self.seed = seed
+        super().__init__(jit_compile=jit_compile, run_eagerly=run_eagerly)
+
+    def get_next_token(self, next_token_probs):
+        pass
+
+    def sample(
+        self,
+        prompt,
+        token_probability_fn,
+        ask,
+        batch_size,
+        num_steps,
+        current_index,
+        from_logits=True,
+    ):
+        batch_size, max_length = tf.shape(prompt)[0], tf.shape(prompt)[1]
+        max_length = tf.cast(max_length, num_steps.dtype)
+        # The index of the last non-padding token in prompt. Since all sequences
+        # are aligned to the right side, the index is the same for all.
+        current_index = max_length - num_steps
+        return batch_size, current_index
+
+        def one_step(current_index, prompt, mask):
+            #################################################################
+            # 1. Get top-k tokens along with their probibility and representation.
+            #
+            top_k_pred, top_k_indices = tf.math.top_k(
+                next_token_probs, k=self.k, sorted=False
+            )
+            # Your code goes here!
+            #################################################################
+            next_token = tf.random.categorical(
+                tf.math.log(top_k_pred), 1, seed=self.seed
+            )
+
+            #################################################################
+            # 2. Compute the penalty for each token in the top-k selection.
+            #
+            # Your code goes here!
+            #################################################################
+
+            #################################################################
+            # 3. Update the corresponding index and mask.
+            #
+            # Your code goes here!
+            #################################################################
+
+            # Run a while loop till `max_length` of tokens has been generated.
+            current_index, prompt, mask = tf.while_loop(
+                cond=lambda current_index, prompt, mask: tf.less(
+                    current_index, max_length
                 ),
-                keras.layers.Dense(self.vocab_size),
-                keras.layers.Softmax(),
-            ]
-        )
+                body=one_step,
+                loop_vars=(current_index, prompt, mask),
+            )
+            return prompt
 
-        def token_probability_fn(inputs, mask):
-            return model(inputs)
+        def get_config(self):
+            config = super().get_config()
 
-        self.token_probability_fn = token_probability_fn
-        self.sampler = ContrastiveSampler(k=2)
+            config.update({"num_beams": self.num_beams})
+            return config
