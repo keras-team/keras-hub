5c0faeb1fbda87a079faab2fe68e7e2cc314b1cf
Anshuman Mishra <51750587+shivance@users.noreply.github.com>
2023-03-22 05:31:10 +0530

    Speed up default RoBERTa testing roughly 3x (#897)
    * initial commit for roberta test improvement :octocat:
    
    * fixing up errors; wrapping up
    
    * fixing save model test

--- keras_nlp/models/roberta/roberta_masked_lm_preprocessor_test.py
+++ keras_nlp/models/roberta/roberta_masked_lm_preprocessor_test.py
@@ -16,6 +16,7 @@
 
 import os
 
+import pytest
 import tensorflow as tf
 from absl.testing import parameterized
 from tensorflow import keras
@@ -140,10 +141,19 @@ class RobertaMaskedLMPreprocessorTest(tf.test.TestCase, parameterized.TestCase):
         self.assertAllEqual(y, [0, 0, 0, 0, 0])
         self.assertAllEqual(sw, [0.0, 0.0, 0.0, 0.0, 0.0])
 
+    def test_serialization(self):
+        config = keras.utils.serialize_keras_object(self.preprocessor)
+        new_preprocessor = keras.utils.deserialize_keras_object(config)
+        self.assertEqual(
+            new_preprocessor.get_config(),
+            self.preprocessor.get_config(),
+        )
+
     @parameterized.named_parameters(
         ("tf_format", "tf", "model"),
         ("keras_format", "keras_v3", "model.keras"),
     )
+    @pytest.mark.large  # Saving is slow, so mark these large.
     def test_saved_model(self, save_format, filename):
         input_data = tf.constant([" airplane at airport"])
 
