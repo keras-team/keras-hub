71373162a794f83c99a2c7af8d9d984ad2b1bae5
Matt Watson <mattdangerw@gmail.com>
2023-03-27 13:28:08 -0700

    Update generate to accept datasets
    This updates generate to feel more like fit/predict/evaluate.
    
    Inputs to generate can be a dataset, or raw tensors.
    Inputs can be preprocessed or not depending on if the model has a
    preprocessor layer attached.
    
    The preprocessing layer is used to preprocess all inputs before
    generation.

--- keras_nlp/models/gpt2/gpt2_causal_lm_preprocessor_test.py
+++ keras_nlp/models/gpt2/gpt2_causal_lm_preprocessor_test.py
@@ -16,6 +16,7 @@
 
 import os
 
+import pytest
 import tensorflow as tf
 from absl.testing import parameterized
 from tensorflow import keras
@@ -29,25 +30,18 @@ from keras_nlp.models.gpt2.gpt2_tokenizer import GPT2Tokenizer
 class GPT2CausalLMPreprocessorTest(tf.test.TestCase, parameterized.TestCase):
     def setUp(self):
         self.vocab = {
-            "<|endoftext|>": 0,
-            "!": 1,
-            "air": 2,
-            "Ġair": 3,
-            "plane": 4,
-            "Ġat": 5,
-            "port": 6,
-            "Ġkoh": 7,
-            "li": 8,
-            "Ġis": 9,
-            "Ġthe": 10,
-            "Ġbest": 11,
+            "!": 0,
+            "air": 1,
+            "Ġair": 2,
+            "plane": 3,
+            "Ġat": 4,
+            "port": 5,
+            "<|endoftext|>": 6,
         }
 
-        merges = ["Ġ a", "Ġ t", "Ġ k", "Ġ i", "Ġ b", "a i", "p l", "n e"]
-        merges += ["Ġa t", "p o", "r t", "o h", "l i", "Ġi s", "Ġb e", "s t"]
-        merges += ["Ġt h", "ai r", "pl a", "Ġk oh", "Ġth e", "Ġbe st", "po rt"]
-        merges += ["Ġai r", "Ġa i", "pla ne"]
-        self.merges = merges
+        self.merges = ["Ġ a", "Ġ t", "Ġ i", "Ġ b", "a i", "p l", "n e"]
+        self.merges += ["Ġa t", "p o", "r t", "Ġt h", "ai r", "pl a", "po rt"]
+        self.merges += ["Ġai r", "Ġa i", "pla ne"]
 
         self.preprocessor = GPT2CausalLMPreprocessor(
             tokenizer=GPT2Tokenizer(
@@ -57,28 +51,25 @@ class GPT2CausalLMPreprocessorTest(tf.test.TestCase, parameterized.TestCase):
             sequence_length=8,
         )
 
-    def test_tokenize_strings(self):
+    def test_strings(self):
         input_data = "airplane at airport"
 
         x, y, sw = self.preprocessor(input_data)
-        self.assertAllEqual(x["token_ids"], [2, 4, 5, 3, 6, 0, 0])
-        self.assertAllEqual(x["padding_mask"], [1, 1, 1, 1, 1, 0, 0])
-        self.assertAllEqual(y, [4, 5, 3, 6, 0, 0, 0])
-        self.assertAllEqual(sw, [1, 1, 1, 1, 0, 0, 0])
+        self.assertAllEqual(x["token_ids"], [6, 1, 3, 4, 2, 5, 6, 0])
+        self.assertAllEqual(x["padding_mask"], [1, 1, 1, 1, 1, 1, 1, 0])
+        self.assertAllEqual(y, [1, 3, 4, 2, 5, 6, 0, 0])
+        self.assertAllEqual(sw, [1, 1, 1, 1, 1, 1, 0, 0])
 
-    def test_tokenize_list_of_strings(self):
+    def test_list_of_strings(self):
         input_data = ["airplane at airport"] * 4
 
         x, y, sw = self.preprocessor(input_data)
-        self.assertAllEqual(
-            x["token_ids"],
-            [[2, 4, 5, 3, 6, 0, 0]] * 4,
-        )
-        self.assertAllEqual(x["padding_mask"], [[1, 1, 1, 1, 1, 0, 0]] * 4)
-        self.assertAllEqual(y, [[4, 5, 3, 6, 0, 0, 0]] * 4)
-        self.assertAllEqual(sw, [[1, 1, 1, 1, 0, 0, 0]] * 4)
+        self.assertAllEqual(x["token_ids"], [[6, 1, 3, 4, 2, 5, 6, 0]] * 4)
+        self.assertAllEqual(x["padding_mask"], [[1, 1, 1, 1, 1, 1, 1, 0]] * 4)
+        self.assertAllEqual(y, [[1, 3, 4, 2, 5, 6, 0, 0]] * 4)
+        self.assertAllEqual(sw, [[1, 1, 1, 1, 1, 1, 0, 0]] * 4)
 
-    def test_pad_start_end_token(self):
+    def test_no_start_end_token(self):
         input_data = ["airplane at airport"] * 4
 
         preprocessor = GPT2CausalLMPreprocessor(
@@ -87,51 +78,59 @@ class GPT2CausalLMPreprocessorTest(tf.test.TestCase, parameterized.TestCase):
                 merges=self.merges,
             ),
             sequence_length=8,
-            add_start_token=True,
-            add_end_token=True,
+            add_start_token=False,
+            add_end_token=False,
         )
         x, y, sw = preprocessor(input_data)
-        self.assertAllEqual(
-            x["token_ids"],
-            [[0, 2, 4, 5, 3, 6, 0]] * 4,
-        )
-        self.assertAllEqual(x["padding_mask"], [[1, 1, 1, 1, 1, 1, 1]] * 4)
-        self.assertAllEqual(y, [[2, 4, 5, 3, 6, 0, 0]] * 4)
-        self.assertAllEqual(sw, [[1, 1, 1, 1, 1, 1, 0]] * 4)
+        self.assertAllEqual(x["token_ids"], [[1, 3, 4, 2, 5, 0, 0, 0]] * 4)
+        self.assertAllEqual(x["padding_mask"], [[1, 1, 1, 1, 1, 0, 0, 0]] * 4)
+        self.assertAllEqual(y, [[3, 4, 2, 5, 0, 0, 0, 0]] * 4)
+        self.assertAllEqual(sw, [[1, 1, 1, 1, 0, 0, 0, 0]] * 4)
 
-    def test_tokenize_labeled_batch(self):
+    def test_labeled_batch(self):
         x = tf.constant(["airplane at airport"] * 4)
-        y = tf.constant([1] * 4)
-        sw = tf.constant([1.0] * 4)
+        y = tf.constant([1] * 4)  # Ignored.
+        sw = tf.constant([1.0] * 4)  # Ignored.
         x, y, sw = self.preprocessor(x, y, sw)
-        self.assertAllEqual(
-            x["token_ids"],
-            [[2, 4, 5, 3, 6, 0, 0]] * 4,
-        )
-        self.assertAllEqual(x["padding_mask"], [[1, 1, 1, 1, 1, 0, 0]] * 4)
-        self.assertAllEqual(y, [[4, 5, 3, 6, 0, 0, 0]] * 4)
-        self.assertAllEqual(sw, [[1, 1, 1, 1, 0, 0, 0]] * 4)
+        self.assertAllEqual(x["token_ids"], [[6, 1, 3, 4, 2, 5, 6, 0]] * 4)
+        self.assertAllEqual(x["padding_mask"], [[1, 1, 1, 1, 1, 1, 1, 0]] * 4)
+        self.assertAllEqual(y, [[1, 3, 4, 2, 5, 6, 0, 0]] * 4)
+        self.assertAllEqual(sw, [[1, 1, 1, 1, 1, 1, 0, 0]] * 4)
 
-    def test_tokenize_labeled_dataset(self):
+    def test_dataset(self):
         x = tf.constant(["airplane at airport"] * 4)
-        y = tf.constant([1] * 4)
-        sw = tf.constant([1.0] * 4)
-        ds = tf.data.Dataset.from_tensor_slices((x, y, sw))
+        ds = tf.data.Dataset.from_tensor_slices(x)
         ds = ds.map(self.preprocessor)
         x, y, sw = ds.batch(4).take(1).get_single_element()
+        self.assertAllEqual(x["token_ids"], [[6, 1, 3, 4, 2, 5, 6, 0]] * 4)
+        self.assertAllEqual(x["padding_mask"], [[1, 1, 1, 1, 1, 1, 1, 0]] * 4)
+        self.assertAllEqual(y, [[1, 3, 4, 2, 5, 6, 0, 0]] * 4)
+        self.assertAllEqual(sw, [[1, 1, 1, 1, 1, 1, 0, 0]] * 4)
 
-        self.assertAllEqual(
-            x["token_ids"],
-            [[2, 4, 5, 3, 6, 0, 0]] * 4,
+    def test_call_overrides(self):
+        input_data = "airplane at airport"
+        x, _, _ = self.preprocessor(input_data, add_start_token=False)
+        self.assertAllEqual(x["token_ids"], [1, 3, 4, 2, 5, 6, 0, 0])
+        x, _, _ = self.preprocessor(input_data, add_end_token=False)
+        self.assertAllEqual(x["token_ids"], [6, 1, 3, 4, 2, 5, 0, 0])
+        x, _, _ = self.preprocessor(input_data, sequence_length=4)
+        self.assertAllEqual(x["token_ids"], [6, 1, 3, 4])
+        x = self.preprocessor(input_data, return_labels=False)
+        self.assertAllEqual(x["token_ids"], [6, 1, 3, 4, 2, 5, 6, 0])
+
+    def test_serialization(self):
+        config = keras.utils.serialize_keras_object(self.preprocessor)
+        new_preprocessor = keras.utils.deserialize_keras_object(config)
+        self.assertEqual(
+            new_preprocessor.get_config(),
+            self.preprocessor.get_config(),
         )
-        self.assertAllEqual(x["padding_mask"], [[1, 1, 1, 1, 1, 0, 0]] * 4)
-        self.assertAllEqual(y, [[4, 5, 3, 6, 0, 0, 0]] * 4)
-        self.assertAllEqual(sw, [[1, 1, 1, 1, 0, 0, 0]] * 4)
 
     @parameterized.named_parameters(
         ("tf_format", "tf", "model"),
         ("keras_format", "keras_v3", "model.keras"),
     )
+    @pytest.mark.large
     def test_saved_model(self, save_format, filename):
         input_data = tf.constant(["airplane at airport"])
 
