f05cc0fb468529fd384d9b1c8b7fc6e53afa01cf
chenmoneygithub <chenmoney@google.com>
2023-03-29 08:57:34 +0800

    ensure shape

--- keras_nlp/samplers/sampler.py
+++ keras_nlp/samplers/sampler.py
@@ -115,6 +115,9 @@ class Sampler:
             next_token = self.get_next_token(probabilities)
             # Don't overwrite anywhere mask is True.
             next_token = tf.cast(next_token, prompt.dtype)
+            # Ensure shape is `[None]`, otherwise it causes issues after
+            # converting to TFLite.
+            next_token = tf.ensure_shape(next_token, [None])
             next_token = tf.where(mask[:, index], prompt[:, index], next_token)
             # Update the prompt with the next token.
             next_token = next_token[:, tf.newaxis]
