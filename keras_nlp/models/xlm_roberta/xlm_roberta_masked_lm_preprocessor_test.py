119de4dc3286c6f7a01a65dfb533eec2e486116f
Somasree Majumder <bishnupadamajumder32@gmail.com>
2023-04-01 05:28:29 +0530

    Update xlm_roberta_masked_lm_preprocessor_test.py

--- keras_nlp/models/xlm_roberta/xlm_roberta_masked_lm_preprocessor_test.py
+++ keras_nlp/models/xlm_roberta/xlm_roberta_masked_lm_preprocessor_test.py
@@ -1,4 +1,4 @@
-# Copyright 2023 The KerasNLP Authors
+# Copyright 2022 The KerasNLP Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,7 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for  XLMRoBERTa masked language model preprocessor layer."""
+
+"""Tests for XLM-RoBERTa masked language model preprocessor layer."""
+
 import io
 import os
 
@@ -22,22 +24,21 @@ from absl.testing import parameterized
 from tensorflow import keras
 
 from keras_nlp.models.xlm_roberta.xlm_roberta_masked_lm_preprocessor import (
-    XLMRoBERTaMaskedLMPreprocessor,
+    XLMRobertaMaskedLMPreprocessor,
 )
 from keras_nlp.models.xlm_roberta.xlm_roberta_tokenizer import (
     XLMRobertaTokenizer,
 )
 
 
-class XLMRoBERTaMaskedLMPreprocessorTest(
+class XLMRobertaMaskedLMPreprocessorTest(
     tf.test.TestCase, parameterized.TestCase
 ):
     def setUp(self):
+        bytes_io = io.BytesIO()
         vocab_data = tf.data.Dataset.from_tensor_slices(
             ["the quick brown fox", "the earth is round"]
         )
-
-        bytes_io = io.BytesIO()
         sentencepiece.SentencePieceTrainer.train(
             sentence_iterator=vocab_data.as_numpy_iterator(),
             model_writer=bytes_io,
@@ -49,102 +50,100 @@ class XLMRoBERTaMaskedLMPreprocessorTest(
             eos_id=3,
             pad_piece="<pad>",
             unk_piece="<unk>",
-            bos_piece="</s>",
-            eos_piece="<s>",
+            bos_piece="<s>",
+            eos_piece="</s>",
             user_defined_symbols="[MASK]",
         )
-
         self.proto = bytes_io.getvalue()
 
         self.tokenizer = XLMRobertaTokenizer(proto=self.proto)
-
-        self.preprocessor = XLMRoBERTaMaskedLMPreprocessor(
+        self.preprocessor = XLMRobertaMaskedLMPreprocessor(
             tokenizer=self.tokenizer,
             # Simplify out testing by masking every available token.
             mask_selection_rate=1.0,
             mask_token_rate=1.0,
             random_token_rate=0.0,
-            mask_selection_length=4,
+            mask_selection_length=5,
             sequence_length=12,
         )
 
     def test_preprocess_strings(self):
-        input_data = "the quick brown fox"
+        input_data = " brown fox quick"
 
         x, y, sw = self.preprocessor(input_data)
         self.assertAllEqual(
-            x["token_ids"], [1, 4, 4, 4, 4, 2, 0, 0, 0, 0, 0, 0]
+            x["token_ids"], [0, 4, 4, 4, 2, 1, 1, 1, 1, 1, 1, 1]
         )
         self.assertAllEqual(
-            x["padding_mask"], [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+            x["padding_mask"], [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]
         )
-        self.assertAllEqual(x["mask_positions"], [1, 2, 3, 4])
-        self.assertAllEqual(y, [5, 10, 6, 8])
-        self.assertAllEqual(sw, [1.0, 1.0, 1.0, 1.0])
+        self.assertAllEqual(x["mask_positions"], [1, 2, 3, 0, 0])
+        self.assertAllEqual(y, [7, 9, 11, 0, 0])
+        self.assertAllEqual(sw, [1.0, 1.0, 1.0, 0.0, 0.0])
 
     def test_preprocess_list_of_strings(self):
-        input_data = ["the quick brown fox"] * 4
+        input_data = [" brown fox quick"] * 4
 
         x, y, sw = self.preprocessor(input_data)
         self.assertAllEqual(
-            x["token_ids"], [[1, 4, 4, 4, 4, 2, 0, 0, 0, 0, 0, 0]] * 4
+            x["token_ids"], [[0, 4, 4, 4, 2, 1, 1, 1, 1, 1, 1, 1]] * 4
         )
         self.assertAllEqual(
-            x["padding_mask"], [[1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0]] * 4
+            x["padding_mask"], [[1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]] * 4
         )
-        self.assertAllEqual(x["mask_positions"], [[1, 2, 3, 4]] * 4)
-        self.assertAllEqual(y, [[5, 10, 6, 8]] * 4)
-        self.assertAllEqual(sw, [[1.0, 1.0, 1.0, 1.0]] * 4)
+        self.assertAllEqual(x["mask_positions"], [[1, 2, 3, 0, 0]] * 4)
+        self.assertAllEqual(y, [[7, 9, 11, 0, 0]] * 4)
+        self.assertAllEqual(sw, [[1.0, 1.0, 1.0, 0.0, 0.0]] * 4)
 
     def test_preprocess_dataset(self):
-        sentences = tf.constant(["the quick brown fox"] * 4)
+        sentences = tf.constant([" brown fox quick"] * 4)
         ds = tf.data.Dataset.from_tensor_slices(sentences)
         ds = ds.map(self.preprocessor)
         x, y, sw = ds.batch(4).take(1).get_single_element()
         self.assertAllEqual(
-            x["token_ids"], [[1, 4, 4, 4, 4, 2, 0, 0, 0, 0, 0, 0]] * 4
+            x["token_ids"], [[0, 4, 4, 4, 2, 1, 1, 1, 1, 1, 1, 1]] * 4
         )
         self.assertAllEqual(
-            x["padding_mask"], [[1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0]] * 4
+            x["padding_mask"], [[1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]] * 4
         )
-        self.assertAllEqual(x["mask_positions"], [[1, 2, 3, 4]] * 4)
-        self.assertAllEqual(y, [[5, 10, 6, 8]] * 4)
-        self.assertAllEqual(sw, [[1.0, 1.0, 1.0, 1.0]] * 4)
+        self.assertAllEqual(x["mask_positions"], [[1, 2, 3, 0, 0]] * 4)
+        self.assertAllEqual(y, [[7, 9, 11, 0, 0]] * 4)
+        self.assertAllEqual(sw, [[1.0, 1.0, 1.0, 0.0, 0.0]] * 4)
 
     def test_mask_multiple_sentences(self):
-        sentence_one = tf.constant("the quick")
-        sentence_two = tf.constant("brown fox")
+        sentence_one = tf.constant(" airplane")
+        sentence_two = tf.constant(" round")
 
         x, y, sw = self.preprocessor((sentence_one, sentence_two))
         self.assertAllEqual(
-            x["token_ids"], [1, 4, 4, 2, 4, 4, 2, 0, 0, 0, 0, 0]
-        )
-        self.assertAllEqual(
-            x["padding_mask"], [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
-        )
-        self.assertAllEqual(x["mask_positions"], [1, 2, 4, 5])
-        self.assertAllEqual(y, [5, 10, 6, 8])
-        self.assertAllEqual(sw, [1.0, 1.0, 1.0, 1.0])
-
-    def test_no_masking_zero_rate(self):
-        no_mask_preprocessor = XLMRoBERTaMaskedLMPreprocessor(
-            self.preprocessor.tokenizer,
-            mask_selection_rate=0.0,
-            mask_selection_length=4,
-            sequence_length=12,
-        )
-        input_data = "the quick brown fox"
-
-        x, y, sw = no_mask_preprocessor(input_data)
-        self.assertAllEqual(
-            x["token_ids"], [1, 5, 10, 6, 8, 2, 0, 0, 0, 0, 0, 0]
+            x["token_ids"], [0, 2, 2, 2, 4, 2, 1, 1, 1, 1, 1, 1]
         )
         self.assertAllEqual(
             x["padding_mask"], [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
         )
-        self.assertAllEqual(x["mask_positions"], [0, 0, 0, 0])
-        self.assertAllEqual(y, [0, 0, 0, 0])
-        self.assertAllEqual(sw, [0.0, 0.0, 0.0, 0.0])
+        self.assertAllEqual(x["mask_positions"], [4, 0, 0, 0, 0])
+        self.assertAllEqual(y, [12, 0, 0, 0, 0])
+        self.assertAllEqual(sw, [1.0, 0.0, 0.0, 0.0, 0.0])
+
+    def test_no_masking_zero_rate(self):
+        no_mask_preprocessor = XLMRobertaMaskedLMPreprocessor(
+            self.preprocessor.tokenizer,
+            mask_selection_rate=0.0,
+            mask_selection_length=5,
+            sequence_length=12,
+        )
+        input_data = " quick brown fox"
+
+        x, y, sw = no_mask_preprocessor(input_data)
+        self.assertAllEqual(
+            x["token_ids"], [0, 11, 7, 9, 2, 1, 1, 1, 1, 1, 1, 1]
+        )
+        self.assertAllEqual(
+            x["padding_mask"], [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+        )
+        self.assertAllEqual(x["mask_positions"], [0, 0, 0, 0, 0])
+        self.assertAllEqual(y, [0, 0, 0, 0, 0])
+        self.assertAllEqual(sw, [0.0, 0.0, 0.0, 0.0, 0.0])
 
     def test_serialization(self):
         config = keras.utils.serialize_keras_object(self.preprocessor)
@@ -160,7 +159,7 @@ class XLMRoBERTaMaskedLMPreprocessorTest(
     )
     @pytest.mark.large
     def test_saved_model(self, save_format, filename):
-        input_data = tf.constant(["the quick brown fox"])
+        input_data = tf.constant([" quick brown fox"])
 
         inputs = keras.Input(dtype="string", shape=())
         outputs = self.preprocessor(inputs)
