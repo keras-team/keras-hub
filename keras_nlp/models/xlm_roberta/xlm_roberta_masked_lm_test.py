c800eca65eca7839471537bd3df332cd964c7316
Somasree Majumder <bishnupadamajumder32@gmail.com>
2023-04-01 05:31:47 +0530

    Update xlm_roberta_masked_lm_test.py

--- keras_nlp/models/xlm_roberta/xlm_roberta_masked_lm_test.py
+++ keras_nlp/models/xlm_roberta/xlm_roberta_masked_lm_test.py
@@ -1,4 +1,4 @@
-# Copyright 2023 The KerasNLP Authors
+# Copyright 2022 The KerasNLP Authors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,7 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""Tests for XLMRobertaMaskedLM masked language model."""
+"""Tests for XLM-RoBERTa masked language model."""
+
 import io
 import os
 
@@ -26,7 +27,7 @@ from keras_nlp.models.xlm_roberta.xlm_roberta_masked_lm import (
     XLMRobertaMaskedLM,
 )
 from keras_nlp.models.xlm_roberta.xlm_roberta_masked_lm_preprocessor import (
-    XLMRoBERTaMaskedLMPreprocessor,
+    XLMRobertaMaskedLMPreprocessor,
 )
 from keras_nlp.models.xlm_roberta.xlm_roberta_tokenizer import (
     XLMRobertaTokenizer,
@@ -37,30 +38,31 @@ class XLMRobertaMaskedLMTest(tf.test.TestCase, parameterized.TestCase):
     def setUp(self):
         bytes_io = io.BytesIO()
         vocab_data = tf.data.Dataset.from_tensor_slices(
-            ["the quick brown fox", "the earth is round", "an eagle flew"]
+            ["the quick brown fox", "the slow brown fox"]
         )
-
         sentencepiece.SentencePieceTrainer.train(
             sentence_iterator=vocab_data.as_numpy_iterator(),
             model_writer=bytes_io,
-            vocab_size=15,
+            vocab_size=5,
             model_type="WORD",
             pad_id=0,
-            bos_id=1,
-            eos_id=2,
-            unk_id=3,
-            pad_piece="[PAD]",
-            bos_piece="</s>",
-            eos_piece="<s>",
-            unk_piece="[UNK]",
+            unk_id=1,
+            bos_id=2,
+            eos_id=3,
+            pad_piece="<pad>",
+            unk_piece="<unk>",
+            bos_piece="<s>",
+            eos_piece="</s>",
             user_defined_symbols="[MASK]",
         )
-        proto = bytes_io.getvalue()
-        self.preprocessor = XLMRoBERTaMaskedLMPreprocessor(
-            XLMRobertaTokenizer(proto=proto),
-            mask_selection_length=2,
+        self.proto = bytes_io.getvalue()
+
+        self.preprocessor = XLMRobertaMaskedLMPreprocessor(
+            XLMRobertaTokenizer(proto=self.proto),
             sequence_length=5,
+            mask_selection_length=2,
         )
+
         self.backbone = XLMRobertaBackbone(
             vocabulary_size=self.preprocessor.tokenizer.vocabulary_size(),
             num_layers=2,
@@ -69,30 +71,28 @@ class XLMRobertaMaskedLMTest(tf.test.TestCase, parameterized.TestCase):
             intermediate_dim=4,
             max_sequence_length=self.preprocessor.packer.sequence_length,
         )
+
         self.masked_lm = XLMRobertaMaskedLM(
             self.backbone,
             preprocessor=self.preprocessor,
         )
 
         self.raw_batch = tf.constant(
-            [
-                "the quick brown fox.",
-                "the eagle flew over fox.",
-            ]
+            ["the quick brown fox", "the slow brown fox"]
         )
-        self.preprocessed_batch = self.preprocessor(self.raw_batch)
+        self.preprocessed_batch = self.preprocessor(self.raw_batch)[0]
         self.raw_dataset = tf.data.Dataset.from_tensor_slices(
             self.raw_batch
         ).batch(2)
         self.preprocessed_dataset = self.raw_dataset.map(self.preprocessor)
 
-    def test_valid_call_classifier(self):
-        self.masked_lm(self.preprocessed_batch[0])
+    def test_valid_call_masked_lm(self):
+        self.masked_lm(self.preprocessed_batch)
 
     def test_classifier_predict(self):
-        self.masked_lm.predict(self.raw_batch)
+        # self.masked_lm.predict(self.raw_batch)
         self.masked_lm.preprocessor = None
-        self.masked_lm.predict(self.preprocessed_batch[0])
+        self.masked_lm.predict(self.preprocessed_batch)
 
     def test_classifier_fit(self):
         self.masked_lm.fit(self.raw_dataset)
@@ -121,7 +121,6 @@ class XLMRobertaMaskedLMTest(tf.test.TestCase, parameterized.TestCase):
     )
     @pytest.mark.large
     def test_saved_model(self, save_format, filename):
-        model_output = self.masked_lm.predict(self.raw_batch)
         save_path = os.path.join(self.get_temp_dir(), filename)
         self.masked_lm.save(save_path, save_format=save_format)
         restored_model = keras.models.load_model(save_path)
@@ -129,6 +128,7 @@ class XLMRobertaMaskedLMTest(tf.test.TestCase, parameterized.TestCase):
         # Check we got the real object back.
         self.assertIsInstance(restored_model, XLMRobertaMaskedLM)
 
-        # Check that output matches.
-        restored_output = restored_model.predict(self.raw_batch)
+        model_output = self.masked_lm(self.preprocessed_batch)
+        restored_output = restored_model(self.preprocessed_batch)
+
         self.assertAllClose(model_output, restored_output)
