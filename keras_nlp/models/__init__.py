1a67fe020544e8fd7b30948bd3d58f22a33be5d0
abheesht17 <sharmabhee@gmail.com>
2023-04-02 01:00:35 +0530

    Fix import

--- keras_nlp/models/__init__.py
+++ keras_nlp/models/__init__.py
@@ -22,8 +22,8 @@ from keras_nlp.models.albert.albert_preprocessor import AlbertPreprocessor
 from keras_nlp.models.albert.albert_tokenizer import AlbertTokenizer
 from keras_nlp.models.bart.bart_backbone import BartBackbone
 from keras_nlp.models.bart.bart_preprocessor import BartPreprocessor
-from keras_nlp.models.bart.bart_seq_2_seq_preprocessor import (
-    BartSeq2SeqPreprocessor,
+from keras_nlp.models.bart.bart_seq_2_seq_lm_preprocessor import (
+    BartSeq2SeqLMPreprocessor,
 )
 from keras_nlp.models.bart.bart_tokenizer import BartTokenizer
 from keras_nlp.models.bert.bert_backbone import BertBackbone
