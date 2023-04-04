Fix gpt2, t5 and fnet under mixed precision
Also add a testing mode for mixed precision, though it does not fully
pass yet due to saving and other output errors.

keras_nlp/layers/cached_multi_head_attention.py
@@ -84,7 +84,9 @@ class CachedMultiHeadAttention(keras.layers.MultiHeadAttention):
             value = dynamic_update_slice(value_cache, value, start)
             cache = tf.stack((key, value), axis=1)
 
-        query = tf.multiply(query, 1.0 / tf.math.sqrt(float(self._key_dim)))
+        query = tf.multiply(
+            query, 1.0 / tf.math.sqrt(tf.cast(self._key_dim, query.dtype))
+        )
         attention_scores = tf.einsum(self._dot_product_equation, key, query)
         attention_scores = self._masked_softmax(
             attention_scores, attention_mask

keras_nlp/layers/f_net_encoder.py
@@ -132,9 +132,9 @@ class FNetEncoder(keras.layers.Layer):
             # Apply FFT on the input and take the real part.
             # Before we apply fourier transform, let's convert the dtype of the
             # input tensor to complex64.
-            input = tf.cast(input, tf.complex64)
-            mixing_output = tf.math.real(tf.signal.fft2d(input))
-            return mixing_output
+            x = tf.cast(input, tf.complex64)
+            mixing_output = tf.math.real(tf.signal.fft2d(x))
+            return tf.cast(mixing_output, input.dtype)
 
         def add_and_norm(input1, input2, norm_layer):
             return norm_layer(input1 + input2)

keras_nlp/models/gpt2/gpt2_causal_lm.py
@@ -261,7 +261,7 @@ class GPT2CausalLM(Task):
         num_heads = self.backbone.num_heads
         head_dim = self.backbone.hidden_dim // self.backbone.num_heads
         shape = [batch_size, num_layers, 2, max_length, num_heads, head_dim]
-        cache = tf.zeros(shape)
+        cache = tf.zeros(shape, dtype=self.compute_dtype)
         # Seed the cache.
         _, cache = self.call_with_cache(prompt, cache, 0)
         return cache