f82d3d54f4c2e0e3e39479dab36b2e81e7f0fd2f
Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
2023-04-12 16:14:30 -0700

    Clean up the docker and lint setup (#981)
    * update devcontainer settings
    
    * clean up the lint and format setup
    
    * addressing comments

--- shell/format.sh
+++ shell/format.sh
@@ -3,8 +3,8 @@
 base_dir=$(dirname $(dirname $0))
 targets="${base_dir}/*.py ${base_dir}/examples/ ${base_dir}/keras_nlp/ ${base_dir}/tools/"
 
-isort --sp "${base_dir}/setup.cfg" --sl ${targets}
-black --line-length 80 ${targets}
+isort --sp "${base_dir}/pyproject.toml" ${targets}
+black --config "${base_dir}/pyproject.toml" ${targets}
 
 for i in $(find ${targets} -name '*.py'); do
   if ! grep -q Copyright $i; then
@@ -13,4 +13,4 @@ for i in $(find ${targets} -name '*.py'); do
   fi
 done
 
-flake8 --config "${base_dir}/setup.cfg" --max-line-length=200 ${targets}
+flake8 --config "${base_dir}/setup.cfg" ${targets}
