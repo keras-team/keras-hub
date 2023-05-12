3cf041b248100ea7b4c07fef522ca379946ae26d
Abheesht Sharma <sharmabhee@gmail.com>
2023-05-12 23:11:39 +0530

    Small comment edit

--- keras_nlp/models/whisper/whisper_tokenizer.py
+++ keras_nlp/models/whisper/whisper_tokenizer.py
@@ -117,7 +117,7 @@ class WhisperTokenizer(BytePairTokenizer):
             # Multilingual tokenizer.
             # TODO: The pad token for the multilingual tokenizer is actually
             # "", but it errors out (OOM). After BPE is fixed, we can update
-            # this to "". For now, we will use `"<endoftext>"`.
+            # this to "". For now, we will use `"<|endoftext|>"`.
             pad_token = "<|endoftext|>"
             language_tokens = _load_dict(language_tokens)
 
