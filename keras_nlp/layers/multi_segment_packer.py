2ad67dffdf8a49e28997c4a8d592f692421af258
Abheesht Sharma <sharmabhee@gmail.com>
2023-05-17 16:09:19 +0530

    NIT

--- keras_nlp/layers/multi_segment_packer.py
+++ keras_nlp/layers/multi_segment_packer.py
@@ -150,7 +150,7 @@ class MultiSegmentPacker(keras.layers.Layer):
         def check_special_value_type(value, value_name):
             if isinstance(value, (int, str)):
                 return [value]
-            elif value and not isinstance(value, (list, tuple)):
+            if value and not isinstance(value, (list, tuple)):
                 raise ValueError(
                     f"{value_name} should be of type int/str/list/tuple."
                     f"Received type: `{type(value)}`."
