6af824623b5b9e6a8e537224089d313d0ac16bc0
Anshuman Mishra <51750587+shivance@users.noreply.github.com>
2023-07-06 22:31:21 +0530

    Refactor `RotaryEmbedding` and `GPTNeoXAttention` (#1101)
    * fix rotary emb
    
    * refactor + remove unnecessary typecast
    
    * fix formatting
    
    * refactor
    
    * formatting fix
    
    * refactoring rotary emb
    
    * added a kwarg in super().__init__()

--- keras_nlp/layers/rotary_embedding.py
+++ keras_nlp/layers/rotary_embedding.py
@@ -16,6 +16,8 @@ from tensorflow import keras
 
 
 class RotaryEmbedding(keras.layers.Layer):
+    def __init__(self, max_wavelength=10000, **kwargs):
+        super().__init__(**kwargs)
     """Rotary positional encoding layer.
 
         Tbjs layer encodes absolute positional information with rotation matrix and naturally
@@ -62,45 +64,38 @@ class RotaryEmbedding(keras.layers.Layer):
     def _apply_rotary_pos_emb(self, tensor, cos_emb, sin_emb):
         cos_emb = cos_emb[:, : tf.shape(tensor)[1], :, :]
         sin_emb = sin_emb[:, : tf.shape(tensor)[1], :, :]
+
         x1, x2 = tf.split(tensor, 2, axis=-1)
         half_rot_tensor = tf.concat((-x2, x1), axis=-1)
         ret = (tensor * cos_emb) + (half_rot_tensor * sin_emb)
         return ret
 
-    def _compute_cos_sin_embedding(self, x, rotary_ndims, seq_dim=1):
+        return (tensor * cos_emb) + (half_rot_tensor * sin_emb)
+
+    def _compute_cos_sin_embedding(self, x, rotary_dim, seq_dim=1):
         seq_len = tf.shape(x)[seq_dim]
-        rotary_ndims = tf.cast(rotary_ndims, tf.float32)
-        range = tf.range(start=0, limit=rotary_ndims, delta=2, dtype="float32")
-        inverse_freq = 1.0 / (self.max_wavelength ** (range / rotary_ndims))
+        rotary_dim = tf.cast(rotary_dim, "float32")
+
+        range = tf.range(start=0, limit=rotary_dim, delta=2, dtype="float32")
+        inverse_freq = 1.0 / (self.max_wavelength ** (range / rotary_dim))
+
         tensor = tf.range(seq_len, dtype=inverse_freq.dtype)
         freqs = tf.einsum("i, j -> ij", tensor, inverse_freq)
         embedding = tf.concat((freqs, freqs), axis=-1)[None, :, None, :]
+
         return tf.cos(embedding), tf.sin(embedding)
 
     def call(self, query, key):
-        attn_head_size = tf.shape(query)[-1]
-        rotary_ndims = tf.cast(
-            tf.cast(attn_head_size, self.compute_dtype) * self.percentage,
-            tf.int32,
-        )
-
-        query_rot, query_pass = (
-            query[..., :rotary_ndims],
-            query[..., rotary_ndims:],
-        )
-        key_rot, key_pass = (
-            key[..., :rotary_ndims],
-            key[..., rotary_ndims:],
-        )
+        rotary_dim = tf.shape(query)[-1]
 
         cos_emb, sin_emb = self._compute_cos_sin_embedding(
-            key_rot, rotary_ndims, seq_dim=1
+            query, rotary_dim, seq_dim=1
         )
         query_emb = self._apply_rotary_pos_emb(query_rot, cos_emb, sin_emb)
         key_emb = self._apply_rotary_pos_emb(key_rot, cos_emb, sin_emb)
 
-        query = tf.concat((query_emb, query_pass), axis=-1)
-        key = tf.concat((key_emb, key_pass), axis=-1)
+        query = self._apply_rotary_pos_emb(query, cos_emb, sin_emb)
+        key = self._apply_rotary_pos_emb(key, cos_emb, sin_emb)
 
         return query, key
 
@@ -108,7 +103,7 @@ class RotaryEmbedding(keras.layers.Layer):
         config = super().get_config()
         config.update(
             {
-                "percentage": self.percentage,
+                "rotary_percentage": self.rotary_percentage,
                 "max_wavelength": self.max_wavelength,
             }
         )
