1f12fe1f37c8df442af0f2bae1deedcebe005cfd
Abheesht <sharmabhee@gmail.com>
2023-07-18 02:06:58 +0530

    Port DeBERTa to multi-backend (#1155)
    * Port DeBERTa to multi-backend
    
    * Fix ops.where
    
    * Replace .rank with len()
    
    * Fix attempt (1)

--- keras_nlp/models/deberta_v3/deberta_v3_backbone.py
+++ keras_nlp/models/deberta_v3/deberta_v3_backbone.py
@@ -25,7 +25,6 @@ from keras_nlp.models.deberta_v3.disentangled_attention_encoder import (
 )
 from keras_nlp.models.deberta_v3.relative_embedding import RelativeEmbedding
 from keras_nlp.utils.python_utils import classproperty
-from keras_nlp.utils.tensor_utils import assert_tf_backend
 
 
 def deberta_kernel_initializer(stddev=0.02):
@@ -110,8 +109,6 @@ class DebertaV3Backbone(Backbone):
         bucket_size=256,
         **kwargs,
     ):
-        assert_tf_backend(self.__class__.__name__)
-
         # Inputs
         token_id_input = keras.Input(
             shape=(None,), dtype="int32", name="token_ids"
