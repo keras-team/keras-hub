3b6b352ced49024a9eba55d0f8317e0f5780b8dc
Matt Watson <mattdangerw@gmail.com>
2023-10-16 11:45:22 -0700

    gpt neox consolidate testing

--- keras_nlp/models/gpt_neo_x/gpt_neo_x_tokenizer_test.py
+++ keras_nlp/models/gpt_neo_x/gpt_neo_x_tokenizer_test.py
@@ -12,82 +12,32 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from keras_nlp.backend import keras
 from keras_nlp.models.gpt_neo_x.gpt_neo_x_tokenizer import GPTNeoXTokenizer
 from keras_nlp.tests.test_case import TestCase
 
 
 class GPTNeoXTokenizerTest(TestCase):
     def setUp(self):
-        self.vocab = {
-            "<|endoftext|>": 0,
-            "Ġair": 1,
-            "plane": 2,
-            "Ġat": 3,
-            "port": 4,
-            "Ġkoh": 5,
-            "li": 6,
-            "Ġis": 7,
-            "Ġthe": 8,
-            "Ġbest": 9,
-        }
-        self.merges = ["Ġ a", "Ġ t", "Ġ k", "Ġ i", "Ġ b", "Ġa i", "p l", "n e"]
-        self.merges += [
-            "Ġa t",
-            "p o",
-            "r t",
-            "o h",
-            "l i",
-            "Ġi s",
-            "Ġb e",
-            "s t",
+        self.vocab = ["!", "air", "Ġair", "plane", "Ġat", "port"]
+        self.vocab += ["<|endoftext|>"]
+        self.vocab = dict([(token, i) for i, token in enumerate(self.vocab)])
+        self.merges = ["Ġ a", "Ġ t", "Ġ i", "Ġ b", "a i", "p l", "n e"]
+        self.merges += ["Ġa t", "p o", "r t", "Ġt h", "ai r", "pl a", "po rt"]
+        self.merges += ["Ġai r", "Ġa i", "pla ne"]
+        self.init_kwargs = {"vocabulary": self.vocab, "merges": self.merges}
+        self.input_data = [
+            " airplane at airport<|endoftext|>",
+            " airplane airport",
         ]
-        self.merges += [
-            "Ġt h",
-            "Ġai r",
-            "pl a",
-            "Ġk oh",
-            "Ġth e",
-            "Ġbe st",
-            "po rt",
-        ]
-        self.merges += ["pla ne"]
 
-        self.tokenizer = GPTNeoXTokenizer(
-            vocabulary=self.vocab, merges=self.merges
+    def test_tokenizer_basics(self):
+        self.run_preprocessing_layer_test(
+            cls=GPTNeoXTokenizer,
+            init_kwargs=self.init_kwargs,
+            input_data=self.input_data,
+            expected_output=[[2, 3, 4, 2, 5, 6], [2, 3, 2, 5]],
         )
-
-    def test_tokenize(self):
-        input_data = " airplane at airport"
-        output = self.tokenizer(input_data)
-        self.assertAllEqual(output, [1, 2, 3, 1, 4])
-
-    def test_tokenize_end_token(self):
-        input_data = " airplane at airport<|endoftext|>"
-        output = self.tokenizer(input_data)
-        self.assertAllEqual(output, [1, 2, 3, 1, 4, 0])
-
-    def test_tokenize_batch(self):
-        input_data = [" airplane at airport", " kohli is the best"]
-        output = self.tokenizer(input_data)
-        self.assertAllEqual(output, [[1, 2, 3, 1, 4], [5, 6, 7, 8, 9]])
-
-    def test_detokenize(self):
-        input_tokens = [1, 2, 3, 1, 4]
-        output = self.tokenizer.detokenize(input_tokens)
-        self.assertEqual(output, " airplane at airport")
-
-    def test_vocabulary_size(self):
-        self.assertEqual(self.tokenizer.vocabulary_size(), 10)
 
     def test_errors_missing_special_tokens(self):
         with self.assertRaises(ValueError):
             GPTNeoXTokenizer(vocabulary=["a", "b", "c"], merges=[])
-
-    def test_serialization(self):
-        config = keras.saving.serialize_keras_object(self.tokenizer)
-        new_tokenizer = keras.saving.deserialize_keras_object(config)
-        self.assertEqual(
-            new_tokenizer.get_config(),
-            self.tokenizer.get_config(),
-        )
