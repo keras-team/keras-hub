1f8b6c8b78c07105ed2affc33a766fd833881ebe
Matt Watson <mattdangerw@gmail.com>
2023-10-15 18:53:53 -0700

    opt consolidate testing

--- keras_nlp/models/opt/opt_tokenizer_test.py
+++ keras_nlp/models/opt/opt_tokenizer_test.py
@@ -12,66 +12,51 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from keras_nlp.backend import keras
+import pytest
+
 from keras_nlp.models.opt.opt_tokenizer import OPTTokenizer
 from keras_nlp.tests.test_case import TestCase
 
 
 class OPTTokenizerTest(TestCase):
     def setUp(self):
-        self.vocab = {
-            "<pad>": 0,
-            "</s>": 1,
-            "Ġair": 2,
-            "plane": 3,
-            "Ġat": 4,
-            "port": 5,
-            "Ġkoh": 6,
-            "li": 7,
-            "Ġis": 8,
-            "Ġthe": 9,
-            "Ġbest": 10,
-        }
+        self.vocab = ["<pad>", "</s>", "air", "Ġair", "plane", "Ġat", "port"]
+        self.vocab = dict([(token, i) for i, token in enumerate(self.vocab)])
+        self.merges = ["Ġ a", "Ġ t", "Ġ i", "Ġ b", "a i", "p l", "n e"]
+        self.merges += ["Ġa t", "p o", "r t", "Ġt h", "ai r", "pl a", "po rt"]
+        self.merges += ["Ġai r", "Ġa i", "pla ne"]
+        self.init_kwargs = {"vocabulary": self.vocab, "merges": self.merges}
+        self.input_data = [
+            " airplane at airport</s>",
+            " airplane airport",
+        ]
 
-        merges = ["Ġ a", "Ġ t", "Ġ k", "Ġ i", "Ġ b", "Ġa i", "p l", "n e"]
-        merges += ["Ġa t", "p o", "r t", "o h", "l i", "Ġi s", "Ġb e", "s t"]
-        merges += ["Ġt h", "Ġai r", "pl a", "Ġk oh", "Ġth e", "Ġbe st", "po rt"]
-        merges += ["pla ne"]
-        self.merges = merges
-
-        self.tokenizer = OPTTokenizer(vocabulary=self.vocab, merges=self.merges)
-
-    def test_tokenize(self):
-        input_data = " airplane at airport"
-        output = self.tokenizer(input_data)
-        self.assertAllEqual(output, [2, 3, 4, 2, 5])
-
-    def test_tokenize_special_tokens(self):
-        input_data = "</s> airplane at airport</s><pad>"
-        output = self.tokenizer(input_data)
-        self.assertAllEqual(output, [1, 2, 3, 4, 2, 5, 1, 0])
-
-    def test_tokenize_batch(self):
-        input_data = [" airplane at airport", " kohli is the best"]
-        output = self.tokenizer(input_data)
-        self.assertAllEqual(output, [[2, 3, 4, 2, 5], [6, 7, 8, 9, 10]])
-
-    def test_detokenize(self):
-        input_tokens = [2, 3, 4, 2, 5]
-        output = self.tokenizer.detokenize(input_tokens)
-        self.assertEqual(output, " airplane at airport")
-
-    def test_vocabulary_size(self):
-        self.assertEqual(self.tokenizer.vocabulary_size(), 11)
+    def test_tokenizer_basics(self):
+        self.run_preprocessing_layer_test(
+            cls=OPTTokenizer,
+            init_kwargs=self.init_kwargs,
+            input_data=self.input_data,
+            expected_output=[[3, 4, 5, 3, 6, 1], [3, 4, 3, 6]],
+        )
 
     def test_errors_missing_special_tokens(self):
         with self.assertRaises(ValueError):
             OPTTokenizer(vocabulary=["a", "b", "c"], merges=[])
 
-    def test_serialization(self):
-        config = keras.saving.serialize_keras_object(self.tokenizer)
-        new_tokenizer = keras.saving.deserialize_keras_object(config)
-        self.assertEqual(
-            new_tokenizer.get_config(),
-            self.tokenizer.get_config(),
+    @pytest.mark.large
+    def test_smallest_preset(self):
+        self.run_preset_test(
+            cls=OPTTokenizer,
+            preset="opt_125m_en",
+            input_data=["The quick brown fox."],
+            expected_output=[[133, 2119, 6219, 23602, 4]],
         )
+
+    @pytest.mark.extra_large
+    def test_all_presets(self):
+        for preset in OPTTokenizer.presets:
+            self.run_preset_test(
+                cls=OPTTokenizer,
+                preset=preset,
+                input_data=self.input_data,
+            )
