0b17acc9d888614011f6e059b30b63ff443d15ad
Matt Watson <mattdangerw@gmail.com>
2023-10-16 16:52:50 -0700

    roberta consolidated testing

--- keras_nlp/models/roberta/roberta_backbone_test.py
+++ keras_nlp/models/roberta/roberta_backbone_test.py
@@ -12,13 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
-import numpy as np
 import pytest
-import tensorflow as tf
 
-from keras_nlp.backend import keras
 from keras_nlp.backend import ops
 from keras_nlp.models.roberta.roberta_backbone import RobertaBackbone
 from keras_nlp.tests.test_case import TestCase
@@ -26,66 +21,58 @@ from keras_nlp.tests.test_case import TestCase
 
 class RobertaBackboneTest(TestCase):
     def setUp(self):
-        self.backbone = RobertaBackbone(
-            vocabulary_size=10,
-            num_layers=2,
-            num_heads=2,
-            hidden_dim=2,
-            intermediate_dim=4,
-            max_sequence_length=5,
-        )
-        self.batch_size = 8
-        self.input_batch = {
-            "token_ids": np.ones((2, 5), dtype="int32"),
-            "padding_mask": np.ones((2, 5), dtype="int32"),
+        self.init_kwargs = {
+            "vocabulary_size": 10,
+            "num_layers": 2,
+            "num_heads": 2,
+            "hidden_dim": 2,
+            "intermediate_dim": 4,
+            "max_sequence_length": 5,
+        }
+        self.input_data = {
+            "token_ids": ops.ones((2, 5), dtype="int32"),
+            "segment_ids": ops.zeros((2, 5), dtype="int32"),
+            "padding_mask": ops.ones((2, 5), dtype="int32"),
         }
 
-        self.input_dataset = tf.data.Dataset.from_tensor_slices(
-            self.input_batch
-        ).batch(2)
-
-    def test_valid_call_roberta(self):
-        self.backbone(self.input_batch)
-
-    def test_token_embedding(self):
-        output = self.backbone.token_embedding(self.input_batch["token_ids"])
-        self.assertEqual(output.shape, (2, 5, 2))
-
-    def test_name(self):
-        self.assertRegexpMatches(self.backbone.name, "roberta_backbone")
-
-    def test_predict(self):
-        self.backbone.predict(self.input_batch)
-        self.backbone.predict(self.input_dataset)
-
-    def test_serialization(self):
-        new_backbone = keras.saving.deserialize_keras_object(
-            keras.saving.serialize_keras_object(self.backbone)
+    def test_backbone_basics(self):
+        self.run_backbone_test(
+            cls=RobertaBackbone,
+            init_kwargs=self.init_kwargs,
+            input_data=self.input_data,
+            expected_output_shape=(2, 5, 2),
         )
-        self.assertEqual(new_backbone.get_config(), self.backbone.get_config())
 
-    def test_variable_sequence_length_call_roberta(self):
-        for seq_length in (2, 3, 4):
-            input_data = {
-                "token_ids": np.ones((2, seq_length), dtype="int32"),
-                "padding_mask": np.ones((2, seq_length), dtype="int32"),
-            }
-            output = self.backbone(input_data)
-            self.assertAllEqual(
-                ops.shape(output),
-                (2, seq_length, self.backbone.hidden_dim),
-            )
-
-    @pytest.mark.large  # Saving is slow, so mark these large.
+    @pytest.mark.large
     def test_saved_model(self):
-        model_output = self.backbone(self.input_batch)
-        path = os.path.join(self.get_temp_dir(), "model.keras")
-        self.backbone.save(path, save_format="keras_v3")
-        restored_model = keras.models.load_model(path)
+        self.run_model_saving_test(
+            cls=RobertaBackbone,
+            init_kwargs=self.init_kwargs,
+            input_data=self.input_data,
+        )
 
-        # Check we got the real object back.
-        self.assertIsInstance(restored_model, RobertaBackbone)
+    @pytest.mark.large
+    def test_smallest_preset(self):
+        self.run_preset_test(
+            cls=RobertaBackbone,
+            preset="roberta_base_en",
+            input_data={
+                "token_ids": ops.array([[0, 133, 2119, 2]], dtype="int32"),
+                "segment_ids": ops.zeros((1, 4), dtype="int32"),
+                "padding_mask": ops.ones((1, 4), dtype="int32"),
+            },
+            expected_output_shape=(1, 4, 768),
+            # The forward pass from a preset should be stable!
+            expected_partial_output=ops.array(
+                [-0.051, 0.100, -0.010, -0.097, 0.059],
+            ),
+        )
 
-        # Check that output matches.
-        restored_output = restored_model(self.input_batch)
-        self.assertAllClose(model_output, restored_output)
+    @pytest.mark.extra_large
+    def test_all_presets(self):
+        for preset in RobertaBackbone.presets:
+            self.run_preset_test(
+                cls=RobertaBackbone,
+                preset=preset,
+                input_data=self.input_data,
+            )
