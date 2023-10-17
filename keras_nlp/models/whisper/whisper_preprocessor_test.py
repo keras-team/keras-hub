9d01bf01e67d0593b255caa110ff1f7a153985b0
Matt Watson <mattdangerw@gmail.com>
2023-10-16 19:55:08 -0700

    whisper consolidated testing

--- keras_nlp/models/whisper/whisper_preprocessor_test.py
+++ keras_nlp/models/whisper/whisper_preprocessor_test.py
@@ -13,9 +13,7 @@
 # limitations under the License.
 
 import numpy as np
-import tensorflow as tf
 
-from keras_nlp.backend import keras
 from keras_nlp.models.whisper.whisper_audio_feature_extractor import (
     WhisperAudioFeatureExtractor,
 )
@@ -26,40 +24,18 @@ from keras_nlp.tests.test_case import TestCase
 
 class WhisperPreprocessorTest(TestCase):
     def setUp(self):
-        self.num_mels = 80
-        self.num_fft_bins = 400
-        self.stride = 100
-        self.sampling_rate = 100
-        self.max_audio_length = 5
-        self.output_length = (
-            self.max_audio_length * self.sampling_rate
-        ) // self.stride
         self.audio_feature_extractor = WhisperAudioFeatureExtractor(
-            num_mels=self.num_mels,
-            num_fft_bins=self.num_fft_bins,
-            stride=self.stride,
-            sampling_rate=self.sampling_rate,
-            max_audio_length=self.max_audio_length,
+            num_mels=80,
+            num_fft_bins=400,
+            stride=100,
+            sampling_rate=100,
+            max_audio_length=5,
         )
-
-        self.vocab = {
-            "Ġair": 0,
-            "plane": 1,
-            "Ġat": 2,
-            "port": 3,
-            "Ġkoh": 4,
-            "li": 5,
-            "Ġis": 6,
-            "Ġthe": 7,
-            "Ġbest": 8,
-        }
-
-        merges = ["Ġ a", "Ġ t", "Ġ k", "Ġ i", "Ġ b", "Ġa i", "p l", "n e"]
-        merges += ["Ġa t", "p o", "r t", "o h", "l i", "Ġi s", "Ġb e", "s t"]
-        merges += ["Ġt h", "Ġai r", "pl a", "Ġk oh", "Ġth e", "Ġbe st", "po rt"]
-        merges += ["pla ne"]
-        self.merges = merges
-
+        self.vocab = ["air", "Ġair", "plane", "Ġat", "port"]
+        self.vocab = dict([(token, i) for i, token in enumerate(self.vocab)])
+        self.merges = ["Ġ a", "Ġ t", "Ġ i", "Ġ b", "a i", "p l", "n e"]
+        self.merges += ["Ġa t", "p o", "r t", "Ġt h", "ai r", "pl a", "po rt"]
+        self.merges += ["Ġai r", "Ġa i", "pla ne"]
         self.special_tokens = {
             "<|startoftranscript|>": 9,
             "<|endoftext|>": 10,
@@ -67,103 +43,33 @@ class WhisperPreprocessorTest(TestCase):
             "<|transcribe|>": 12,
             "<|translate|>": 13,
         }
-
         self.language_tokens = {
             "<|en|>": 14,
             "<|fr|>": 15,
         }
-
         self.tokenizer = WhisperTokenizer(
             vocabulary=self.vocab,
             merges=self.merges,
             special_tokens=self.special_tokens,
             language_tokens=self.language_tokens,
         )
-
-        self.preprocessor = WhisperPreprocessor(
-            audio_feature_extractor=self.audio_feature_extractor,
-            tokenizer=self.tokenizer,
-            decoder_sequence_length=12,
-            language="<|en|>",
-            task="translate",
-        )
-
-    def test_unbatched_preprocess(self):
-        input_data = {
-            "encoder_audio": np.ones((200,)),
-            "decoder_text": " airplane at airport",
+        self.init_kwargs = {
+            "audio_feature_extractor": self.audio_feature_extractor,
+            "tokenizer": self.tokenizer,
+            "decoder_sequence_length": 12,
+            "language": "<|en|>",
+            "task": "translate",
+        }
+        self.input_data = {
+            "encoder_audio": np.ones((2, 200)),
+            "decoder_text": [" airplane at airport", " airplane at"],
         }
 
-        x = self.preprocessor(input_data)
-        self.assertAllEqual(
-            x["encoder_features"].shape, [self.output_length, self.num_mels]
-        )
-        self.assertAllEqual(
-            x["decoder_token_ids"], [9, 14, 13, 11, 0, 1, 2, 0, 3, 10, 10, 10]
-        )
-        self.assertAllEqual(
-            x["decoder_padding_mask"], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0]
-        )
-
-    def test_preprocess_batch(self):
-        input_data = {
-            "encoder_audio": np.ones((4, 200)),
-            "decoder_text": [" airplane at airport"] * 4,
-        }
-
-        x = self.preprocessor(input_data)
-        self.assertAllEqual(
-            x["encoder_features"].shape, [4, self.output_length, self.num_mels]
-        )
-        self.assertAllEqual(
-            x["decoder_token_ids"],
-            [[9, 14, 13, 11, 0, 1, 2, 0, 3, 10, 10, 10]] * 4,
-        )
-        self.assertAllEqual(
-            x["decoder_padding_mask"],
-            [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0]] * 4,
-        )
-
-    def test_preprocess_labeled_batch(self):
-        x = {
-            "encoder_audio": np.ones((4, 200)),
-            "decoder_text": [" airplane at airport"] * 4,
-        }
-        y_in = np.ones((4,))
-        sw_in = np.ones((4,))
-        x, y, sw = self.preprocessor(x, y_in, sw_in)
-        self.assertAllEqual(
-            x["encoder_features"].shape, [4, self.output_length, self.num_mels]
-        )
-        self.assertAllEqual(
-            x["decoder_token_ids"],
-            [[9, 14, 13, 11, 0, 1, 2, 0, 3, 10, 10, 10]] * 4,
-        )
-        self.assertAllEqual(
-            x["decoder_padding_mask"],
-            [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0]] * 4,
-        )
-        self.assertAllEqual(y, y_in)
-        self.assertAllEqual(sw, sw_in)
-
-    def test_preprocess_dataset(self):
-        x = {
-            "encoder_audio": np.ones((4, 200)),
-            "decoder_text": [" airplane at airport"] * 4,
-        }
-        ds = tf.data.Dataset.from_tensor_slices(x)
-        ds = ds.map(self.preprocessor)
-        x = ds.batch(4).take(1).get_single_element()
-        self.assertAllEqual(
-            x["encoder_features"].shape, [4, self.output_length, self.num_mels]
-        )
-        self.assertAllEqual(
-            x["decoder_token_ids"],
-            [[9, 14, 13, 11, 0, 1, 2, 0, 3, 10, 10, 10]] * 4,
-        )
-        self.assertAllEqual(
-            x["decoder_padding_mask"],
-            [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0]] * 4,
+    def test_feature_extractor_basics(self):
+        self.run_preprocessing_layer_test(
+            cls=WhisperPreprocessor,
+            init_kwargs=self.init_kwargs,
+            input_data=self.input_data,
         )
 
     def test_sequence_length_override(self):
@@ -171,13 +77,6 @@ class WhisperPreprocessorTest(TestCase):
             "encoder_audio": np.ones((200,)),
             "decoder_text": " airplane at airport",
         }
-        x = self.preprocessor(input_data, decoder_sequence_length=6)
-        self.assertAllEqual(x["decoder_token_ids"], [9, 14, 13, 11, 0, 10])
-
-    def test_serialization(self):
-        config = keras.saving.serialize_keras_object(self.preprocessor)
-        new_preprocessor = keras.saving.deserialize_keras_object(config)
-        self.assertEqual(
-            new_preprocessor.get_config(),
-            self.preprocessor.get_config(),
-        )
+        preprocessor = WhisperPreprocessor(**self.init_kwargs)
+        x = preprocessor(input_data, decoder_sequence_length=6)
+        self.assertAllEqual(x["decoder_token_ids"], [9, 14, 13, 11, 1, 10])
