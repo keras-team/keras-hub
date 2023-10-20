dba4ac5c26e2721615e4dcb2750741f141b51326
Tirth Patel <tirthasheshpatel@gmail.com>
2023-10-20 21:54:00 +0000

    Remove SentencePieceTrainer from keras_nlp/models/f_net

---
+++ tools/sentencepiece_testing/create_f_net_test_proto.py
@@ -0,0 +1,47 @@
+# Copyright 2023 The KerasNLP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from tools.sentencepiece_testing.utils import _save
+from tools.sentencepiece_testing.utils import _train_sentencepiece
+
+
+def main():
+    bytes_io = _train_sentencepiece(
+        ["the quick brown fox", "the earth is round"],
+        vocab_size=12,
+        model_type="WORD",
+        pad_id=0,
+        unk_id=1,
+        bos_id=2,
+        eos_id=3,
+        pad_piece="<pad>",
+        unk_piece="<unk>",
+        bos_piece="[CLS]",
+        eos_piece="[SEP]",
+        user_defined_symbols="[MASK]",
+    )
+    _save(bytes_io, "f_net_sentencepiece.proto")
+
+    bytes_io = _train_sentencepiece(
+        ["abc"],
+        vocab_size=5,
+        pad_id=-1,
+        eos_id=-1,
+        bos_id=-1,
+    )
+    _save(bytes_io, "f_net_sentencepiece_bad.proto")
+
+
+if __name__ == "__main__":
+    main()
