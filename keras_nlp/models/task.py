ecbbbe6fdd07ee372dbd2e5981820c44206e394a
sampathweb <1437573+sampathweb@users.noreply.github.com>
2023-12-19 16:47:00 -0600

    Fix preprocessor validation in tasks

--- keras_nlp/models/task.py
+++ keras_nlp/models/task.py
@@ -174,6 +174,13 @@ class Task(PipelineModel):
         )
         ```
         """
+        if "backbone" in kwargs:
+            raise ValueError(
+                "You cannot pass a `backbone` argument to the `from_preset` "
+                f"method. Instead, call the {cls.__name__} default "
+                "constructor with a `backbone` argument. "
+                f"Received: backbone={kwargs['backbone']}."
+            )
         # We support short IDs for official presets, e.g. `"bert_base_en"`.
         # Map these to a Kaggle Models handle.
         if preset in cls.presets:
@@ -187,13 +194,6 @@ class Task(PipelineModel):
                 preset,
                 load_weights=load_weights,
             )
-            if "backbone" in kwargs:
-                raise ValueError(
-                    "You cannot pass a `backbone` argument to the "
-                    f"`from_preset` method. Instead, call the {cls.__name__} "
-                    "default constructor with `backbone` argument. "
-                    f"Received: backbone={backbone}."
-                )
             if "preprocessor" in kwargs:
                 preprocessor = kwargs.pop("preprocessor")
             else:
