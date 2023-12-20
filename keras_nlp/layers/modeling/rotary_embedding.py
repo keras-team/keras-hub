4ea8c2311f4dff2db640e8b411410e2f671108a4
Tirth Patel <tirthasheshpatel@gmail.com>
2023-12-19 17:37:07 -0700

    Add MistralAI's 7B Transformer as a backbone in KerasNLP Models (#1314)
    * Add MistralBackbone
    
    * Fix Keras 2 failure
    
    * Fix another Keras 2 failure
    
    * Make the caching step XLA compatible
    
    * Add dtype support for the MistralBackbone
    
    * Address review comments
    
    * Add docs; Make args keyword-only; Cosmetic fixes
    
    * Use keras.backend.floatx() instead of keras.config.floatx()
    
    for Keras 2 compatibility
    
    * Add review comments

--- keras_nlp/layers/modeling/rotary_embedding.py
+++ keras_nlp/layers/modeling/rotary_embedding.py
@@ -97,7 +97,7 @@ class RotaryEmbedding(keras.layers.Layer):
         return (tensor * cos_emb) + (half_rot_tensor * sin_emb)
 
     def _compute_cos_sin_embedding(self, x, rotary_dim, start_index):
-        freq_range = ops.arange(0, rotary_dim, 2, dtype="float32")
+        freq_range = ops.arange(0, rotary_dim, 2)
         freq_range = ops.cast(freq_range, self.compute_dtype)
         freq_range = freq_range / ops.cast(
             self.scaling_factor, self.compute_dtype
@@ -107,7 +107,7 @@ class RotaryEmbedding(keras.layers.Layer):
             ** (freq_range / ops.cast(rotary_dim, self.compute_dtype))
         )
         seq_len = ops.shape(x)[self.sequence_axis]
-        tensor = ops.arange(seq_len, dtype="float32") + start_index
+        tensor = ops.cast(ops.arange(seq_len), self.compute_dtype) + start_index
         tensor = ops.cast(tensor, dtype=inverse_freq.dtype)
         freq = ops.einsum("i, j -> ij", tensor, inverse_freq)
         embedding = ops.concatenate((freq, freq), axis=self.feature_axis)
