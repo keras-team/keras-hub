124868211b2c0a42c81649a3bf9caf7c52df609f
Mehdi BRAHIMI <36081903+mbrhd@users.noreply.github.com>
2023-12-20 20:12:41 +0100

    changing pooling output (#1364)
    * changing pooling output
    
    * pr comments

--- keras_nlp/models/bert/bert_backbone.py
+++ keras_nlp/models/bert/bert_backbone.py
@@ -162,13 +162,12 @@ class BertBackbone(Backbone):
         # Construct the two BERT outputs. The pooled output is a dense layer on
         # top of the [CLS] token.
         sequence_output = x
-        x = keras.layers.Dense(
+        pooled_output = keras.layers.Dense(
             hidden_dim,
             kernel_initializer=bert_kernel_initializer(),
             activation="tanh",
             name="pooled_dense",
-        )(x)
-        pooled_output = x[:, cls_token_index, :]
+        )(x[:, cls_token_index, :])
 
         # Instantiate using Functional API Model constructor
         super().__init__(
