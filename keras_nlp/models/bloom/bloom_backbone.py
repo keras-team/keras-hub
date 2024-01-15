c41e844703230d948d5eadcd8ac45555e511fd26
Mohamed Abu El-Nasr <64566340+abuelnasr0@users.noreply.github.com>
2024-01-15 06:33:13 +0200

    Add Bloom Model (#1382)
    * Add Bloom Model
    
    * Add Backbone test and some fixes
    
    * Add BloomBackbone to keras_nlp.models
    
    * Fix a typo in layer naming
    
    * Remove self.built = True
    
    * Revert "Remove self.built = True"
    
    This reverts commit 889f2040bb56ca7dda6e64c110ff6168b41eb494.
    
    * Add built=True to MLP layer
    
    * Add Checkpoint conversion script
    
    * Change LayerNorm name
    
    * Fix typo
    
    * Fix getting HF model output
    
    * Add  and  to allclose function in checkpoint conversion script
    
    * Remove allclose check
    
    * Add doc for bloom
    
    * Write batch size instead of _
    
    * Rename out_dense to output_dense
    
    * Rename out_dense to output_dense
    
    * Format to 80 chars and remove unnecessery check
    
    * Remove exporting BloomDecoder
    
    * Add intermediate_dim Arg
    
    * Format the code
    
    * Remove unnecessery comment
    
    * Use keras gelu
    
    * Remove MLP layer and implement it inside BloomDecoder
    
    * Split q k v heads
    
    * Remove shapes comments
    
    * Revert "Split q k v heads"
    
    This reverts commit 2d03d2c0e872b5e26f9e96b1cc5d610e5a306289.
    
    * Revert "Revert "Split q k v heads""
    
    This reverts commit 531b1ff64f9e4d30515dfaa19345db4677c87e15.
    
    * Revert "Remove shapes comments"
    
    This reverts commit 2eeb5f48a7e6d30b66a1fa61df449d2d39f33950.
    
    * Add bias axes
    
    * Add bias axes to the correct axes
    
    * Update conversion script for splitting q,k,v
    
    * format the code
    
    * Rename _dropout -> _dropout_layer
    
    * use clone initializer instead of paasing str name
    
    * Serialize kernal & bais initializers
    
    * Format the code
    
    * Add alibi_bias_max to _build_alibi_tensor function
    
    * Format the code
    
    * Lowercase vairiable names

---
+++ keras_nlp/models/bloom/bloom_backbone.py
@@ -0,0 +1,153 @@
+# Copyright 2023 The KerasNLP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from keras_nlp.api_export import keras_nlp_export
+from keras_nlp.backend import keras
+from keras_nlp.layers.modeling.reversible_embedding import ReversibleEmbedding
+from keras_nlp.models.backbone import Backbone
+from keras_nlp.models.bloom.bloom_decoder import BloomDecoder
+
+
+def _bloom_kernel_initializer(stddev=0.02):
+    return keras.initializers.RandomNormal(stddev=stddev)
+
+
+@keras_nlp_export("keras_nlp.models.BloomBackbone")
+class BloomBackbone(Backbone):
+    """A Bloom decoder network.
+
+    This network implements a Transformer-based decoder network, BigScience
+    Language Open-science Open-access Multilingual (BLOOM), as descriped in
+    ["BLOOM: A 176B-Parameter Open-Access Multilingual Language Model"](https://arxiv.org/pdf/2211.05100.pdf).
+
+    The default constructor gives a fully customizable, randomly initialized
+    Bloom model with any number of layers, heads, and embedding dimensions. To
+    load preset architectures and weights, use the `from_preset()` constructor.
+
+    Disclaimer: Pre-trained models are provided on an "as is" basis, without
+    warranties or conditions of any kind.
+
+    Args:
+        vocabulary_size: int. The size of the token vocabulary.
+        num_layers: int. The number of transformer layers.
+        num_heads: int. The number of attention heads for each transformer.
+            The hidden size must be divisible by the number of attention heads.
+        hidden_dim: int. The dimensionality of the embeddings and hidden states.
+        intermediate_dim: int. The output dimension of the first Dense layer in
+            the MLP network of each transformer.
+        dropout: float. Dropout probability for the Transformer decoder.
+        layer_norm_epsilon: float. Epsilon for the layer normalization layers in
+            the transformer decoder.
+        max_sequence_length: int. The maximum sequence length that this decoder
+            can consume.
+
+    Examples:
+    ```python
+    input_data = {
+        "token_ids": np.ones(shape=(1, 12), dtype="int32"),
+        "padding_mask": np.array([[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0]]),
+    }
+
+    # Randomly initialized BLOOM decoder with a custom config.
+    model = keras_nlp.models.BloomBackbone(
+        vocabulary_size=10,
+        num_layers=2,
+        num_heads=2,
+        hidden_dim=32,
+        intermediate_dim=32*4,
+        dropout=0.0,
+        layer_norm_epsilon=1e-5,
+        max_sequence_length=128,
+    )
+    model(input_data)
+    ```
+
+    """
+
+    def __init__(
+        self,
+        vocabulary_size,
+        num_layers,
+        num_heads,
+        hidden_dim,
+        intermediate_dim,
+        dropout=0.0,
+        layer_norm_epsilon=1e-5,
+        max_sequence_length=512,
+        **kwargs,
+    ):
+        token_ids = keras.Input(shape=(None,), dtype="int32", name="token_ids")
+        padding_mask = keras.Input(
+            shape=(None,), dtype="int32", name="padding_mask"
+        )
+
+        # Embed tokens
+        token_embedding_layer = ReversibleEmbedding(
+            input_dim=vocabulary_size,
+            output_dim=hidden_dim,
+            embeddings_initializer=_bloom_kernel_initializer(stddev=0.02),
+            tie_weights=False,
+            name="token_embedding",
+        )
+        token_embedding = token_embedding_layer(token_ids)
+
+        x = keras.layers.LayerNormalization(
+            epsilon=layer_norm_epsilon, name="token_embedding_layernorm"
+        )(token_embedding)
+
+        for i in range(num_layers):
+            x = BloomDecoder(
+                num_heads=num_heads,
+                intermediate_dim=intermediate_dim,
+                dropout=dropout,
+                layer_norm_epsilon=layer_norm_epsilon,
+                name=f"transformer_layer_{i}",
+            )(x, decoder_padding_mask=padding_mask)
+
+        sequence_output = keras.layers.LayerNormalization(
+            epsilon=layer_norm_epsilon, name="final_layernorm"
+        )(x)
+
+        super().__init__(
+            inputs={
+                "token_ids": token_ids,
+                "padding_mask": padding_mask,
+            },
+            outputs=sequence_output,
+            **kwargs,
+        )
+        self.vocabulary_size = vocabulary_size
+        self.num_layers = num_layers
+        self.num_heads = num_heads
+        self.hidden_dim = hidden_dim
+        self.intermediate_dim = intermediate_dim
+        self.dropout = dropout
+        self.layer_norm_epsilon = layer_norm_epsilon
+        self.max_sequence_length = max_sequence_length
+        self.token_embedding = token_embedding_layer
+
+    def get_config(self):
+        config = super().get_config()
+        config.update(
+            {
+                "vocabulary_size": self.vocabulary_size,
+                "num_layers": self.num_layers,
+                "num_heads": self.num_heads,
+                "hidden_dim": self.hidden_dim,
+                "intermediate_dim": self.intermediate_dim,
+                "dropout": self.dropout,
+                "layer_norm_epsilon": self.layer_norm_epsilon,
+                "max_sequence_length": self.max_sequence_length,
+            }
+        )
+        return config
