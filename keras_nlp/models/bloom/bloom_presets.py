0770b8a2686a9c1a03c16c4fc3f0c20995a94653
abuelnasr0 <abuelnasr70@gmail.com>
2024-01-17 04:11:10 +0200

    Comment unused code for now

--- keras_nlp/models/bloom/bloom_presets.py
+++ keras_nlp/models/bloom/bloom_presets.py
@@ -11,20 +11,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""BERT model preset configurations."""
+"""BLOOM model preset configurations."""
 
 backbone_presets = {
-    "bloom_tiny_multi": {
+    "bloom_560m_multi": {
         "metadata": {
-            "description": (
-                "2-layer BERT model where all input is lowercased. "
-                "Trained on English Wikipedia + BooksCorpus."
-            ),
-            "params": 4385920,
-            "official_name": "BERT",
-            "path": "bert",
-            "model_card": "https://github.com/google-research/bert/blob/master/README.md",
+            # "description": (
+            #     "2-layer BERT model where all input is lowercased. "
+            #     "Trained on English Wikipedia + BooksCorpus."
+            # ),
+            "params": 816115712,
+            "official_name": "BLOOM",
+            "path": "bloom",
+            # "model_card": "https://github.com/google-research/bert/blob/master/README.md",
         },
-        "kaggle_handle": "kaggle://keras/bert/keras/bert_tiny_en_uncased/2",
+        # "kaggle_handle": "kaggle://keras/bert/keras/bert_tiny_en_uncased/2",
     },
 }
