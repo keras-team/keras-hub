dea9768b267faaaa5befd0b888926febe908a95e
Matt Watson <mattdangerw@gmail.com>
2024-03-11 15:51:49 -0700

    Review comment

--- tools/checkpoint_conversion/convert_gemma_checkpoints.py
+++ tools/checkpoint_conversion/convert_gemma_checkpoints.py
@@ -15,7 +15,7 @@
 Convert Gemma flax checkpoints to the Keras format.
 
 Setup:
-pip install requirements.txt
+pip install -r requirements.txt
 pip install git+https://github.com/google-deepmind/gemma.git
 python pip_build.py --install
 
