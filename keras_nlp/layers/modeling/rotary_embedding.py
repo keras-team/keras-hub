bd24a2b50644d36e1457413db93e5855c7d3bb7e
Tirth Patel <tirthasheshpatel@gmail.com>
2024-03-12 00:15:19 +0000

    Always run the rotary embedding layer in float32

--- keras_nlp/layers/modeling/rotary_embedding.py
+++ keras_nlp/layers/modeling/rotary_embedding.py
@@ -85,8 +85,11 @@ class RotaryEmbedding(keras.layers.Layer):
         self.built = True
 
     def call(self, inputs, start_index=0):
+        inputs = ops.cast(inputs, "float32")
+        start_index = ops.cast(start_index, dtype="float32")
         cos_emb, sin_emb = self._compute_cos_sin_embedding(inputs, start_index)
-        return self._apply_rotary_pos_emb(inputs, cos_emb, sin_emb)
+        output = self._apply_rotary_pos_emb(inputs, cos_emb, sin_emb)
+        return ops.cast(output, self.compute_dtype)
 
     def _apply_rotary_pos_emb(self, tensor, cos_emb, sin_emb):
         x1, x2 = ops.split(tensor, 2, axis=self.feature_axis)
@@ -101,10 +104,12 @@ class RotaryEmbedding(keras.layers.Layer):
         sequence_axis = get_axis(self.sequence_axis)
 
         rotary_dim = ops.shape(inputs)[feature_axis]
+        rotary_dim = ops.cast(rotary_dim, "int32")
         inverse_freq = self._get_inverse_freq(rotary_dim)
 
         seq_len = ops.shape(inputs)[self.sequence_axis]
-        tensor = ops.cast(ops.arange(seq_len), self.compute_dtype) + start_index
+        seq_len = ops.cast(seq_len, "int32")
+        tensor = ops.cast(ops.arange(seq_len), "float32") + start_index
 
         tensor = ops.cast(tensor, dtype=inverse_freq.dtype)
         freq = ops.einsum("i,j->ij", tensor, inverse_freq)
@@ -121,13 +126,11 @@ class RotaryEmbedding(keras.layers.Layer):
 
     def _get_inverse_freq(self, rotary_dim):
         freq_range = ops.arange(0, rotary_dim, 2)
-        freq_range = ops.cast(freq_range, self.compute_dtype)
-        freq_range = freq_range / ops.cast(
-            self.scaling_factor, self.compute_dtype
-        )
+        freq_range = ops.cast(freq_range, "float32")
+        freq_range = freq_range / ops.cast(self.scaling_factor, "float32")
         inverse_freq = 1.0 / (
             self.max_wavelength
-            ** (freq_range / ops.cast(rotary_dim, self.compute_dtype))
+            ** (freq_range / ops.cast(rotary_dim, "float32"))
         )
         return inverse_freq
 
