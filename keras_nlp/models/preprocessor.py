7279f15d2c33f7c8d8380903e9ed90dd70d5169b
Sachin Prasad <sachinprasad@google.com>
2024-03-20 11:19:01 -0700

    Standardize docstring

--- keras_nlp/models/preprocessor.py
+++ keras_nlp/models/preprocessor.py
@@ -75,7 +75,7 @@ class Preprocessor(PreprocessingLayer):
         Args:
             preset: string. Must be one of "{{preset_names}}".
 
-        Examples:
+        Example:
         ```python
         # Load a preprocessor layer from a preset.
         preprocessor = keras_nlp.models.{{preprocessor_name}}.from_preset(
