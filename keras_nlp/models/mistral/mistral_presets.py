374d1899ee7f349b6acf686328835e570cea4d65
Tirth Patel <tirthasheshpatel@gmail.com>
2024-03-25 21:35:32 +0000

    change preset name

--- keras_nlp/models/mistral/mistral_presets.py
+++ keras_nlp/models/mistral/mistral_presets.py
@@ -35,7 +35,7 @@ backbone_presets = {
         },
         "kaggle_handle": "kaggle://keras/mistral/keras/mistral_instruct_7b_en/6",
     },
-    "mistral_instruct_7b_v0.2_en": {
+    "mistral_0.2_instruct_7b_en": {
         "metadata": {
             "description": "Mistral 7B instruct Version 0.2 model",
             "params": 7241732096,
@@ -43,6 +43,6 @@ backbone_presets = {
             "path": "mistral",
             "model_card": "https://github.com/mistralai/mistral-src/blob/main/README.md",
         },
-        "kaggle_handle": "kaggle://keras/mistral/keras/mistral_instruct_7b_v0.2_en/1",
+        "kaggle_handle": "kaggle://keras/mistral/keras/mistral_0.2_instruct_7b_en/1",
     },
 }
