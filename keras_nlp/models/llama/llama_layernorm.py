d5fa09d9532f60c7236ab2746f6a3072df3d1f04
Tirth Patel <tirthasheshpatel@gmail.com>
2024-03-29 17:13:07 +0000

    Address review comments
    - Change private variables to public vars
    - Change `self._weight` to `self.scale`
    - Don't persist the input dim
    - Move the var computation to its own line for readability

--- keras_nlp/models/llama/llama_layernorm.py
+++ keras_nlp/models/llama/llama_layernorm.py
@@ -23,14 +23,14 @@ class LlamaLayerNorm(keras.layers.Layer):
 
     def __init__(self, epsilon=1e-6, **kwargs):
         super().__init__(**kwargs)
-        self._epsilon = epsilon
+        self.epsilon = epsilon
 
     def build(self, input_shape):
-        self._dim = input_shape[-1]
-        self._weight = self.add_weight(
+        dim = input_shape[-1]
+        self.scale = self.add_weight(
             name="weight",
             trainable=True,
-            shape=(self._dim,),
+            shape=(dim,),
             initializer="ones",
             dtype=self.variable_dtype,
         )
@@ -38,12 +38,11 @@ class LlamaLayerNorm(keras.layers.Layer):
 
     def call(self, x):
         x = ops.cast(x, "float32")
-        x = x * ops.rsqrt(
-            ops.mean(ops.power(x, 2), axis=-1, keepdims=True) + self._epsilon
-        )
-        return ops.cast(x, self.compute_dtype) * self._weight
+        var = ops.mean(ops.power(x, 2), axis=-1, keepdims=True)
+        x = x * ops.rsqrt(var + self.epsilon)
+        return ops.cast(x, self.compute_dtype) * self.scale
 
     def get_config(self):
         config = super().get_config()
-        config.update({"epsilon": self._epsilon})
+        config.update({"epsilon": self.epsilon})
         return config
