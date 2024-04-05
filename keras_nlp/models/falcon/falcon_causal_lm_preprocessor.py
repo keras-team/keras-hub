11a93430d323f254aa4e9be356f1e176e490f963
Matt Watson <1389937+mattdangerw@users.noreply.github.com>
2024-04-04 19:25:32 -0700

    Remove falcon from the release (#1554)
    Still missing the generative class and more presets.

--- keras_nlp/models/falcon/falcon_causal_lm_preprocessor.py
+++ keras_nlp/models/falcon/falcon_causal_lm_preprocessor.py
@@ -15,7 +15,7 @@
 import tensorflow as tf
 from absl import logging
 
-from keras_nlp.api_export import keras_nlp_export
+from keras_nlp.backend import keras
 from keras_nlp.backend import ops
 from keras_nlp.models.falcon.falcon_preprocessor import FalconPreprocessor
 from keras_nlp.utils.keras_utils import (
@@ -24,7 +24,7 @@ from keras_nlp.utils.keras_utils import (
 from keras_nlp.utils.keras_utils import pack_x_y_sample_weight
 
 
-@keras_nlp_export("keras_nlp.models.FalconCausalLMPreprocessor")
+@keras.saving.register_keras_serializable(package="keras_nlp")
 class FalconCausalLMPreprocessor(FalconPreprocessor):
     """Falcon Causal LM preprocessor.
 
