84b2f911ad0e29c7132140edd4cc1785e923655d
Matt Watson <1389937+mattdangerw@users.noreply.github.com>
2024-04-04 19:25:32 -0700

    Remove falcon from the release (#1554)
    Still missing the generative class and more presets.

--- keras_nlp/models/falcon/falcon_backbone.py
+++ keras_nlp/models/falcon/falcon_backbone.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from keras_nlp.api_export import keras_nlp_export
+
 from keras_nlp.backend import keras
 from keras_nlp.layers.modeling.reversible_embedding import ReversibleEmbedding
 from keras_nlp.models.backbone import Backbone
@@ -20,7 +20,7 @@ from keras_nlp.models.falcon.falcon_transformer_decoder import (
 )
 
 
-@keras_nlp_export("keras_nlp.models.FalconBackbone")
+@keras.saving.register_keras_serializable(package="keras_nlp")
 class FalconBackbone(Backbone):
     """The Falcon core architecure.
 
