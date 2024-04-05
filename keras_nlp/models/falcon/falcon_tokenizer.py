3fde53fa0a36495ce27865d21d61e778fe25c835
Matt Watson <1389937+mattdangerw@users.noreply.github.com>
2024-04-04 19:25:32 -0700

    Remove falcon from the release (#1554)
    Still missing the generative class and more presets.

--- keras_nlp/models/falcon/falcon_tokenizer.py
+++ keras_nlp/models/falcon/falcon_tokenizer.py
@@ -12,12 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-from keras_nlp.api_export import keras_nlp_export
+from keras_nlp.backend import keras
 from keras_nlp.tokenizers.byte_pair_tokenizer import BytePairTokenizer
 
 
-@keras_nlp_export("keras_nlp.models.FalconTokenizer")
+@keras.saving.register_keras_serializable(package="keras_nlp")
 class FalconTokenizer(BytePairTokenizer):
     """Falcon tokenizer based on BytePairTokenizer.
 
