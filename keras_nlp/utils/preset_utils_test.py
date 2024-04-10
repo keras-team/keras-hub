f89d795501b4c725ed786c2bda44383e5f487bc2
Samaneh Saadat <ssaadat@google.com>
2024-04-10 20:30:27 +0000

    Remove preset saving and loading tests.

--- keras_nlp/utils/preset_utils_test.py
+++ keras_nlp/utils/preset_utils_test.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
 import os
 
 import pytest
@@ -21,83 +20,13 @@ from absl.testing import parameterized
 from keras_nlp import upload_preset
 from keras_nlp.models import AlbertClassifier
 from keras_nlp.models import BertBackbone
-from keras_nlp.models import BertClassifier
 from keras_nlp.models import BertTokenizer
-from keras_nlp.models import RobertaClassifier
 from keras_nlp.tests.test_case import TestCase
 from keras_nlp.utils.preset_utils import CONFIG_FILE
 from keras_nlp.utils.preset_utils import TOKENIZER_CONFIG_FILE
-from keras_nlp.utils.preset_utils import check_config_class
-from keras_nlp.utils.preset_utils import load_from_preset
-from keras_nlp.utils.preset_utils import save_to_preset
 
 
 class PresetUtilsTest(TestCase):
-    @parameterized.parameters(
-        (AlbertClassifier, "albert_base_en_uncased", "sentencepiece"),
-        (RobertaClassifier, "roberta_base_en", "bytepair"),
-        (BertClassifier, "bert_tiny_en_uncased", "wordpiece"),
-    )
-    @pytest.mark.keras_3_only
-    @pytest.mark.large
-    def test_preset_saving(self, cls, preset_name, tokenizer_type):
-        save_dir = self.get_temp_dir()
-        model = cls.from_preset(preset_name, num_classes=2)
-        save_to_preset(model, save_dir)
-
-        if tokenizer_type == "bytepair":
-            vocab_filename = "assets/tokenizer/vocabulary.json"
-            expected_assets = [
-                "assets/tokenizer/vocabulary.json",
-                "assets/tokenizer/merges.txt",
-            ]
-        elif tokenizer_type == "sentencepiece":
-            vocab_filename = "assets/tokenizer/vocabulary.spm"
-            expected_assets = ["assets/tokenizer/vocabulary.spm"]
-        else:
-            vocab_filename = "assets/tokenizer/vocabulary.txt"
-            expected_assets = ["assets/tokenizer/vocabulary.txt"]
-
-        # Check existence of files
-        self.assertTrue(os.path.exists(os.path.join(save_dir, vocab_filename)))
-        self.assertTrue(os.path.exists(os.path.join(save_dir, "config.json")))
-        self.assertTrue(
-            os.path.exists(os.path.join(save_dir, "model.weights.h5"))
-        )
-        self.assertTrue(os.path.exists(os.path.join(save_dir, "metadata.json")))
-
-        # Check the model config (`config.json`)
-        config_json = open(os.path.join(save_dir, "config.json"), "r").read()
-        self.assertTrue(
-            "build_config" not in config_json
-        )  # Test on raw json to include nested keys
-        self.assertTrue(
-            "compile_config" not in config_json
-        )  # Test on raw json to include nested keys
-        config = json.loads(config_json)
-        self.assertEqual(set(config["assets"]), set(expected_assets))
-        self.assertEqual(config["weights"], "model.weights.h5")
-
-        # Try loading the model from preset directory
-        self.assertEqual(cls, check_config_class(save_dir))
-
-        # Try loading the model from preset directory
-        restored_model = load_from_preset(save_dir)
-
-        train_data = (
-            ["the quick brown fox.", "the slow brown fox."],  # Features.
-        )
-        model_input_data = model.preprocessor(*train_data)
-        restored_model_input_data = restored_model.preprocessor(*train_data)
-
-        # Check that saved vocab is equal to the original preset vocab
-        self.assertAllClose(model_input_data, restored_model_input_data)
-
-        # Check model outputs
-        self.assertAllEqual(
-            model(model_input_data), restored_model(restored_model_input_data)
-        )
-
     def test_preset_errors(self):
         with self.assertRaisesRegex(ValueError, "must be a string"):
             AlbertClassifier.from_preset(AlbertClassifier)
