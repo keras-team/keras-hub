2348fc94641085d7c0106c8715f563104411d8ca
Matt Watson <1389937+mattdangerw@users.noreply.github.com>
2024-05-06 18:38:36 -0700

    Export the public API surface

--- keras_nlp/api/models/__init__.py
+++ keras_nlp/api/models/__init__.py
@@ -159,6 +159,16 @@ from keras_nlp.src.models.opt.opt_causal_lm_preprocessor import (
 )
 from keras_nlp.src.models.opt.opt_preprocessor import OPTPreprocessor
 from keras_nlp.src.models.opt.opt_tokenizer import OPTTokenizer
+from keras_nlp.src.models.paligemma.pali_gemma_backbone import PaliGemmaBackbone
+from keras_nlp.src.models.paligemma.pali_gemma_causal_lm import (
+    PaliGemmaCausalLM,
+)
+from keras_nlp.src.models.paligemma.pali_gemma_causal_lm_preprocesor import (
+    PaliGemmaCausalLMPreprocessor,
+)
+from keras_nlp.src.models.paligemma.pali_gemma_tokenizer import (
+    PaliGemmaTokenizer,
+)
 from keras_nlp.src.models.phi3.phi3_backbone import Phi3Backbone
 from keras_nlp.src.models.phi3.phi3_causal_lm import Phi3CausalLM
 from keras_nlp.src.models.phi3.phi3_causal_lm_preprocessor import (
