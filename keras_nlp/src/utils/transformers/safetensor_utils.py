225219bc5c04afbee368c1d20e6942fe13a1f3a9
Aritra Roy Gosthipaty <aritra.born2fly@gmail.com>
2024-06-11 14:31:37 +0000

    check: none type for reshape and transpose patterns

--- keras_nlp/src/utils/transformers/safetensor_utils.py
+++ keras_nlp/src/utils/transformers/safetensor_utils.py
@@ -97,8 +97,12 @@ def set_keras_weights(
 
     if isinstance(hf_weight_keys, str):
         hf_weight_keys = [hf_weight_keys]
-        reshape_patterns = [reshape_patterns]
-        transpose_patterns = [transpose_patterns]
+
+    if reshape_patterns is None:
+        reshape_patterns = [reshape_patterns] * len(hf_weight_keys)
+
+    if transpose_patterns is None:
+        transpose_patterns = [transpose_patterns] * len(hf_weight_keys)
 
     tensors = []
     for hf_weight_key, reshape_pattern, transpose_pattern in zip(
