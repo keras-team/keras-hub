7e325102502c115833c0482fbef03652bf65d407
Aritra Roy Gosthipaty <aritra.born2fly@gmail.com>
2024-07-07 12:53:32 +0000

    chore: adding tests

---
+++ keras_nlp/src/utils/transformers/convert_pali_gemma_test.py
@@ -0,0 +1,33 @@
+# Copyright 2023 The KerasNLP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import numpy as np
+import pytest
+
+from keras_nlp.src.models.pali_gemma.pali_gemma_causal_lm import (
+    PaliGemmaCausalLM,
+)
+from keras_nlp.src.tests.test_case import TestCase
+
+
+class TestTask(TestCase):
+    @pytest.mark.large
+    def test_convert_tiny_preset(self):
+        model = PaliGemmaCausalLM.from_preset(
+            "hf://ariG23498/tiny-pali-gemma-test"
+        )
+        image = np.random.rand(224, 224, 3)
+        prompt = "describe the image "
+        model.generate({"images": image, "prompts": prompt}, max_length=15)
+
+    # TODO: compare numerics with huggingface model
