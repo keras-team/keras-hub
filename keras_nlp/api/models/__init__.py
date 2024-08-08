7a4bc47780ec46f1a0f311c2f84b0e8e6814a587
Divyashree Sreepathihalli <divyashreepathihalli@gmail.com>
2024-08-07 17:57:31 -0700

    Add VGG16 backbone (#1737)
    * Agg Vgg16 backbone
    
    * update names
    
    * update tests
    
    * update test
    
    * add image classifier
    
    * incorporate review comments
    
    * Update test case
    
    * update backbone test
    
    * add image classifier
    
    * classifier cleanup
    
    * code reformat
    
    * add vgg16 image classifier
    
    * make vgg generic
    
    * update doc string
    
    * update docstring
    
    * add classifier test
    
    * update tests
    
    * update docstring
    
    * address review comments
    
    * code reformat
    
    * update the configs
    
    * address review comments
    
    * fix task saved model test
    
    * update init
    
    * code reformatted

--- keras_nlp/api/models/__init__.py
+++ keras_nlp/api/models/__init__.py
@@ -152,6 +152,7 @@ from keras_nlp.src.models.gpt_neo_x.gpt_neo_x_preprocessor import (
     GPTNeoXPreprocessor,
 )
 from keras_nlp.src.models.gpt_neo_x.gpt_neo_x_tokenizer import GPTNeoXTokenizer
+from keras_nlp.src.models.image_classifier import ImageClassifier
 from keras_nlp.src.models.llama3.llama3_backbone import Llama3Backbone
 from keras_nlp.src.models.llama3.llama3_causal_lm import Llama3CausalLM
 from keras_nlp.src.models.llama3.llama3_causal_lm_preprocessor import (
@@ -228,6 +229,8 @@ from keras_nlp.src.models.text_classifier import TextClassifier
 from keras_nlp.src.models.text_classifier_preprocessor import (
     TextClassifierPreprocessor,
 )
+from keras_nlp.src.models.vgg.vgg_backbone import VGGBackbone
+from keras_nlp.src.models.vgg.vgg_image_classifier import VGGImageClassifier
 from keras_nlp.src.models.whisper.whisper_backbone import WhisperBackbone
 from keras_nlp.src.models.whisper.whisper_tokenizer import WhisperTokenizer
 from keras_nlp.src.models.xlm_roberta.xlm_roberta_backbone import (
