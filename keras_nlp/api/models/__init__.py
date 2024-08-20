091752e17a5695d3af6e3108165f6fd11867a2d9
ushareng <usha.rengaraju@gmail.com>
2024-08-21 03:08:56 +0530

    merge conflict resolved

--- keras_nlp/api/models/__init__.py
+++ keras_nlp/api/models/__init__.py
@@ -165,15 +165,15 @@ from keras_nlp.src.models.mistral.mistral_preprocessor import (
     MistralPreprocessor,
 )
 from keras_nlp.src.models.mistral.mistral_tokenizer import MistralTokenizer
-from keras_nlp.src.models.mobilenet.mobilenet_backbone import MobileNetBackbone
-from keras_nlp.src.models.mobilenet.mobilenet_image_classifier import (
-    MobileNetImageClassifier,
-)
 from keras_nlp.src.models.mix_transformer.mix_transformer_backbone import (
     MiTBackbone,
 )
 from keras_nlp.src.models.mix_transformer.mix_transformer_classifier import (
     MiTImageClassifier,
+)
+from keras_nlp.src.models.mobilenet.mobilenet_backbone import MobileNetBackbone
+from keras_nlp.src.models.mobilenet.mobilenet_image_classifier import (
+    MobileNetImageClassifier,
 )
 from keras_nlp.src.models.opt.opt_backbone import OPTBackbone
 from keras_nlp.src.models.opt.opt_causal_lm import OPTCausalLM
