3c7f7380a9661d79b213851368e8372511d52bc0
Sravana Neeli <sineeli@google.com>
2024-08-22 18:39:18 -0700

    - fix api gen

--- keras_nlp/src/bounding_box/__init__.py
+++ keras_nlp/src/bounding_box/__init__.py
@@ -17,3 +17,4 @@ from keras_nlp.src.bounding_box.converters import _encode_box_to_deltas
 from keras_nlp.src.bounding_box.converters import convert_format
 from keras_nlp.src.bounding_box.to_dense import to_dense
 from keras_nlp.src.bounding_box.to_ragged import to_ragged
+from keras_nlp.src.bounding_box.validate_format import validate_format
