fbc13353af69e2e92bb28bfaa6100f5977675983
Matt Watson <1389937+mattdangerw@users.noreply.github.com>
2024-08-23 17:38:28 -0700

    Remove python 3.8 version in setup.py (#1792)
    We don't support that version

--- setup.py
+++ setup.py
@@ -78,7 +78,6 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
