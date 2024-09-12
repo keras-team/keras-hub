dd0651e711b0da1a9f4149520860bb4537df4059
Matt Watson <1389937+mattdangerw@users.noreply.github.com>
2024-09-12 16:59:22 -0700

    Remove preprocessing base classes we no longer use (#1824)
    For some reason, all our causal lm models had a preprocessing
    layer base class that packed inputs without setting things up for
    the causal language model loss.
    
    With our new base classes, `CausalLMPreprocessor` and
    `Seq2SeqLMPreprocessor`, these are no longer used at all.
    
    These are public, but I did a search for them on GitHub and the
    only one I could find with any usage is `GPT2Preprocessor`. The
    others are complete unused, and not generally very useful.
    
    Let's remove them before anyone does depend on them so that we can
    keep preprocessing one-to-one paired with a task. E.g.
    
    ```
    BertTextClassifier
    BertTextClassifierPreprocesssor
    ```

--- keras_nlp/src/models/gpt2/gpt2_preprocessor.py
+++ keras_nlp/src/models/gpt2/gpt2_preprocessor.py
@@ -25,85 +25,12 @@ from keras_nlp.src.utils.tensor_utils import preprocessing_function
 
 @keras_nlp_export("keras_nlp.models.GPT2Preprocessor")
 class GPT2Preprocessor(Preprocessor):
-    """GPT2 preprocessing layer which tokenizes and packs inputs.
+    """Legacy preprocessing layer for GPT2.
 
-    This preprocessing layer will do 2 things:
-
-    - Tokenize the inputs using the `tokenizer`.
-    - Construct a dictionary with keys `"token_ids"`, `"padding_mask"`, that can
-        be passed directly to a `keras_nlp.models.GPT2Backbone`.
-
-    This layer can be used directly with `tf.data.Dataset.map` to preprocess
-    string data in the `(x, y, sample_weight)` format used by
-    `keras.Model.fit`.
-
-    The call method of this layer accepts three arguments, `x`, `y`, and
-    `sample_weight`. `x` can be a python string or tensor representing a single
-    segment, a list of python strings representing a batch of single segments,
-    or a list of tensors representing multiple segments to be packed together.
-    `y` and `sample_weight` are both optional, can have any format, and will be
-    passed through unaltered.
-
-    `GPT2Preprocessor` forces the input to have only one segment, as GPT2 is
-    mainly used for generation tasks. For tasks having multi-segment inputs
-    like "glue/mnli", please use a model designed for classification purposes
-    such as BERT or RoBERTa.
-
-    Args:
-        tokenizer: A `keras_nlp.models.GPT2Tokenizer` instance.
-        sequence_length: The length of the packed inputs.
-        add_start_token: If `True`, the preprocessor will prepend the tokenizer
-            start token to each input sequence.
-        add_end_token: If `True`, the preprocessor will append the tokenizer
-            end token to each input sequence.
-
-    Call arguments:
-        x: A string, `tf.Tensor` or list of python strings.
-        y: Any label data. Will be passed through unaltered.
-        sample_weight: Any label weight data. Will be passed through unaltered.
-        sequence_length: Pass to override the configured `sequence_length` of
-            the layer.
-
-    Examples:
-
-    Directly calling the layer on data.
-    ```python
-    preprocessor = keras_nlp.models.GPT2Preprocessor.from_preset("gpt2_base_en")
-
-    # Tokenize and pack a single sentence.
-    preprocessor("The quick brown fox jumped.")
-
-    # Tokenize a batch of single sentences.
-    preprocessor(["The quick brown fox jumped.", "Call me Ishmael."])
-
-    # Custom vocabulary.
-    features = ["a quick fox.", "a fox quick."]
-    vocab = {"<|endoftext|>": 0, "a": 4, "Ġquick": 5, "Ġfox": 6}
-    merges = ["Ġ q", "u i", "c k", "ui ck", "Ġq uick"]
-    merges += ["Ġ f", "o x", "Ġf ox"]
-    tokenizer = keras_nlp.models.GPT2Tokenizer(
-        vocabulary=vocab,
-        merges=merges,
-    )
-    preprocessor = keras_nlp.models.GPT2Preprocessor(tokenizer=tokenizer)
-    preprocessor("The quick brown fox jumped.")
-    ```
-
-    Mapping with `tf.data.Dataset`.
-    ```python
-    preprocessor = keras_nlp.models.GPT2Preprocessor.from_preset("gpt2_base_en")
-
-    text = tf.constant(["The quick brown fox jumped.", "Call me Ishmael."])
-    label = tf.constant([1, 1])
-
-    # Map labeled single sentences.
-    ds = tf.data.Dataset.from_tensor_slices((text, label))
-    ds = ds.map(preprocessor, num_parallel_calls=tf.data.AUTOTUNE)
-
-    # Map unlabeled single sentences.
-    ds = tf.data.Dataset.from_tensor_slices(text)
-    ds = ds.map(preprocessor, num_parallel_calls=tf.data.AUTOTUNE)
-    ```
+    This layer should not be used in new code! All preprocessing layers pair
+    directly with a task. E.g. `BertClassifier` and
+    `BertClassifierPreprocessor`. Either use `GPT2CausalLMPreprocessor` or
+    wrap `GPT2Tokenizer` into a custom preprocessing layer or function.
     """
 
     backbone_cls = GPT2Backbone
@@ -117,6 +44,8 @@ class GPT2Preprocessor(Preprocessor):
         add_end_token=True,
         **kwargs,
     ):
+        # TODO: this class has some usage, but barely any, and is no longer
+        # documented. We should consider dropping it.
         super().__init__(**kwargs)
         self.tokenizer = tokenizer
         self.packer = None
