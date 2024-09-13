0472bcbb439a9813c8dbec2e99d73d31d1b5b3b0
Matt Watson <1389937+mattdangerw@users.noreply.github.com>
2024-09-13 12:26:54 -0700

    Bump nightly and head package version to 0.16 (#1826)
    0.15 is now the latest release!

--- keras_nlp/src/version_utils.py
+++ keras_nlp/src/version_utils.py
@@ -15,7 +15,7 @@
 from keras_nlp.src.api_export import keras_nlp_export
 
 # Unique source of truth for the version number.
-__version__ = "0.15.0"
+__version__ = "0.16.0"
 
 
 @keras_nlp_export("keras_nlp.version")
