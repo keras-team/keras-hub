fc1a3a53f888d25d9af7cbe2bd864c50d48810fb
sachin prasad <sachinprasad@google.com>
2024-09-17 14:46:23 -0700

    fix imports

--- keras_nlp/api/models/__init__.py
+++ keras_nlp/api/models/__init__.py
@@ -96,15 +96,15 @@ from keras_nlp.src.models.deberta_v3.deberta_v3_text_classifier_preprocessor imp
 from keras_nlp.src.models.deberta_v3.deberta_v3_tokenizer import (
     DebertaV3Tokenizer,
 )
-from keras_nlp.src.models.densenet.densenet_backbone import DenseNetBackbone
-from keras_nlp.src.models.densenet.densenet_image_classifier import (
-    DenseNetImageClassifier,
-)
 from keras_nlp.src.models.deeplab_v3.deeplab_v3_backbone import (
     DeepLabV3Backbone,
 )
 from keras_nlp.src.models.deeplab_v3.deeplab_v3_segmenter import (
     DeepLabV3ImageSegmenter,
+)
+from keras_nlp.src.models.densenet.densenet_backbone import DenseNetBackbone
+from keras_nlp.src.models.densenet.densenet_image_classifier import (
+    DenseNetImageClassifier,
 )
 from keras_nlp.src.models.distil_bert.distil_bert_backbone import (
     DistilBertBackbone,
