2fb495310642464d37d8108dc40a3d0203e42594
Hongyu Chiu <20734616+james77777778@users.noreply.github.com>
2024-09-24 22:47:07 +0800

    Fix CI

--- keras_hub/src/models/stable_diffusion_3/stable_diffusion_3_text_to_image_preprocessor_test.py
+++ keras_hub/src/models/stable_diffusion_3/stable_diffusion_3_text_to_image_preprocessor_test.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
-
 import pytest
 
 from keras_hub.src.models.clip.clip_preprocessor import CLIPPreprocessor
