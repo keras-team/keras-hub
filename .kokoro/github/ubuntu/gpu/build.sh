69b40075a3f8dff3710b3873486d70acd0285a26
divyashreepathihalli <divyashreepathihalli@gmail.com>
2024-09-27 18:42:17 +0000

    remove keras 2 from gpu tests

--- .kokoro/github/ubuntu/gpu/build.sh
+++ .kokoro/github/ubuntu/gpu/build.sh
@@ -32,14 +32,7 @@ nvcc --version
 cd "src/github/keras-hub"
 pip install -U pip setuptools psutil
 
-if [ "${KERAS2:-0}" == "1" ]
-then
-   echo "Keras2 detected."
-   pip install -r requirements-common.txt --progress-bar off --timeout 1000
-   pip install tensorflow-text==2.15 tensorflow[and-cuda]~=2.15 keras-core \
-      --timeout 1000
-
-elif [ "$KERAS_BACKEND" == "tensorflow" ]
+if [ "$KERAS_BACKEND" == "tensorflow" ]
 then
    echo "TensorFlow backend detected."
    pip install -r requirements-tensorflow-cuda.txt --progress-bar off \
