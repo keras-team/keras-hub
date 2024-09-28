22131181a4a13e80e5d1d0acb753c368b3b0d596
divyashreepathihalli <divyashreepathihalli@gmail.com>
2024-09-28 07:19:55 +0000

    remove onemore copyright banner

--- tools/checkpoint_conversion/convert_sam_checkpoints.py
+++ tools/checkpoint_conversion/convert_sam_checkpoints.py
@@ -1,17 +1,3 @@
-# Copyright 2024 The KerasNLP Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import argparse
 import os
 
@@ -426,6 +412,10 @@ def port_weights(keras_model, torch_model):
 
 
 def main(args):
+    # URL to download checkpoints form
+    # https://dl.fbaipublicfiles.com/segment_anything/sam_vit_h_4b8939.pth
+    # https://dl.fbaipublicfiles.com/segment_anything/sam_vit_l_0b3195.pth
+    # https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth
     if args.model_variant == "base":
         print("Loading Keras model")
         keras_model = build_sam_base_model()
