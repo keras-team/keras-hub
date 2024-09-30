29c7f835ba5dd8288dba7f29202148b9f6e6be03
Gowtham Paimagam <gowthamkumarp@google.com>
2024-09-30 22:39:23 +0200

    Add ResNetVD presets

--- keras_hub/src/models/resnet/resnet_presets.py
+++ keras_hub/src/models/resnet/resnet_presets.py
@@ -79,4 +79,145 @@ backbone_presets = {
         },
         "kaggle_handle": "kaggle://kerashub/resnetv2/keras/resnet_v2_101_imagenet/3",
     },
+    "resnet_vd_18_imagenet": {
+        "metadata": {
+            "description": (
+                "18-layer ResNetVD (ResNet with bag of tricks) model "
+                "pre-trained on the ImageNet 1k dataset at a 224x224 "
+                "resolution."
+            ),
+            "params": 11722824,
+            "official_name": "ResNet",
+            "path": "resnet",
+            "model_card": "https://arxiv.org/abs/1812.01187",
+        },
+        "kaggle_handle": "kaggle://kerashub/resnetvd/keras/resnet18_vd",
+    },
+    "resnet_vd_34_imagenet": {
+        "metadata": {
+            "description": (
+                "34-layer ResNetVD (ResNet with bag of tricks) model "
+                "pre-trained on the ImageNet 1k dataset at a 224x224 "
+                "resolution."
+            ),
+            "params": 21838408,
+            "official_name": "ResNet",
+            "path": "resnet",
+            "model_card": "https://arxiv.org/abs/1812.01187",
+        },
+        "kaggle_handle": "kaggle://kerashub/resnetvd/keras/resnet34_vd",
+    },
+    "resnet_vd_50_imagenet": {
+        "metadata": {
+            "description": (
+                "50-layer ResNetVD (ResNet with bag of tricks) model "
+                "pre-trained on the ImageNet 1k dataset at a 224x224 "
+                "resolution."
+            ),
+            "params": 25629512,
+            "official_name": "ResNet",
+            "path": "resnet",
+            "model_card": "https://arxiv.org/abs/1812.01187",
+        },
+        "kaggle_handle": "kaggle://kerashub/resnetvd/keras/resnet50_vd",
+    },
+    "resnet_vd_50_ssld_imagenet": {
+        "metadata": {
+            "description": (
+                "50-layer ResNetVD (ResNet with bag of tricks) model "
+                "pre-trained on the ImageNet 1k dataset at a 224x224 "
+                "resolution with knowledge distillation."
+            ),
+            "params": 25629512,
+            "official_name": "ResNet",
+            "path": "resnet",
+            "model_card": "https://arxiv.org/abs/1812.01187",
+        },
+        "kaggle_handle": "kaggle://kerashub/resnetvd/keras/resnet50_vd_ssld",
+    },
+    "resnet_vd_50_ssld_v2_imagenet": {
+        "metadata": {
+            "description": (
+                "50-layer ResNetVD (ResNet with bag of tricks) model "
+                "pre-trained on the ImageNet 1k dataset at a 224x224 "
+                "resolution with knowledge distillation and AutoAugment."
+            ),
+            "params": 25629512,
+            "official_name": "ResNet",
+            "path": "resnet",
+            "model_card": "https://arxiv.org/abs/1812.01187",
+        },
+        "kaggle_handle": "kaggle://kerashub/resnetvd/keras/resnet50_vd_ssld_v2",
+    },
+    "resnet_vd_50_ssld_v2_fix_imagenet": {
+        "metadata": {
+            "description": (
+                "50-layer ResNetVD (ResNet with bag of tricks) model "
+                "pre-trained on the ImageNet 1k dataset at a 224x224 "
+                "resolution with knowledge distillation, AutoAugment and "
+                "additional fine-tuning of the classification head."
+            ),
+            "params": 25629512,
+            "official_name": "ResNet",
+            "path": "resnet",
+            "model_card": "https://arxiv.org/abs/1812.01187",
+        },
+        "kaggle_handle": "kaggle://kerashub/resnetvd/keras/fix_resnet50_vd_ssld_v2",
+    },
+    "resnet_vd_101_imagenet": {
+        "metadata": {
+            "description": (
+                "101-layer ResNetVD (ResNet with bag of tricks) model "
+                "pre-trained on the ImageNet 1k dataset at a 224x224 "
+                "resolution."
+            ),
+            "params": 44673864,
+            "official_name": "ResNet",
+            "path": "resnet",
+            "model_card": "https://arxiv.org/abs/1812.01187",
+        },
+        "kaggle_handle": "kaggle://kerashub/resnetvd/keras/resnet101_vd",
+    },
+    "resnet_vd_101_ssld_imagenet": {
+        "metadata": {
+            "description": (
+                "101-layer ResNetVD (ResNet with bag of tricks) model "
+                "pre-trained on the ImageNet 1k dataset at a 224x224 "
+                "resolution with knowledge distillation."
+            ),
+            "params": 44673864,
+            "official_name": "ResNet",
+            "path": "resnet",
+            "model_card": "https://arxiv.org/abs/1812.01187",
+        },
+        "kaggle_handle": "kaggle://kerashub/resnetvd/keras/resnet101_vd_ssld",
+    },
+    "resnet_vd_152_imagenet": {
+        "metadata": {
+            "description": (
+                "152-layer ResNetVD (ResNet with bag of tricks) model "
+                "pre-trained on the ImageNet 1k dataset at a 224x224 "
+                "resolution."
+            ),
+            "params": 60363592,
+            "official_name": "ResNet",
+            "path": "resnet",
+            "model_card": "https://arxiv.org/abs/1812.01187",
+        },
+        "kaggle_handle": "kaggle://kerashub/resnetvd/keras/resnet152_vd",
+    },
+    "resnet_vd_200_imagenet": {
+        "metadata": {
+            "description": (
+                "200-layer ResNetVD (ResNet with bag of tricks) model "
+                "pre-trained on the ImageNet 1k dataset at a 224x224 "
+                "resolution."
+            ),
+            "params": 74933064,
+            "official_name": "ResNet",
+            "path": "resnet",
+            "model_card": "https://arxiv.org/abs/1812.01187",
+        },
+        "kaggle_handle": "kaggle://kerashub/resnetvd/keras/resnet200_vd",
+    },
 }
