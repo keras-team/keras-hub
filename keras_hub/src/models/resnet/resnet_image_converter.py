b90150ded11edf0955e1d7e8685bef717b98283d
Matt Watson <1389937+mattdangerw@users.noreply.github.com>
2024-10-02 15:03:27 -0700

    Image classifier changes (#1901)
    * Image classifier changes
    
    - Move image classifier implementation to the base class.
    - Allow passing arbitrary layers as preprocessing.
    
    * Fix tests, address review
    
    * Add dropout
    
    * Add args for image converter

--- keras_hub/src/models/resnet/resnet_image_converter.py
+++ keras_hub/src/models/resnet/resnet_image_converter.py
@@ -1,10 +1,8 @@
 from keras_hub.src.api_export import keras_hub_export
-from keras_hub.src.layers.preprocessing.resizing_image_converter import (
-    ResizingImageConverter,
-)
+from keras_hub.src.layers.preprocessing.image_converter import ImageConverter
 from keras_hub.src.models.resnet.resnet_backbone import ResNetBackbone
 
 
 @keras_hub_export("keras_hub.layers.ResNetImageConverter")
-class ResNetImageConverter(ResizingImageConverter):
+class ResNetImageConverter(ImageConverter):
     backbone_cls = ResNetBackbone
