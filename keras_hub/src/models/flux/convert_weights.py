34e2477cbbb0eb208a40f462bd402a3bcd8df9c6
David Landup <david.landup.0@gmail.com>
2024-10-06 20:51:22 +0900

    add license

--- keras_hub/src/models/flux/convert_weights.py
+++ keras_hub/src/models/flux/convert_weights.py
@@ -1,3 +1,16 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 def convert_mlpembedder_weights(pytorch_model, keras_model):
     pytorch_in_layer_weight = (
         pytorch_model.in_layer.weight.detach().cpu().numpy()
@@ -140,7 +153,7 @@ def convert_flux_weights(pytorch_model, keras_model):
     convert_mlpembedder_weights(pytorch_model.vector_in, keras_model.vector_in)
 
     # Convert guidance_in (if present)
-    if keras_model.params.guidance_embed:
+    if keras_model.guidance_embed:
         convert_mlpembedder_weights(
             pytorch_model.guidance_in, keras_model.guidance_in
         )
