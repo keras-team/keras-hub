e3c6dc6cdaf372ff602730ecb3de54afb1f2beb3
David Landup <david.landup.0@gmail.com>
2024-10-08 18:56:59 +0900

    resizing image converter -> ImageConverter

--- keras_hub/src/models/mix_transformer/mix_transformer_image_converter.py
+++ keras_hub/src/models/mix_transformer/mix_transformer_image_converter.py
@@ -1,10 +1,8 @@
 from keras_hub.src.api_export import keras_hub_export
-from keras_hub.src.layers.preprocessing.resizing_image_converter import (
-    ResizingImageConverter,
-)
+from keras_hub.src.layers.preprocessing.image_converter import ImageConverter
 from keras_hub.src.models.mix_transformer import MiTBackbone
 
 
 @keras_hub_export("keras_hub.layers.MiTImageConverter")
-class MiTImageConverter(ResizingImageConverter):
+class MiTImageConverter(ImageConverter):
     backbone_cls = MiTBackbone
