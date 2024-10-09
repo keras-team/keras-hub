f083c83701bc2553d3eff7958208c52246438782
Octavio Arriaga <arriaga.camargo@gmail.com>
2024-10-09 18:25:50 +0200

    Update conversion script to work for all YOLOV8 backbones

--- tools/checkpoint_conversion/convert_yolov8_checkpoints.py
+++ tools/checkpoint_conversion/convert_yolov8_checkpoints.py
@@ -1,19 +1,20 @@
 from pathlib import Path
+
 import numpy as np
 from keras import ops
 from keras_cv.models import YOLOV8Backbone as KerasCVYOLOV8Backbone
 from keras_hub.models import YOLOV8Backbone
 
 
-def validate_numerics(rng, model_A, model_B):
+def validate_numerics(rng, preset_name, model_A, model_B):
     random_data = rng.random((2, 224, 224, 3))
     output_A = model_A(random_data)
     output_B = model_B(random_data)
     output_A = ops.convert_to_numpy(output_A)
     output_B = ops.convert_to_numpy(output_B)
-    all_close = np.allclose(output_A, output_B)
-    assert all_close
-    return all_close
+    is_valid = np.allclose(output_A, output_B)
+    print(f"Port {preset_name} with valid numerics: {is_valid}")
+    assert is_valid
 
 
 def make_directory(root, preset):
@@ -22,34 +23,45 @@ def make_directory(root, preset):
     return preset_path_name
 
 
-def port_and_validate(rng, weights_path, preset_name):
+def pass_weights_A_to_B(model_A, model_B, root_path):
+    weights_filepath = f"{root_path}.weights.h5"
+    model_A.save_weights(weights_filepath)
+    model_B.load_weights(weights_filepath)
+    return model_A, model_B
+
+
+def convert_backbone(ModelA, ModelB, weights_path, preset_name):
     preset_path = make_directory(weights_path, preset_name)
-
-    model_A = KerasCVYOLOV8Backbone.from_preset(preset_name)
-    model_B = YOLOV8Backbone(**model_A.get_config())
-
-    weights_name = f"{weights_path}/{preset_name}.weights.h5"
-    model_A.save_weights(weights_name)
-    model_B.load_weights(weights_name)
+    model_A = ModelA.from_preset(preset_name)
+    model_B = ModelB(**model_A.get_config())
+    model_A, model_B = pass_weights_A_to_B(model_A, model_B, preset_path)
     model_B.save_to_preset(preset_path)
-    is_valid = validate_numerics(rng, model_A, model_B)
-    print(f"Port {preset_name} is {is_valid}")
+    return model_A, model_B
 
 
 if __name__ == "__main__":
     import argparse
+    from functools import partial
     description = "Convert YOLOV8 keras-cv to keras-hub weights."
     parser = argparse.ArgumentParser(description=description)
-    parser.add_argument("--weights_path", type=str, default="YOLOV8")
     parser.add_argument("--seed", type=int, default=777)
+    parser.add_argument("--weights_path", type=str, default="YOLOV8")
     args = parser.parse_args()
     rng = np.random.default_rng(args.seed)
-    presets = [
+    backbone_presets = [
+        "yolo_v8_xs_backbone",
+        "yolo_v8_s_backbone",
+        "yolo_v8_m_backbone",
+        "yolo_v8_l_backbone",
+        "yolo_v8_xl_backbone",
         "yolo_v8_xs_backbone_coco",
         "yolo_v8_s_backbone_coco",
         "yolo_v8_m_backbone_coco",
         "yolo_v8_l_backbone_coco",
         "yolo_v8_xl_backbone_coco"
     ]
-    for preset in presets:
-        port_and_validate(rng, args.weights_path, preset)
+
+    convert = partial(convert_backbone, KerasCVYOLOV8Backbone, YOLOV8Backbone)
+    for preset in backbone_presets:
+        model_A, model_B = convert(args.weights_path, preset)
+        validate_numerics(rng, preset, model_A, model_B)
