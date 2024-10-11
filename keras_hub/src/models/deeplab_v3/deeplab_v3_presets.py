7ba7e3c3fd4fde8f3150df6807324350d9f6be2b
Sachin Prasad <sachinprasad@google.com>
2024-10-11 10:59:57 -0700

    Add training details

--- keras_hub/src/models/deeplab_v3/deeplab_v3_presets.py
+++ keras_hub/src/models/deeplab_v3/deeplab_v3_presets.py
@@ -5,7 +5,8 @@ backbone_presets = {
         "metadata": {
             "description": (
                 "DeepLabV3+ model with ResNet50 as image encoder and trained on "
-                "augmented Pascal VOC dataset by Semantic Boundaries Dataset(SBD)."
+                "augmented Pascal VOC dataset by Semantic Boundaries Dataset(SBD)"
+                "which is having categorical accuracy of 90.01 and 0.63 Mean IoU."
             ),
             "params": 39190656,
             "official_name": "DeepLabV3",
