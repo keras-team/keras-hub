5092d717f9bbcbf1d9eaf0deb3527a447d67367e
David Landup <david.landup.0@gmail.com>
2024-10-17 13:16:13 +0900

    remove backbone presets, remove copyrights, remove backbone cls from segmenter

--- keras_hub/src/models/segformer/segformer_backbone.py
+++ keras_hub/src/models/segformer/segformer_backbone.py
@@ -1,33 +1,7 @@
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import keras
 
 from keras_hub.src.api_export import keras_hub_export
 from keras_hub.src.models.backbone import Backbone
-from keras_hub.src.models.mix_transformer.mix_transformer_backbone import (
-    MiTBackbone,
-)
 
 
 @keras_hub_export("keras_hub.models.SegFormerBackbone")
@@ -89,8 +63,6 @@ class SegFormerBackbone(Backbone):
     ```
 
     """
-
-    backbone_cls = MiTBackbone
 
     def __init__(
         self,
