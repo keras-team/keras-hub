0c3235ccfde4996b96c6cf05f83d98a6807243c2
Gowtham Paimagam <gowthamkumarp@google.com>
2024-10-22 21:26:24 +0200

    Refactorings for `DifferentialBinarization`

--- keras_hub/src/models/differential_binarization/differential_binarization.py
+++ keras_hub/src/models/differential_binarization/differential_binarization.py
@@ -16,13 +16,13 @@ import math
 
 import keras
 from keras import layers
-from keras_hub.src.api_export import keras_nlp_export
+from keras_hub.src.api_export import keras_hub_export
 from keras_hub.src.models.task import Task
 
 from keras_hub.src.models.differential_binarization.losses import DBLoss
 
 
-@keras_nlp_export("keras_hub.models.DifferentialBinarization")
+@keras_hub_export("keras_hub.models.DifferentialBinarization")
 class DifferentialBinarization(Task):
     """
     A Keras model implementing the Differential Binarization
@@ -31,17 +31,17 @@ class DifferentialBinarization(Task):
     https://arxiv.org/abs/1911.08947).
 
     Args:
-        backbone: A `keras_hub.models.ResNetBackbone` instance.
-        fpn_channels: int. The number of channels to output by the Feature
-            Pyramid Network. Defaults to 256.
-        fpn_kernel_list: list of ints. The number of filters for probability
+        backbone: A `keras_hub.models.DifferentialBinarizationBackbone`
+            instance.
+        head_kernel_list: list of ints. The number of filters for probability
             and threshold maps. Defaults to [3, 2, 2].
 
     Examples:
     ```python
     input_data = np.ones(shape=(8, 224, 224, 3))
 
-    backbone = keras_hub.models.ResNetBackbone.from_preset("resnet50_vd")
+    image_encoder = keras_hub.models.ResNetBackbone.from_preset("resnet50_vd")
+    backbone = keras_hub.models.DifferentialBinarizationBackbone(image_encoder)
     detector = keras_hub.models.DifferentialBinarization(
         backbone=backbone
     )
@@ -53,26 +53,23 @@ class DifferentialBinarization(Task):
     def __init__(
         self,
         backbone,
-        fpn_channels=256,
         head_kernel_list=[3, 2, 2],
         step_function_k=50.0,
         preprocessor=None,  # adding this dummy arg for saved model test
         **kwargs,
     ):
-        backbone = backbone
 
         inputs = backbone.input
-        x = backbone.pyramid_outputs
-        x = diffbin_fpn_model(x, out_channels=fpn_channels)
+        x = backbone(inputs)
         probability_maps = diffbin_head(
             x,
-            in_channels=fpn_channels,
+            in_channels=backbone.fpn_channels,
             kernel_list=head_kernel_list,
             name="head_prob",
         )
         threshold_maps = diffbin_head(
             x,
-            in_channels=fpn_channels,
+            in_channels=backbone.fpn_channels,
             kernel_list=head_kernel_list,
             name="head_thresh",
         )
@@ -86,7 +83,6 @@ class DifferentialBinarization(Task):
         super().__init__(inputs=inputs, outputs=outputs, **kwargs)
 
         self.backbone = backbone
-        self.fpn_channels = fpn_channels
         self.head_kernel_list = head_kernel_list
         self.step_function_k = step_function_k
 
@@ -133,65 +129,11 @@ class DifferentialBinarization(Task):
         config = super().get_config()
         config.update(
             {
-                "fpn_channels": self.fpn_channels,
                 "head_kernel_list": self.head_kernel_list,
                 "step_function_k": self.step_function_k,
             }
         )
         return config
-
-
-def diffbin_fpn_model(inputs, out_channels):
-    in2 = layers.Conv2D(
-        out_channels, kernel_size=1, use_bias=False, name="neck_in2"
-    )(inputs["P2"])
-    in3 = layers.Conv2D(
-        out_channels, kernel_size=1, use_bias=False, name="neck_in3"
-    )(inputs["P3"])
-    in4 = layers.Conv2D(
-        out_channels, kernel_size=1, use_bias=False, name="neck_in4"
-    )(inputs["P4"])
-    in5 = layers.Conv2D(
-        out_channels, kernel_size=1, use_bias=False, name="neck_in5"
-    )(inputs["P5"])
-    out4 = layers.Add(name="add1")([layers.UpSampling2D()(in5), in4])
-    out3 = layers.Add(name="add2")([layers.UpSampling2D()(out4), in3])
-    out2 = layers.Add(name="add3")([layers.UpSampling2D()(out3), in2])
-    p5 = layers.Conv2D(
-        out_channels // 4,
-        kernel_size=3,
-        padding="same",
-        use_bias=False,
-        name="neck_p5",
-    )(in5)
-    p4 = layers.Conv2D(
-        out_channels // 4,
-        kernel_size=3,
-        padding="same",
-        use_bias=False,
-        name="neck_p4",
-    )(out4)
-    p3 = layers.Conv2D(
-        out_channels // 4,
-        kernel_size=3,
-        padding="same",
-        use_bias=False,
-        name="neck_p3",
-    )(out3)
-    p2 = layers.Conv2D(
-        out_channels // 4,
-        kernel_size=3,
-        padding="same",
-        use_bias=False,
-        name="neck_p2",
-    )(out2)
-    p5 = layers.UpSampling2D((8, 8))(p5)
-    p4 = layers.UpSampling2D((4, 4))(p4)
-    p3 = layers.UpSampling2D((2, 2))(p3)
-
-    fused = layers.Concatenate(axis=-1)([p5, p4, p3, p2])
-    return fused
-
 
 def step_function(x, y, k):
     return 1.0 / (1.0 + keras.ops.exp(-k * (x - y)))
---
+++ keras_hub/src/models/differential_binarization/differential_binarization_backbone.py
@@ -0,0 +1,105 @@
+# Copyright 2024 The KerasNLP Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from keras import layers
+from keras_hub.src.api_export import keras_hub_export
+from keras_hub.src.models.backbone import Backbone
+
+
+@keras_hub_export("keras_hub.models.DifferentialBinarizationBackbone")
+class DifferentialBinarization(Backbone):
+    """
+    A Keras model implementing the Differential Binarization
+    architecture for scene text detection, described in
+    [Real-time Scene Text Detection with Differentiable Binarization](
+    https://arxiv.org/abs/1911.08947).
+
+    Args:
+        image_encoder: A `keras_hub.models.ResNetBackbone` instance.
+
+    """
+
+    def __init__(
+        self,
+        image_encoder,
+        fpn_channels=256,
+        **kwargs,
+    ):
+        inputs = image_encoder.input
+        x = image_encoder.pyramid_outputs
+        x = diffbin_fpn_model(x, out_channels=fpn_channels)
+
+        super().__init__(inputs=inputs, outputs=x, **kwargs)
+
+        self.image_encoder = image_encoder
+        self.fpn_channels = fpn_channels
+
+    def get_config(self):
+        config = super().get_config()
+        config["fpn_channels"] = self.fpn_channels
+        config["image_encoder"] = self.image_encoder
+        return config
+
+
+def diffbin_fpn_model(inputs, out_channels):
+    in2 = layers.Conv2D(
+        out_channels, kernel_size=1, use_bias=False, name="neck_in2"
+    )(inputs["P2"])
+    in3 = layers.Conv2D(
+        out_channels, kernel_size=1, use_bias=False, name="neck_in3"
+    )(inputs["P3"])
+    in4 = layers.Conv2D(
+        out_channels, kernel_size=1, use_bias=False, name="neck_in4"
+    )(inputs["P4"])
+    in5 = layers.Conv2D(
+        out_channels, kernel_size=1, use_bias=False, name="neck_in5"
+    )(inputs["P5"])
+    out4 = layers.Add(name="add1")([layers.UpSampling2D()(in5), in4])
+    out3 = layers.Add(name="add2")([layers.UpSampling2D()(out4), in3])
+    out2 = layers.Add(name="add3")([layers.UpSampling2D()(out3), in2])
+    p5 = layers.Conv2D(
+        out_channels // 4,
+        kernel_size=3,
+        padding="same",
+        use_bias=False,
+        name="neck_p5",
+    )(in5)
+    p4 = layers.Conv2D(
+        out_channels // 4,
+        kernel_size=3,
+        padding="same",
+        use_bias=False,
+        name="neck_p4",
+    )(out4)
+    p3 = layers.Conv2D(
+        out_channels // 4,
+        kernel_size=3,
+        padding="same",
+        use_bias=False,
+        name="neck_p3",
+    )(out3)
+    p2 = layers.Conv2D(
+        out_channels // 4,
+        kernel_size=3,
+        padding="same",
+        use_bias=False,
+        name="neck_p2",
+    )(out2)
+    p5 = layers.UpSampling2D((8, 8))(p5)
+    p4 = layers.UpSampling2D((4, 4))(p4)
+    p3 = layers.UpSampling2D((2, 2))(p3)
+
+    fused = layers.Concatenate(axis=-1)([p5, p4, p3, p2])
+    return fused
+
