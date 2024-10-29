55af01da705b93fb737d9d3fdd78b169051e9112
David Landup <david.landup.0@gmail.com>
2024-10-29 15:15:41 +0900

    update kerashub links to keras links

--- keras_hub/src/models/t5/t5_presets.py
+++ keras_hub/src/models/t5/t5_presets.py
@@ -22,7 +22,7 @@ backbone_presets = {
             "path": "t5",
             "model_card": "https://github.com/google-research/text-to-text-transfer-transformer/blob/main/README.md",
         },
-        "kaggle_handle": "kaggle://kerashub/t5-1.1/keras/t5_1.1_small",
+        "kaggle_handle": "kaggle://keras/t5/keras/t5_1.1_small",
     },
     "t5_base_multi": {
         "metadata": {
@@ -45,7 +45,7 @@ backbone_presets = {
             "path": "t5",
             "model_card": "https://github.com/google-research/text-to-text-transfer-transformer/blob/main/README.md",
         },
-        "kaggle_handle": "kaggle://kerashub/t5-1.1/keras/t5_1.1_base",
+        "kaggle_handle": "kaggle://keras/t5/keras/t5_1.1_base",
     },
     "t5_large_multi": {
         "metadata": {
@@ -68,7 +68,7 @@ backbone_presets = {
             "path": "t5",
             "model_card": "https://github.com/google-research/text-to-text-transfer-transformer/blob/main/README.md",
         },
-        "kaggle_handle": "kaggle://kerashub/t5-1.1/keras/t5_1.1_large",
+        "kaggle_handle": "kaggle://keras/t5/keras/t5_1.1_large",
     },
     "flan_small_multi": {
         "metadata": {
