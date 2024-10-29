f15b7b90822d44ce174e4de4ce9f0a37146bb3ed
Gowtham Paimagam <gowthamkumarp@google.com>
2024-10-29 21:19:21 +0100

    Add presets

---
+++ keras_hub/src/models/differential_binarization/differential_binarization_presets.py
@@ -0,0 +1,15 @@
+"""Differential Binarization preset configurations."""
+
+backbone_presets = {
+    "diffbin_r50vd_icdar2015": {
+        "metadata": {
+            "description": ("Differential Binarization using 50-layer"
+                            "ResNetVD trained on the ICDAR2015 dataset."),
+            "params": 25482722,
+            "official_name": "DifferentialBinarization",
+            "path": "differential_binarization",
+            "model_card": "https://arxiv.org/abs/1911.08947",
+        },
+        "kaggle_handle": "", # TODO
+    }
+}
