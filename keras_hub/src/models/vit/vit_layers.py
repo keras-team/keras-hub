618e163cb5b8bcc6d36fcc5e44c89acbd4560d48
Sravana Neeli <sineeli@google.com>
2024-11-19 11:25:17 -0800

    correct position ids

--- keras_hub/src/models/vit/vit_layers.py
+++ keras_hub/src/models/vit/vit_layers.py
@@ -106,15 +106,8 @@ class ViTPatchingAndEmbedding(keras.layers.Layer):
             name="position_embedding",
         )
         self.position_embedding.build([1, self.num_positions])
-        self.position_ids = self.add_weight(
-            shape=(1, self.num_positions),
-            initializer="zeros",
-            # Let the backend determine the int dtype. For example, tf
-            # requires int64 for correct device placement, whereas jax and torch
-            # don't.
-            dtype=int,
-            trainable=False,
-            name="position_ids",
+        self.position_ids = keras.ops.expand_dims(
+            keras.ops.arange(self.num_positions), axis=0
         )
         self.built = True
 
