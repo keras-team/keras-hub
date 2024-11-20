4025500f21e2bdb69f631ec270387c7bfdc604ad
pkgoogle <132095473+pkgoogle@users.noreply.github.com>
2024-11-20 20:35:43 +0000

    EfficientNet: Add lite0 variant (#1968)
    * add lite0 variant
    
    * format pass
    
    * added to preset file, fix uri
    
    * fix parameter regression
    
    * format fix
    
    * fix merge regression

--- keras_hub/src/models/efficientnet/efficientnet_presets.py
+++ keras_hub/src/models/efficientnet/efficientnet_presets.py
@@ -163,4 +163,17 @@ backbone_presets = {
         },
         "kaggle_handle": "kaggle://keras/efficientnet/keras/efficientnet_es_ra_imagenet/1",
     },
+    "efficientnet_lite0_ra_imagenet": {
+        "metadata": {
+            "description": (
+                "EfficientNet-Lite model fine-trained on the ImageNet 1k dataset "
+                "with RandAugment recipe."
+            ),
+            "params": 4652008,
+            "official_name": "EfficientNet",
+            "path": "efficientnet",
+            "model_card": "https://arxiv.org/abs/1905.11946",
+        },
+        "kaggle_handle": "kaggle://keras/efficientnet/keras/efficientnet_lite0_ra_imagenet",
+    },
 }
