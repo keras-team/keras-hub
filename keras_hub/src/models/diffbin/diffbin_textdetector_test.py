f047012ddc3b6e659667843256e7f26aa3bf8aed
Gowtham Paimagam <gowthamkumarp@google.com>
2025-01-27 16:25:41 +0100

    `diffbin_ocr_test.py` -> `diffbin_textdetector_test.py`

--- keras_hub/src/models/diffbin/diffbin_textdetector_test.py
+++ keras_hub/src/models/diffbin/diffbin_textdetector_test.py
@@ -2,15 +2,17 @@ import numpy as np
 import pytest
 
 from keras_hub.src.models.diffbin.diffbin_backbone import DiffBinBackbone
-from keras_hub.src.models.diffbin.diffbin_ocr import DiffBinOCR
 from keras_hub.src.models.diffbin.diffbin_preprocessor import (
     DiffBinPreprocessor,
+)
+from keras_hub.src.models.diffbin.diffbin_textdetector import (
+    DiffBinImageTextDetector,
 )
 from keras_hub.src.models.resnet.resnet_backbone import ResNetBackbone
 from keras_hub.src.tests.test_case import TestCase
 
 
-class DiffBinOCRTest(TestCase):
+class DiffBinImageTextDetectorTest(TestCase):
     def setUp(self):
         self.images = np.ones((2, 32, 32, 3))
         self.labels = np.concatenate(
@@ -35,7 +37,7 @@ class DiffBinOCRTest(TestCase):
 
     def test_basics(self):
         self.run_task_test(
-            cls=DiffBinOCR,
+            cls=DiffBinImageTextDetector,
             init_kwargs=self.init_kwargs,
             train_data=self.train_data,
             expected_output_shape=(2, 32, 32, 3),
@@ -44,22 +46,22 @@ class DiffBinOCRTest(TestCase):
     @pytest.mark.large
     def test_saved_model(self):
         self.run_model_saving_test(
-            cls=DiffBinOCR,
+            cls=DiffBinImageTextDetector,
             init_kwargs=self.init_kwargs,
             input_data=self.images,
         )
 
     def test_end_to_end_model_predict(self):
-        model = DiffBinOCR(**self.init_kwargs)
+        model = DiffBinImageTextDetector(**self.init_kwargs)
         outputs = model.predict(self.images)
         self.assertAllEqual(outputs.shape, (2, 32, 32, 3))
 
     @pytest.mark.skip(reason="disabled until preset's been uploaded to Kaggle")
     @pytest.mark.extra_large
     def test_all_presets(self):
-        for preset in DiffBinOCR.presets:
+        for preset in DiffBinImageTextDetector.presets:
             self.run_preset_test(
-                cls=DiffBinOCR,
+                cls=DiffBinImageTextDetector,
                 preset=preset,
                 input_data=self.images,
                 expected_output_shape=(2, 32, 32, 3),
