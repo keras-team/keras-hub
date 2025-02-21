3bdf9eade938f5d321409bae48099cff9501942f
Laxma Reddy Patlolla <laxmareddyp@google.com>
2025-02-21 11:54:48 -0800

    adapted the latest preset format style

--- keras_hub/src/models/mobilenet/mobilenet_presets.py
+++ keras_hub/src/models/mobilenet/mobilenet_presets.py
@@ -7,9 +7,9 @@ backbone_presets = {
                 "Small MobileNet V3 model pre-trained on the ImageNet 1k "
                 "dataset at a 224x224 resolution."
             ),
-            "official_name": "MobileNet",
-            "path": "mobilenet3",
+            "params": 278784,
+            "path": "mobilenetv3",
         },
-        "kaggle_handle": "kaggle://keras/mobilenetv3/keras/mobilenet_v3_small_050_imagenet",
+        "kaggle_handle": "kaggle://keras/mobilenetv3/keras/mobilenet_v3_small_050_imagenet/1",
     },
 }
