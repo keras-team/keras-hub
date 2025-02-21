36e353bdd495129996afc0cc810b10af4e1887d3
Piseth Ky <pisethk@google.com>
2025-02-21 10:42:01 -0800

    update/add mobilenet presets

--- keras_hub/src/models/mobilenet/mobilenet_presets.py
+++ keras_hub/src/models/mobilenet/mobilenet_presets.py
@@ -5,11 +5,47 @@ backbone_presets = {
         "metadata": {
             "description": (
                 "Small Mobilenet V3 model pre-trained on the ImageNet 1k "
-                "dataset at a 224x224 resolution."
+                "dataset at a 224x224 resolution. Has half channel multiplier."
             ),
             "params": 278784,
             "path": "mobilenetv3",
         },
         "kaggle_handle": "kaggle://keras/mobilenetv3/keras/mobilenet_v3_small_050_imagenet/1",
+    },
+    "mobilenetv3_small_100": {
+        "metadata": {
+            "description": (
+                "Small Mobilenet V3 model pre-trained on the ImageNet 1k "
+                "dataset at a 224x224 resolution. Has baseline channel "
+                "multiplier."
+            ),
+            "params": 939120,
+            "path": "mobilenetv3",
+        },
+        "kaggle_handle": "kaggle://keras/mobilenetv3/keras/mobilenet_v3_small_100_imagenet/1",
+    },
+    "mobilenetv3_large_100.ra_in1k": {
+        "metadata": {
+            "description": (
+                "Large Mobilenet V3 model pre-trained on the ImageNet 1k "
+                "dataset at a 224x224 resolution. Has baseline channel "
+                "multiplier."
+            ),
+            "params": 2996352,
+            "path": "mobilenetv3",
+        },
+        "kaggle_handle": "kaggle://keras/mobilenetv3/keras/mobilenet_v3_large_100_imagenet/1",
+    },
+    "mobilenetv3_large_100.miil_in21k_ft_in1k": {
+        "metadata": {
+            "description": (
+                "Large Mobilenet V3 model pre-trained on the ImageNet 21k "
+                "dataset at a 224x224 resolution. Has baseline channel "
+                "multiplier."
+            ),
+            "params": 2996352,
+            "path": "mobilenetv3",
+        },
+        "kaggle_handle": "kaggle://keras/mobilenetv3/keras/mobilenet_v3_large_100_imagenet_21k/1",
     },
 }
