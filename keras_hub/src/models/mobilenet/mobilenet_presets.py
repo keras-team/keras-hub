dace0adf0e8517681eb75833196ad44048ffb00e
Laxma Reddy Patlolla <laxmareddyp@google.com>
2025-02-21 11:40:36 -0800

    Fixing import in init file and preset name update

--- keras_hub/src/models/mobilenet/mobilenet_presets.py
+++ keras_hub/src/models/mobilenet/mobilenet_presets.py
@@ -1,15 +1,15 @@
 """MobileNet preset configurations."""
 
 backbone_presets = {
-    "mobilenetv3_small_050": {
+    "mobilenet_v3_small_050_imagenet": {
         "metadata": {
             "description": (
-                "Small Mobilenet V3 model pre-trained on the ImageNet 1k "
+                "Small MobileNet V3 model pre-trained on the ImageNet 1k "
                 "dataset at a 224x224 resolution."
             ),
-            "params": 278784,
-            "path": "mobilenetv3",
+            "official_name": "MobileNet",
+            "path": "mobilenet3",
         },
-        "kaggle_handle": "kaggle://keras/mobilenetv3/keras/mobilenet_v3_small_050_imagenet/1",
+        "kaggle_handle": "kaggle://keras/mobilenetv3/keras/mobilenet_v3_small_050_imagenet",
     },
 }
