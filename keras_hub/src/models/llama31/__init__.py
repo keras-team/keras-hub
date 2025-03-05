39b29e700e864529509268a3b39c03fe4890d062
RAHUL KUMAR <hellorahul@google.com>
2025-03-05 12:01:33 +0530

    Update __init__.py

--- keras_hub/src/models/llama31/__init__.py
+++ keras_hub/src/models/llama31/__init__.py
@@ -1,5 +1,5 @@
-from keras_hub.src.models.llama3.llama3_backbone import Llama3Backbone
-from keras_hub.src.models.llama3.llama3_presets import backbone_presets
+from keras_hub.src.models.llama31.llama31_backbone import Llama31Backbone
+from keras_hub.src.models.llama31.llama31_presets import backbone_presets
 from keras_hub.src.utils.preset_utils import register_presets
 
-register_presets(backbone_presets, Llama3Backbone)
+register_presets(backbone_presets, Llama31Backbone)
