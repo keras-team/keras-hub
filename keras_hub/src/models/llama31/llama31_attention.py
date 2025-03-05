9b38b25472bf14f298b5661cf7582596057f3bb4
RAHUL KUMAR <hellorahul@google.com>
2025-03-05 12:45:16 +0530

    Update llama31_attention.py

--- keras_hub/src/models/llama31/llama31_attention.py
+++ keras_hub/src/models/llama31/llama31_attention.py
@@ -1,8 +1,9 @@
 import keras_hub
 from keras_hub.src.models.llama31.llama3_rotary_embedding import Llama31RotaryEmbedding
 from keras_hub.src.utils.keras_utils import clone_initializer
+from keras_hub.src.models.llama.llama_attention import LlamaAttention
 
-class Llama31Attention(keras_hub.models.llama.llama_attention.LlamaAttention):
+class Llama31Attention(LlamaAttention):
     """A cached grounded query attention layer with sliding window."""
 
     def __init__(
