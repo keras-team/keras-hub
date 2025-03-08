7a7a6bdb931ebbd946431f63de3e24e1faa7bc37
Divyashree Sreepathihalli <divyashreepathihalli@gmail.com>
2025-03-07 20:36:59 -0800

    update Gemma attention for TPU (#2130)
    * update Gemma attention for TPU
    
    * add default fallback for GPU and CPU
    
    * add fallback option if not running with JAX and TPU
    
    * address review comments
    
    * check input signature
    
    * remove checking q length
    
    * code reformat
    
    * handle case when soft cap support is not needed
    
    * fix format
    
    * add tests for FA calls
    
    * fix test
    
    * update tests
    
    * fix code format
    
    * address review comments
    
    * Update requirements-jax-cuda.txt
    
    * Update gemma_causal_lm_test.py
    
    * Update requirements-jax-cuda.txt

--- keras_hub/src/models/gemma/gemma_attention.py
+++ keras_hub/src/models/gemma/gemma_attention.py
@@ -1,3 +1,5 @@
+import inspect
+
 import keras
 import numpy as np
 from keras import ops
@@ -5,6 +7,7 @@ from keras import ops
 from keras_hub.src.layers.modeling.rotary_embedding import RotaryEmbedding
 from keras_hub.src.utils.keras_utils import clone_initializer
 from keras_hub.src.utils.keras_utils import has_flash_attention_support
+from keras_hub.src.utils.keras_utils import running_on_tpu
 
 
 class CachedGemmaAttention(keras.layers.Layer):
@@ -103,6 +106,18 @@ class CachedGemmaAttention(keras.layers.Layer):
         )
         return x
 
+    def _can_use_flash_attention(self):
+        if not has_flash_attention_support():
+            return False
+        if self.dropout > 0.0:
+            return False
+        if self.logit_soft_cap is None:
+            return True
+        sig = inspect.signature(ops.dot_product_attention)
+        # We can currently only run soft capped attention for keras >= 3.10
+        # and only on TPU.
+        return running_on_tpu() and "attn_logits_soft_cap" in sig.parameters
+
     def _compute_attention(
         self,
         q,
@@ -118,27 +133,23 @@ class CachedGemmaAttention(keras.layers.Layer):
             query_normalization = 1 / np.sqrt(
                 self.hidden_dim // self.num_query_heads
             )
-        use_dot_product_attention = not (
-            self.dropout > 0.0 or (len(q.shape) != 4)
-        )
-        if has_flash_attention_support() and use_dot_product_attention:
-            if self.dropout > 0.0:
-                raise ValueError(
-                    "Flash attention does not support dropout. "
-                    "Please set `dropout` to 0.0."
-                )
+        if self._can_use_flash_attention():
             if attention_mask is not None:
                 attention_mask = ops.expand_dims(attention_mask, axis=1)
                 attention_mask = ops.cast(attention_mask, dtype="bool")
-
-            attention_output = ops.dot_product_attention(
+            # Only pass soft cap if needed as not all keras versions support.
+            if self.logit_soft_cap:
+                kwargs = {"attn_logits_soft_cap": self.logit_soft_cap}
+            else:
+                kwargs = {}
+            return ops.dot_product_attention(
                 query=q,
                 key=k,
                 value=v,
                 mask=attention_mask,
                 scale=query_normalization,
+                **kwargs,
             )
-            return attention_output
 
         q *= ops.cast(query_normalization, dtype=q.dtype)
         q_shape = ops.shape(q)
