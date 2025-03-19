0926790097e30cc127b2fc8c40c44ffdd7b5fc5e
Abheesht Sharma <abheesht@google.com>
2025-03-20 01:40:41 +0530

    Fix #params

--- keras_hub/src/models/gemma3/gemma3_presets.py
+++ keras_hub/src/models/gemma3/gemma3_presets.py
@@ -2,10 +2,10 @@
 
 # Metadata for loading pretrained model weights.
 backbone_presets = {
-    "gemma_instruct_4b": {
+    "gemma3_instruct_4b": {
         "metadata": {
             "description": "4 billion parameter, 18-layer, Gemma model.",
-            "params": 2506172416,
+            "params": 4299915632,
             "path": "gemma",
         },
         "kaggle_handle": "kaggle://abheesht75/gemma3/keras/gemma3_instruct_4b/1",
