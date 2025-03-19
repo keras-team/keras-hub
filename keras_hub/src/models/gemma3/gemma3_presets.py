baf7a9b2a272ee3d862ee5542044b51d857e1ceb
Abheesht Sharma <abheesht@google.com>
2025-03-20 01:18:28 +0530

    Add temp preset

---
+++ keras_hub/src/models/gemma3/gemma3_presets.py
@@ -0,0 +1,13 @@
+"""Gemma model preset configurations."""
+
+# Metadata for loading pretrained model weights.
+backbone_presets = {
+    "gemma_instruct_4b": {
+        "metadata": {
+            "description": "4 billion parameter, 18-layer, Gemma model.",
+            "params": 2506172416,
+            "path": "gemma",
+        },
+        "kaggle_handle": "kaggle://abheesht/gemma3/keras/gemma3_instruct_4b/1",
+    },
+}
