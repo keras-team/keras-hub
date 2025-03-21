81ffaafa637133e1e610fa3a36ac521ff821d262
pass_lin <935499957@qq.com>
2025-03-21 12:25:19 +0800

    remove RMSnorm form keras

--- keras_hub/src/models/roformerV2/roformerV2_attention.py
+++ keras_hub/src/models/roformerV2/roformerV2_attention.py
@@ -2,11 +2,35 @@ import keras
 from keras import initializers
 from keras import ops
 
-from keras_hub.src.models.llama.llama_layernorm import LlamaLayerNorm
 
+class RoformerNorm(keras.layers.Layer):
+    """A normalization layer for Llama that implements RMS normalization."""
 
-class RoformerNorm(LlamaLayerNorm):
-    pass
+    def __init__(self, epsilon=1e-6, **kwargs):
+        super().__init__(**kwargs)
+        self.epsilon = epsilon
+
+    def build(self, input_shape):
+        dim = input_shape[-1]
+        self.scale = self.add_weight(
+            name="scale",
+            trainable=True,
+            shape=(dim,),
+            initializer="ones",
+            dtype=self.variable_dtype,
+        )
+        self.built = True
+
+    def call(self, x):
+        x = ops.cast(x, "float32")
+        var = ops.mean(ops.power(x, 2), axis=-1, keepdims=True)
+        x = x * ops.rsqrt(var + self.epsilon)
+        return ops.cast(x * self.scale, self.compute_dtype)
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"epsilon": self.epsilon})
+        return config
 
 
 class RoformrPositionalEmbedding(keras.layers.Layer):
