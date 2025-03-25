04fae8bfe62cd9d9a7f772191a6275f2e4956ec1
kernel_loophole <p190119@nu.edu.pk>
2025-03-25 13:23:32 +0500

    cleaning according to the KH

--- keras_hub/src/models/video_swin/video_swin_backbone.py
+++ keras_hub/src/models/video_swin/video_swin_backbone.py
@@ -1,17 +1,9 @@
-import copy
 from functools import partial
 import numpy as np
 from keras import layers
 from keras_hub.src.api_export import keras_hub_export
-import keras
 from keras_hub.src.models import utils
 from keras_hub.src.models.backbone import Backbone
-from keras_hub.src.models.video_swin.video_swin_backbone_presets import (  # noqa: E501
-    backbone_presets,
-)
-from keras_hub.src.models.video_swin.video_swin_backbone_presets import (  # noqa: E501
-    backbone_presets_with_weights,
-)
 from keras_hub.src.models.video_swin.video_swin_layers import (
     VideoSwinBasicLayer,
 )
@@ -21,9 +13,6 @@ from keras_hub.src.models.video_swin.video_swin_layers import (
 from keras_hub.src.models.video_swin.video_swin_layers import (
     VideoSwinPatchMerging,
 )
-from keras_hub.src.utils.python_utils import classproperty
-
-
 @keras_hub_export("keras_hub_export.models.VideoSwinBackbone", package="keras_hub_export.models")
 class VideoSwinBackbone(Backbone):
     """A Video Swin Transformer backbone model.
@@ -37,11 +26,11 @@ class VideoSwinBackbone(Backbone):
             Defaults to `(32, 224, 224, 3)`.
         patch_size : tuple(int). The patch size for depth, height, and width
             dimensions respectively. Default: (2,4,4).
-        embed_dim : int. Number of linear projection output channels.
+        embedding_dim : int. Number of linear projection output channels.
             Default to 96.
-        depth :tuple[int]. Depth of each Swin Transformer stage.
+        stackwise_depth :tuple[int]. Depth of each Swin Transformer stage.
             Default to [2, 2, 6, 2]
-        num_heads : tuple[int]. Number of attention head of each stage.
+        stackwise_num_heads : tuple[int]. Number of attention head of each stage.
             Default to [3, 6, 12, 24]
         window_size : int. The window size for depth, height, and width
             dimensions respectively. Default to [8, 7, 7].
@@ -53,7 +42,7 @@ class VideoSwinBackbone(Backbone):
             Default to None.
         dropout_rate : float. Float between 0 and 1. Fraction of the input units to drop.
             Default: 0.
-        attn_dropout_rate : float. Float between 0 and 1. Attention dropout rate.
+        attention_drop_rate : float. Float between 0 and 1. Attention dropout rate.
             Default: 0.
         drop_path_rate : float. floatFloat between 0 and 1. Stochastic depth rate.
             Default: 0.2.
@@ -63,8 +52,8 @@ class VideoSwinBackbone(Backbone):
     Example:
     ```python
     # Build video swin backbone without top layer
-   from keras_hub.src.models.video_swin.video_swin_layers import VideoSwinBasicLayer
-    model = VideoSwinBasicLayer(
+   from keras_hub.src.models.video_swin.video_swin_layers import VideoSwinBackbone
+    model = VideoSwinBackbone(
          input_shape=(8, 256, 256, 3),
     )
     videos = keras.ops.ones((1, 8, 256, 256, 3))
@@ -76,18 +65,18 @@ class VideoSwinBackbone(Backbone):
         self,
         *,
         input_shape=(32, 224, 224, 3),
-        embed_dim=96,
         patch_size=[2, 4, 4],
-        window_size=[8, 7, 7],
-        mlp_ratio=4.0,
-        patch_norm=True,
-        drop_rate=0.0,
-        attn_drop_rate=0.0,
-        drop_path_rate=0.2,
+        embed_dim=96,
         depths=[2, 2, 6, 2],
         num_heads=[3, 6, 12, 24],
+        window_size=[8, 7, 7],
+        mlp_ratio=4.0,
         qkv_bias=True,
         qk_scale=None,
+        drop_rate=0.0,
+        attention_drop_rate=0.0,
+        drop_path_rate=0.2,
+        patch_norm=True,
         **kwargs,
     ):
         # Parse input specification.
@@ -107,24 +96,7 @@ class VideoSwinBackbone(Backbone):
             )
 
         x = input_spec
-
-        # if include_rescaling:
-        #     # Use common rescaling strategy across keras_cv
-        #     x = keras.layers.Rescaling(1.0 / 255.0)(x)
-
-        #     # VideoSwin scales inputs based on the ImageNet mean/stddev.
-        #     # Officially, Videw Swin takes tensor of [0-255] ranges.
-        #     # And use mean=[123.675, 116.28, 103.53] and
-        #     # std=[58.395, 57.12, 57.375] for normalization.
-        #     # So, if include_rescaling is set to True, then, to match with the
-        #     # official scores, following normalization should be added.
-        #     x = layers.Normalization(
-        #         mean=[0.485, 0.456, 0.406],
-        #         variance=[0.229**2, 0.224**2, 0.225**2],
-        #     )(x)
-
         norm_layer = partial(layers.LayerNormalization, epsilon=1e-05)
-
         x = VideoSwinPatchingAndEmbedding(
             patch_size=patch_size,
             embed_dim=embed_dim,
@@ -145,7 +117,7 @@ class VideoSwinBackbone(Backbone):
                 qkv_bias=qkv_bias,
                 qk_scale=qk_scale,
                 drop_rate=drop_rate,
-                attn_drop_rate=attn_drop_rate,
+                attn_drop_rate=attention_drop_rate,
                 drop_path_rate=dpr[sum(depths[:i]) : sum(depths[: i + 1])],
                 norm_layer=norm_layer,
                 downsampling_layer=(
@@ -164,7 +136,7 @@ class VideoSwinBackbone(Backbone):
         self.norm_layer = norm_layer
         self.patch_norm = patch_norm
         self.drop_rate = drop_rate
-        self.attn_drop_rate = attn_drop_rate
+        self.attn_drop_rate = attention_drop_rate
         self.drop_path_rate = drop_path_rate
         self.num_layers = len(depths)
         self.num_heads = num_heads
@@ -175,38 +147,20 @@ class VideoSwinBackbone(Backbone):
     def get_config(self):
         config = super().get_config()
         config.update(
-            {
-                "input_shape": self.input_shape[1:],
-                "embed_dim": self.embed_dim,
-                "patch_norm": self.patch_norm,
-                "window_size": self.window_size,
-                "patch_size": self.patch_size,
-                "mlp_ratio": self.mlp_ratio,
-                "drop_rate": self.drop_rate,
-                "drop_path_rate": self.drop_path_rate,
-                "attn_drop_rate": self.attn_drop_rate,
-                "depths": self.depths,
-                "num_heads": self.num_heads,
-                "qkv_bias": self.qkv_bias,
-                "qk_scale": self.qk_scale,
-            }
-        )
+    {
+        "input_shape": self.input_shape[1:],
+        "patch_size": self.patch_size,
+        "embed_dim": self.embed_dim,
+        "stackwise_depth": self.depths,
+        "stackwise_num_heads": self.num_heads,
+        "window_size": self.window_size,
+        "mlp_ratio": self.mlp_ratio,
+        "qkv_bias": self.qkv_bias,
+        "qk_scale": self.qk_scale,
+        "drop_rate": self.drop_rate,
+        "attn_drop_rate": self.attn_drop_rate,
+        "drop_path_rate": self.drop_path_rate,
+        "patch_norm": self.patch_norm,
+    }
+)
         return config
-
-    @classproperty
-    def presets(cls):
-        """Dictionary of preset names and configurations."""
-        return copy.deepcopy(backbone_presets)
-
-    @classproperty
-    def presets_with_weights(cls):
-        """Dictionary of preset names and configurations that include
-        weights."""
-        return copy.deepcopy(backbone_presets_with_weights)
-
-    @property
-    def pyramid_level_inputs(self):
-        raise NotImplementedError(
-            "The `VideoSwinBackbone` model doesn't compute"
-            " pyramid level features."
-        )
