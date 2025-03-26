b0de7e713c0ea95c0ceee031d1d57257d68a715e
Yug Borana <yugborana000@gmail.com>
2025-03-26 19:52:16 +0530

    import issue

--- keras_hub/src/models/inception/image_classifier_preprocessor.py
+++ keras_hub/src/models/inception/image_classifier_preprocessor.py
@@ -1,6 +1,6 @@
 from keras_hub.src.api_export import keras_hub_export
-from keras_hub.src.models.inception.image_classifier_preprocessor import (
-    ImageClassifierPreprocessor,
+from keras_hub.src.models.image_classifier_preprocessor import (
+    ImageClassifierPreprocessor
 )
 from keras_hub.src.models.inception.inception_backbone import InceptionBackbone
 from keras_hub.src.models.inception.inception_image_converter import (
