40df2ea432d0ad6e77d735e473c5ca4fdc33a3af
Sravana Neeli <sineeli@google.com>
2025-03-29 21:21:00 -0700

    add generate_step to parseq causal lm

--- keras_hub/src/models/parseq/parseq_causal_lm.py
+++ keras_hub/src/models/parseq/parseq_causal_lm.py
@@ -4,6 +4,7 @@ from keras_hub.src.api_export import keras_hub_export
 from keras_hub.src.models.causal_lm import CausalLM
 from keras_hub.src.models.parseq.parseq_backbone import PARSeqBackbone
 from keras_hub.src.models.parseq.parseq_preprocessor import PARSeqPreprocessor
+from keras_hub.src.utils.tensor_utils import any_equal
 
 
 @keras_hub_export("keras_hub.models.ParSeqCausalLM")
@@ -55,13 +56,62 @@ class ParSeqCausalLM(CausalLM):
             inputs["padding_mask"],
             inputs["images"],
         )
-
-        if len(ops.shape(images)) == 3:
+        images_shape = ops.shape(images)
+        if len(images_shape) == 3:
             # Handle an unbatched image. Unlike `token_ids` and `padding_mask`
             # this will not automatically be upranked.
             images = ops.expand_dims(images, axis=0)
 
-        img_embeddings = self.backbone.image_encoder(images)
+        batch_size = images_shape[0]
+        num_steps = self.backbone.max_label_length + 1
 
-        def next():
-            pass
+        memory = self.backbone.image_encoder(images)
+        pos_queries = ops.expand_dims(
+            self.backbone.decode.pos_queries[:, :num_steps],
+            (batch_size, -1, -1),
+        )
+        target_mask = query_mask = 1 - ops.triu(ops.ones((25, 25)), 1)
+
+        def next(prompt, cache, index):
+            target_out = self.backbone.decode(
+                target=prompt[:, index + 1],
+                memory=memory,
+                target_mask=target_mask[: index + 1, : index + 1],
+                target_query=pos_queries[:, index : index + 1],
+                target_query_mask=query_mask[index : index + 1, : index + 1],
+            )
+            logits = self.backbone.head(target_out)
+
+            return logits, None, cache
+
+        token_ids = self.sampler(
+            next=next,
+            prompt=token_ids,
+            index=1,
+            mask=padding_mask,
+            stop_token_ids=stop_token_ids,
+            model=self,
+        )
+
+        # Compute an output padding mask with the token ids we updated.
+        if stop_token_ids is not None:
+            # Build a mask of `stop_token_ids` locations not in the original
+            # prompt (not in locations where `padding_mask` is True).
+            end_locations = any_equal(
+                token_ids, stop_token_ids, ops.logical_not(padding_mask)
+            )
+
+            end_locations = ops.cast(end_locations, "int32")
+            # Use cumsum to get ones in all locations after end_locations.
+            cumsum = ops.cast(ops.cumsum(end_locations, axis=-1), "int32")
+            overflow = cumsum - end_locations
+            # Our padding mask is the inverse of these overflow locations.
+            padding_mask = ops.logical_not(ops.cast(overflow, "bool"))
+        else:
+            # Without early stopping, all locations will have been updated.
+            padding_mask = ops.ones_like(token_ids, dtype="bool")
+        return {
+            "token_ids": token_ids,
+            "padding_mask": padding_mask,
+            "images": images,
+        }
