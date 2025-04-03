38794ac06fc99cd36898bf57e68bfec2c3a8892b
Divyashree Sreepathihalli <divyashreepathihalli@gmail.com>
2025-04-02 17:55:56 -0700

    Update gating condition to include check for supporting GPUs for flash attention (#2184)
    * update gating condition for flash attention
    
    * fix test
    
    * update utils
    
    * fix tests
    
    * add on t4 on deny list
    
    * address review comments
    
    * address comments

--- keras_hub/src/models/gemma/gemma_causal_lm_test.py
+++ keras_hub/src/models/gemma/gemma_causal_lm_test.py
@@ -12,7 +12,7 @@ from keras_hub.src.models.gemma.gemma_causal_lm_preprocessor import (
 )
 from keras_hub.src.models.gemma.gemma_tokenizer import GemmaTokenizer
 from keras_hub.src.tests.test_case import TestCase
-from keras_hub.src.utils.keras_utils import has_flash_attention_support
+from keras_hub.src.utils.keras_utils import fused_attention_op_available
 from keras_hub.src.utils.keras_utils import running_on_gpu
 
 
@@ -98,7 +98,10 @@ class GemmaCausalLMTest(TestCase):
         )
 
     def test_flash_attention_call(self):
-        if keras.config.backend() != "jax" or not has_flash_attention_support():
+        if (
+            keras.config.backend() != "jax"
+            or not fused_attention_op_available()
+        ):
             self.skipTest("`flash_attention` testing requires the Jax backend.")
 
         with patch("keras.src.backend.nn.dot_product_attention") as mock_func:
