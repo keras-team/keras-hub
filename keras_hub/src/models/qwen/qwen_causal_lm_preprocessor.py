13788a91362f7339c4816194d8ae882933ff7c54
Sachin Prasad <sachinprasad@google.com>
2025-04-08 19:19:49 -0700

    Revert "Do not export Qwen for release (#2198)" (#2208)
    This reverts commit 23ac9774abce4d374bc2b6df0f9ea41736d4b82f.

--- keras_hub/src/models/qwen/qwen_causal_lm_preprocessor.py
+++ keras_hub/src/models/qwen/qwen_causal_lm_preprocessor.py
@@ -1,8 +1,15 @@
+from keras_hub.src.api_export import keras_hub_export
 from keras_hub.src.models.causal_lm_preprocessor import CausalLMPreprocessor
 from keras_hub.src.models.qwen.qwen_backbone import QwenBackbone
 from keras_hub.src.models.qwen.qwen_tokenizer import QwenTokenizer
 
 
+@keras_hub_export(
+    [
+        "keras_hub.models.QwenCausalLMPreprocessor",
+        "keras_hub.models.Qwen2CausalLMPreprocessor",
+    ]
+)
 class QwenCausalLMPreprocessor(CausalLMPreprocessor):
     backbone_cls = QwenBackbone
     tokenizer_cls = QwenTokenizer
