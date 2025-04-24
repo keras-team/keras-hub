86847d8cbe7164f40f17f92287ef4ef33991953f
Laxma Reddy Patlolla <laxmareddyp@google.com>
2025-04-24 10:02:13 -0700

    Address comments

--- keras_hub/src/models/segformer/segformer_image_segmenter_tests.py
+++ keras_hub/src/models/segformer/segformer_image_segmenter_tests.py
@@ -5,11 +5,6 @@ from keras_hub.api.models import MiTBackbone
 from keras_hub.api.models import SegFormerBackbone
 from keras_hub.api.models import SegFormerImageSegmenter
 from keras_hub.api.models import SegFormerImageSegmenterPreprocessor
-from keras_hub.src.models.mit.mit_backbone import MiTBackbone
-from keras_hub.src.models.segformer.segformer_backbone import SegFormerBackbone
-from keras_hub.src.models.segformer.segformer_image_segmenter import (
-    SegFormerImageSegmenter,
-)
 from keras_hub.src.tests.test_case import TestCase
 
 
@@ -17,7 +12,7 @@ class SegFormerTest(TestCase):
     def setUp(self):
         image_encoder = MiTBackbone(
             layerwise_depths=[2, 2],
-            image_shape=(224, 224, 3),
+            image_shape=(32, 32, 3),
             hidden_dims=[32, 64],
             num_layers=2,
             layerwise_num_heads=[1, 2],
@@ -32,7 +27,7 @@ class SegFormerTest(TestCase):
             image_encoder=image_encoder, projection_filters=projection_filters
         )
 
-        self.input_size = 224
+        self.input_size = 32
         self.input_data = np.ones((2, self.input_size, self.input_size, 3))
         self.label_data = np.ones((2, self.input_size, self.input_size, 4))
 
@@ -51,19 +46,19 @@ class SegFormerTest(TestCase):
             backbone=self.backbone, num_classes=4
         )
 
-        images = np.random.uniform(size=(2, 224, 224, 3))
+        images = np.random.uniform(size=(2, 32, 32, 3))
         segformer_output = segformer(images)
         segformer_predict = segformer.predict(images)
 
-        self.assertAllEqual(segformer_output.shape, (2, 224, 224, 4))
-        self.assertAllEqual(segformer_predict.shape, (2, 224, 224, 4))
+        self.assertAllEqual(segformer_output.shape, (2, 32, 32, 4))
+        self.assertAllEqual(segformer_predict.shape, (2, 32, 32, 4))
 
     def test_task(self):
         self.run_task_test(
             cls=SegFormerImageSegmenter,
             init_kwargs={**self.init_kwargs},
             train_data=(self.input_data, self.label_data),
-            expected_output_shape=(2, 224, 224, 4),
+            expected_output_shape=(2, 32, 32, 4),
         )
 
     @pytest.mark.large
