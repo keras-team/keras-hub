d92c8c45eb71d052308a4b2a59cd94eb8563f114
carrycooldude <rawatkari554@gmail.com>
2025-04-27 13:08:54 +0530

    refactor: Move LayoutLMv3 files to dedicated directory

---
+++ keras_hub/src/models/layoutlmv3/__init__.py
@@ -0,0 +1,15 @@
+from keras_hub.src.models.layoutlmv3.layoutlmv3_backbone import LayoutLMv3Backbone
+from keras_hub.src.models.layoutlmv3.layoutlmv3_document_classifier import LayoutLMv3DocumentClassifier
+from keras_hub.src.models.layoutlmv3.layoutlmv3_document_classifier_preprocessor import LayoutLMv3DocumentClassifierPreprocessor
+from keras_hub.src.models.layoutlmv3.layoutlmv3_tokenizer import LayoutLMv3Tokenizer
+from keras_hub.src.models.layoutlmv3.layoutlmv3_transformer import LayoutLMv3Transformer
+from keras_hub.src.models.layoutlmv3.layoutlmv3_presets import layoutlmv3_presets
+
+__all__ = [
+    "LayoutLMv3Backbone",
+    "LayoutLMv3DocumentClassifier",
+    "LayoutLMv3DocumentClassifierPreprocessor",
+    "LayoutLMv3Tokenizer",
+    "LayoutLMv3Transformer",
+    "layoutlmv3_presets",
+] 
