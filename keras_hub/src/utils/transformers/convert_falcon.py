bc4b4f720fee31e693cafeb27fa825ba5742fa6d
Mansi Mehta <mehtamansi@google.com>
2025-04-28 14:23:36 +0530

    Falcon converter changes

--- keras_hub/src/utils/transformers/convert_falcon.py
+++ keras_hub/src/utils/transformers/convert_falcon.py
@@ -8,16 +8,15 @@ backbone_cls = FalconBackbone
 
 def convert_backbone_config(transformers_config):
     return {
-        "vocabulary_size": transformers_config["vocab_size"],
-        "num_layers": transformers_config["num_hidden_layers"],
-        "num_attention_heads": transformers_config["num_attention_heads"],
-        "hidden_dim": transformers_config["hidden_size"],
-        "intermediate_dim": 32 * 4,
-    }
+            "vocabulary_size": transformers_config["vocab_size"],
+            "num_layers": transformers_config["num_hidden_layers"],
+            "hidden_dim": transformers_config["hidden_size"],
+            "num_attention_heads": transformers_config["num_attention_heads"],
+            "num_key_value_heads": transformers_config["num_key_value_heads"],
+            "intermediate_dim": transformers_config["intermediate_size"],
+            "head_dim": transformers_config["head_dim"],
+        }
 
-
-def transpose_and_reshape(x, shape):
-    return np.reshape(np.transpose(x), shape)
 
 
 def convert_weights(backbone, loader, transformers_config):
@@ -41,20 +40,47 @@ def convert_weights(backbone, loader, transformers_config):
             keras_variable=decoder_layer.attention_layer.output_dense.kernel,
             hf_weight_key=f"h.{i}.self_attention.dense.weight",
         )
+        input_dim = decoder_layer.attention_layer.query_dense.kernel.shape[0]
+        attention_dim = decoder_layer.attention_layer.query_dense.kernel.shape[1]
 
+        hf_tensor= loader.get_tensor(f'h.{i}.self_attention.query_key_value.weight')
+        hf_tensor= np.reshape(hf_tensor, (3,attention_dim, input_dim))
+        hf_tensor= np.transpose(hf_tensor,(0,2,1))
+        query_weight, key_weight, value_weight = hf_tensor
+
+        loader.port_weight(decoder_layer.attention_layer.query_dense.kernel, query_weight)
+        loader.port_weight(decoder_layer.attention_layer.key_dense.kernel, key_weight)
+        loader.port_weight(decoder_layer.attention_layer.value_dense.kernel, value_weight)
+
+        #MLP dense layers
         loader.port_weight(
-            keras_variable=decoder_layer.post_attention_layernorm.gamma,
-            hf_weight_key=f"h.{i}.self_attention.query_key_value.weight",
-            hook_fn=lambda hf_tensor, keras_shape: np.mean(
-                np.reshape(hf_tensor, (-1, keras_shape[0])), axis=0
-            ),
+            keras_variable=decoder_layer.dense_h_to_4h.kernel,
+            hf_weight_key=f"h.{i}.mlp.dense_h_to_4h.weight",
         )
-
+        loader.port_weight(
+            keras_variable=decoder_layer.dense_4h_to_h.kernel,
+            hf_weight_key=f"h.{i}.mlp.dense_4h_to_h.weight",
+        )
+        
 
 def convert_tokenizer(cls, preset, **kwargs):
     tokenizer_data = load_json(preset, "tokenizer.json")
     vocab = tokenizer_data["model"]["vocab"]
     merges = tokenizer_data["model"].get("merges", None)
 
-    tokenizer_kwargs = {"vocabulary": vocab, "merges": merges}
+    tokenizer_config2 = load_json(preset, "tokenizer_config.json")
+    bos_token = tokenizer_config2["bos_token"]
+    eos_token = tokenizer_config2["eos_token"]
+
+    tokenizer_kwargs = {"vocabulary": vocab}
+
+    if merges is not None:
+        tokenizer_kwargs["merges"] = merges
+    if bos_token is not None:
+        tokenizer_kwargs["bos_token"] = bos_token
+    if eos_token is not None:
+        tokenizer_kwargs["eos_token"] = eos_token
+
+    tokenizer_kwargs.update(kwargs) 
+
     return cls(**tokenizer_kwargs)
