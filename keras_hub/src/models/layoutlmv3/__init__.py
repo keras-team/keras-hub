0948f95c611f403fbeaea5070493ea6a0b2b69b9
carrycooldude <rawatkari554@gmail.com>
2025-04-30 13:07:05 +0530

    fix: Update LayoutLMv3 init files to follow correct format

--- keras_hub/src/models/layoutlmv3/__init__.py
+++ keras_hub/src/models/layoutlmv3/__init__.py
@@ -3,7 +3,8 @@ from keras_hub.src.models.layoutlmv3.layoutlmv3_document_classifier import Layou
 from keras_hub.src.models.layoutlmv3.layoutlmv3_document_classifier_preprocessor import LayoutLMv3DocumentClassifierPreprocessor
 from keras_hub.src.models.layoutlmv3.layoutlmv3_tokenizer import LayoutLMv3Tokenizer
 from keras_hub.src.models.layoutlmv3.layoutlmv3_transformer import LayoutLMv3Transformer
-from keras_hub.src.models.layoutlmv3.layoutlmv3_presets import layoutlmv3_presets
+from keras_hub.src.models.layoutlmv3.layoutlmv3_presets import layoutlmv3_presets, backbone_presets
+from keras_hub.src.utils.preset_utils import register_presets
 
 __all__ = [
     "LayoutLMv3Backbone",
@@ -12,4 +13,6 @@ __all__ = [
     "LayoutLMv3Tokenizer",
     "LayoutLMv3Transformer",
     "layoutlmv3_presets",
-] 
+]
+
+register_presets(backbone_presets, LayoutLMv3Backbone) 
