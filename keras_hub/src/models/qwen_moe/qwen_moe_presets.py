41e055be0610bca98c45bb97d8a8d180abc455ec
Laxma Reddy Patlolla <laxmareddyp@google.com>
2025-05-29 11:21:56 -0700

    Fix keras-io integration test

--- keras_hub/src/models/qwen_moe/qwen_moe_presets.py
+++ keras_hub/src/models/qwen_moe/qwen_moe_presets.py
@@ -4,8 +4,8 @@ backbone_presets = {
     "qwen1.5_moe_2.7b_en": {
         "metadata": {
             "description": (
-                "24-layer Qwen MoE model with 2.7 billion active parameters ",
-                "and 8 experts per MoE layer.",
+                "24-layer Qwen MoE model with 2.7 billion active parameters "
+                "and 8 experts per MoE layer."
             ),
             "params": 14315784192,
             "path": "qwen-1.5-moe",
