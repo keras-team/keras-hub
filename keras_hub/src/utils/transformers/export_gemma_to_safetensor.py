4045ce6b5905f6b2060e731d24fdad2245428d84
Dhiraj BM <matamdhiraj123@gmail.com>
2025-07-01 22:55:01 +0530

    backend agnostic

--- keras_hub/src/utils/transformers/export_gemma_to_safetensor.py
+++ keras_hub/src/utils/transformers/export_gemma_to_safetensor.py
@@ -3,8 +3,12 @@ import os
 import shutil
 import warnings
 
-import torch
-from safetensors.torch import save_file
+import jax.numpy as jnp
+import keras
+import keras.ops as ops
+from safetensors.flax import save_file as flax_save_file
+from safetensors.tensorflow import save_file as tf_save_file
+from safetensors.torch import save_file as torch_save_file
 
 
 def convert_to_hf_config(keras_config):
@@ -32,84 +36,79 @@ def export_to_hf(keras_model, path):
         path: str. Path of the directory to which the safetensors file,
         config and tokenizer will be saved.
     """
+    backend = keras.config.backend()
     backbone = keras_model.backbone
     hf_config = convert_to_hf_config(backbone)
 
     weights_dict = {}
 
     # Map token embedding
-    token_embedding = backbone.get_layer("token_embedding").get_weights()[0]
-    weights_dict["model.embed_tokens.weight"] = torch.from_numpy(
-        token_embedding
-    )
+    token_embedding_layer = backbone.get_layer("token_embedding")
+    weights_dict["model.embed_tokens.weight"] = token_embedding_layer.weights[0]
 
     for i in range(backbone.num_layers):
         decoder_layer = backbone.get_layer(f"decoder_block_{i}")
 
         # Pre-attention normalization
-        pre_attn_norm = decoder_layer.pre_attention_norm.get_weights()[0]
         weights_dict[f"model.layers.{i}.input_layernorm.weight"] = (
-            torch.from_numpy(pre_attn_norm)
+            decoder_layer.pre_attention_norm.weights[0]
         )
 
         # Attention query projection
-        query_kernel = decoder_layer.attention.query_dense.get_weights()[0]
-        query_kernel = (
-            torch.from_numpy(query_kernel)
-            .permute(1, 0, 2)
-            .reshape(-1, backbone.hidden_dim)
-            .T
-        )
+        query_kernel = decoder_layer.attention.query_dense.weights[0]
+        query_kernel = ops.transpose(query_kernel, axes=(1, 0, 2))
+        query_kernel = ops.reshape(query_kernel, (-1, backbone.hidden_dim))
+        query_kernel = ops.transpose(query_kernel)
         weights_dict[f"model.layers.{i}.self_attn.q_proj.weight"] = query_kernel
 
         # Attention key projection
-        key_kernel = decoder_layer.attention.key_dense.get_weights()[0][0]
-        key_kernel = torch.from_numpy(key_kernel).T
-        weights_dict[f"model.layers.{i}.self_attn.k_proj.weight"] = key_kernel
+        key_kernel = decoder_layer.attention.key_dense.weights[0][0]
+        weights_dict[f"model.layers.{i}.self_attn.k_proj.weight"] = (
+            ops.transpose(key_kernel)
+        )
 
         # Attention value projection
-        value_kernel = decoder_layer.attention.value_dense.get_weights()[0][0]
-        value_kernel = torch.from_numpy(value_kernel).T
-        weights_dict[f"model.layers.{i}.self_attn.v_proj.weight"] = value_kernel
+        value_kernel = decoder_layer.attention.value_dense.weights[0][0]
+        weights_dict[f"model.layers.{i}.self_attn.v_proj.weight"] = (
+            ops.transpose(value_kernel)
+        )
 
         # Attention output projection
-        out_kernel = decoder_layer.attention.output_dense.get_weights()[0]
-        out_kernel = (
-            torch.from_numpy(out_kernel)
-            .permute(2, 0, 1)
-            .reshape(backbone.hidden_dim, -1)
-        )
+        out_kernel = decoder_layer.attention.output_dense.weights[0]
+        out_kernel = ops.transpose(out_kernel, axes=(2, 0, 1))
+        out_kernel = ops.reshape(out_kernel, (backbone.hidden_dim, -1))
         weights_dict[f"model.layers.{i}.self_attn.o_proj.weight"] = out_kernel
 
         # Post-attention normalization
-        post_attn_norm = decoder_layer.pre_ffw_norm.get_weights()[0]
         weights_dict[f"model.layers.{i}.post_attention_layernorm.weight"] = (
-            torch.from_numpy(post_attn_norm)
+            decoder_layer.pre_ffw_norm.weights[0]
         )
 
         # MLP gate projection
-        gate_kernel = decoder_layer.gating_ffw.get_weights()[0]
-        gate_kernel = torch.from_numpy(gate_kernel).T
-        weights_dict[f"model.layers.{i}.mlp.gate_proj.weight"] = gate_kernel
+        gate_kernel = decoder_layer.gating_ffw.weights[0]
+        weights_dict[f"model.layers.{i}.mlp.gate_proj.weight"] = ops.transpose(
+            gate_kernel
+        )
 
         # MLP up projection
-        up_kernel = decoder_layer.gating_ffw_2.get_weights()[0]
-        up_kernel = torch.from_numpy(up_kernel).T
-        weights_dict[f"model.layers.{i}.mlp.up_proj.weight"] = up_kernel
+        up_kernel = decoder_layer.gating_ffw_2.weights[0]
+        weights_dict[f"model.layers.{i}.mlp.up_proj.weight"] = ops.transpose(
+            up_kernel
+        )
 
         # MLP down projection
-        down_kernel = decoder_layer.ffw_linear.get_weights()[0]
-        down_kernel = torch.from_numpy(down_kernel).T
-        weights_dict[f"model.layers.{i}.mlp.down_proj.weight"] = down_kernel
+        down_kernel = decoder_layer.ffw_linear.weights[0]
+        weights_dict[f"model.layers.{i}.mlp.down_proj.weight"] = ops.transpose(
+            down_kernel
+        )
 
     # Map final normalization
-    final_norm = backbone.get_layer("final_normalization").get_weights()[0]
-    weights_dict["model.norm.weight"] = torch.from_numpy(final_norm)
+    weights_dict["model.norm.weight"] = backbone.get_layer(
+        "final_normalization"
+    ).weights[0]
 
     # Tie lm_head.weight to embedding weights
-    weights_dict["lm_head.weight"] = weights_dict[
-        "model.embed_tokens.weight"
-    ].clone()
+    weights_dict["lm_head.weight"] = token_embedding_layer.weights[0]
 
     # Save config
     os.makedirs(path, exist_ok=True)
@@ -117,14 +116,20 @@ def export_to_hf(keras_model, path):
     with open(config_path, "w") as f:
         json.dump(hf_config, f)
 
-    # Make tensors contiguous before saving
-    weights_dict_contiguous = {
-        k: v.contiguous() for k, v in weights_dict.items()
-    }
-
-    # Save weights
+    # Save weights based on backend
     weights_path = os.path.join(path, "model.safetensors")
-    save_file(weights_dict_contiguous, weights_path)
+    if backend == "torch":
+        weights_dict_contiguous = {
+            k: v.contiguous() for k, v in weights_dict.items()
+        }
+        torch_save_file(weights_dict_contiguous, weights_path)
+    elif backend == "tensorflow":
+        tf_save_file(weights_dict, weights_path)
+    elif backend == "jax":
+        weights_dict_contiguous = {
+            k: jnp.ascontiguousarray(v) for k, v in weights_dict.items()
+        }
+        flax_save_file(weights_dict_contiguous, weights_path)
 
     # Save tokenizer assets
     keras_model.preprocessor.tokenizer.save_assets(path)
