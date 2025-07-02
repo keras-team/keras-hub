64176f5c7f3daa8c57ed082b9289caa64ea5799b
Mohamed-Ashraf273 <ma2736666@gmail.com>
2025-07-02 21:14:40 +0300

    supprt model reusing by hashing and add support for GPT-2

--- keras_hub/src/models/causal_lm.py
+++ keras_hub/src/models/causal_lm.py
@@ -191,11 +191,16 @@ class CausalLM(Task):
                 if not hasattr(ov_infer, "_compiled_models"):
                     ov_infer._compiled_models = {}
 
-                # Create hash based on parameters, results, and input shapes
-                inputs_shapes = [str(v.shape) for k, v in inputs.items()]
+                # Create hash based on inputs, inputs shapes, and input dtypes
+                inputs_shapes = []
+                inputs_dtypes = []
+                for k, v in inputs.items():
+                    inputs_shapes.append(str(v.shape))
+                    inputs_dtypes.append(str(v.dtype))
                 model_signature = (
                     f"inputs_{len(inputs)}_"
-                    f"inputs_shapes_{'_'.join(inputs_shapes)}_"
+                    f"shapes_{'_'.join(inputs_shapes)}_"
+                    f"dtypes_{'_'.join(inputs_dtypes)}_"
                 )
 
                 model_hash = hash(model_signature)
