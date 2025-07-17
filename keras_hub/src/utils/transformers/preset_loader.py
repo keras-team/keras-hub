3ef466dc0c66e94caf4dadd93065e77d5b86d67d
pass_lin <935499957@qq.com>
2025-07-18 00:21:58 +0800

    resolve conflit

--- keras_hub/src/utils/transformers/preset_loader.py
+++ keras_hub/src/utils/transformers/preset_loader.py
@@ -39,6 +39,8 @@ class TransformersPresetLoader(PresetLoader):
             self.converter = convert_distilbert
         elif model_type in ("dinov2", "dinov2_with_registers"):
             self.converter = convert_dinov2
+        elif model_type == "esm":
+            self.converter = convert_esm
         elif model_type in ("gemma", "gemma2"):
             self.converter = convert_gemma
         elif model_type == "gpt2":
