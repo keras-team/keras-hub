b9904019740bfac1d14fd8749a11f67ac36f8926
Mansi Mehta <mehtamansi@google.com>
2025-07-21 20:19:26 +0530

    transformer config changes of falcon converter_3

--- keras_hub/src/utils/transformers/convert_falcon.py
+++ keras_hub/src/utils/transformers/convert_falcon.py
@@ -11,7 +11,8 @@ def convert_backbone_config(transformers_config):
             "vocabulary_size": transformers_config["vocab_size"],
             "num_layers": transformers_config["num_hidden_layers"],
             "hidden_dim": transformers_config["hidden_size"],
-            "num_attention_heads": transformers_config["num_attention_heads"]
+            "num_attention_heads": transformers_config["num_attention_heads"],
+            "intermediate_dim": transformers_config["intermediate_size"]
         }
 
 
