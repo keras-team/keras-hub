496eeebf14252eaa645e2900db5ad2e3fe351f5b
Mansi Mehta <mehtamansi@google.com>
2025-08-04 20:58:04 +0530

    preset_loader precommit run

--- keras_hub/src/utils/transformers/preset_loader.py
+++ keras_hub/src/utils/transformers/preset_loader.py
@@ -13,11 +13,7 @@ from keras_hub.src.utils.transformers import convert_gemma
 from keras_hub.src.utils.transformers import convert_gpt2
 from keras_hub.src.utils.transformers import convert_llama3
 from keras_hub.src.utils.transformers import convert_mistral
-from keras_hub.src.utils.transformers import convert_mixtral
 from keras_hub.src.utils.transformers import convert_pali_gemma
-from keras_hub.src.utils.transformers import convert_qwen
-from keras_hub.src.utils.transformers import convert_qwen3
-from keras_hub.src.utils.transformers import convert_qwen_moe
 from keras_hub.src.utils.transformers import convert_vit
 from keras_hub.src.utils.transformers.safetensor_utils import SafetensorLoader
 
