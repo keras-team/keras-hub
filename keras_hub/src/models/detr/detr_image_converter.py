50313ec5035eaebc4b618e42011aab79463666c4
David Landup <david.landup.0@gmail.com>
2025-10-24 01:29:35 +0900

    Add custom preprocessing for detr preprocessor

--- keras_hub/src/models/detr/detr_image_converter.py
+++ keras_hub/src/models/detr/detr_image_converter.py
@@ -6,3 +6,33 @@ from keras_hub.src.models.detr.detr_backbone import DETRBackbone
 @keras_hub_export("keras_hub.layers.DETRImageConverter")
 class DETRImageConverter(ImageConverter):
     backbone_cls = DETRBackbone
+
+    def __init__(
+        self,
+        image_size=(800, 800),
+        scale=None,
+        offset=None,
+        **kwargs,
+    ):
+        if scale is None:
+            # scale = 1/255 / std for each channel
+            scale = [
+                1.0 / 255.0 / 0.229,  # R channel
+                1.0 / 255.0 / 0.224,  # G channel
+                1.0 / 255.0 / 0.225,  # B channel
+            ]
+
+        if offset is None:
+            # offset = -mean / std for each channel
+            offset = [
+                -0.485 / 0.229,  # R channel
+                -0.456 / 0.224,  # G channel
+                -0.406 / 0.225,  # B channel
+            ]
+
+        super().__init__(
+            image_size=image_size,
+            scale=scale,
+            offset=offset,
+            **kwargs,
+        )
