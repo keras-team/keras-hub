b3e33fd00d2e3d964c79ef994b4a1712dce77718
pass_lin <935499957@qq.com>
2025-11-09 19:47:33 +0800

    fix test bug

--- keras_hub/src/models/rwkv7/rwkv7_tokenizer.py
+++ keras_hub/src/models/rwkv7/rwkv7_tokenizer.py
@@ -255,7 +255,7 @@ class RWKVTokenizer(tokenizer.Tokenizer):
         self.vocabulary = None
         self.pad_token_id = pad_token_id
         self.start_token_id = start_token_id
-        self.end_token_id = end_token_id
+        self.end_token_id = end_token_id or self.pad_token_id
         if vocabulary is not None:
             self.set_vocabulary(vocabulary)
         self.file_assets = [VOCAB_FILENAME]
