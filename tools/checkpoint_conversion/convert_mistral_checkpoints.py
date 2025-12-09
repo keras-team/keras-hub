64107d07d54fb800a3b2c15f4be529028500ff03
Omkar Kabde <omkarkabde@gmail.com>
2025-12-09 17:07:57 +0530

    edit hf tokenizer for devstral

--- tools/checkpoint_conversion/convert_mistral_checkpoints.py
+++ tools/checkpoint_conversion/convert_mistral_checkpoints.py
@@ -221,7 +221,10 @@ def main(_):
     try:
         # === Load the Huggingface model ===
         hf_model = MistralForCausalLM.from_pretrained(hf_preset)
-        hf_tokenizer = AutoTokenizer.from_pretrained(hf_preset)
+        if "devstral" in hf_preset.lower():
+            hf_tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-Small-24B-Base-2501")
+        else:   
+            hf_tokenizer = AutoTokenizer.from_pretrained(hf_preset)
         hf_model.eval()
         print("\n-> Huggingface model and tokenizer loaded")
 
@@ -241,7 +244,7 @@ def main(_):
         keras_hub_backbone = MistralBackbone(**backbone_kwargs)
 
         if "devstral" in hf_preset.lower():
-            keras_hub_tokenizer = MistralTokenizer.from_preset(f"hf://mistralai/Mistral-Small-24B-Base-2501")
+            keras_hub_tokenizer = MistralTokenizer.from_preset("hf://mistralai/Mistral-Small-24B-Base-2501")
         else:
             keras_hub_tokenizer = MistralTokenizer.from_preset(f"hf://{hf_preset}")
         print("\n-> Keras 3 model and tokenizer loaded.")
