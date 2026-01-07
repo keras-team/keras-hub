527c427ade3a81656a43ca329fac67ec0617a3aa
RAHUL KUMAR <hellorahul@google.com>
2026-01-07 11:05:19 +0530

    Revert "Update mask assertion in embedding layer test"
    This reverts commit d39d4857b1ecac230a2e75a4798dce38f5af42f9.

--- keras_hub/src/layers/modeling/token_and_position_embedding_test.py
+++ keras_hub/src/layers/modeling/token_and_position_embedding_test.py
@@ -33,5 +33,5 @@ class TokenAndPositionEmbeddingTest(TestCase):
         )
         input_data = np.array([[1, 0], [1, 0]])
         mask = input_data != 0
-        computed_mask = test_layer.compute_mask(input_data)
-        self.assertAllEqual(computed_mask, mask)
+        outputs = test_layer(input_data)
+        self.assertAllEqual(outputs._keras_mask, mask)
