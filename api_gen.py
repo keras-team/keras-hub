ddab891f5e38e0278111a52da0e5866a55720379
divyashreepathihalli <divyashreepathihalli@gmail.com>
2026-01-13 00:59:31 +0000

    fix api gen error

--- api_gen.py
+++ api_gen.py
@@ -9,6 +9,13 @@ It generates API and formats user and generated APIs.
 import os
 import shutil
 
+import numpy as np
+
+try:
+    np.object = object
+except Exception:
+    pass
+
 import namex
 
 PACKAGE = "keras_hub"
