d78952ab0a284cf3349b49fcffd0cb8e93da4aab
Samudraneel Sarkar <samudraneel05@gmail.com>
2026-01-27 00:29:17 +0530

    presets for kagglehub

--- keras_hub/src/utils/transformers/preset_loader.py
+++ keras_hub/src/utils/transformers/preset_loader.py
@@ -24,6 +24,7 @@ from keras_hub.src.utils.transformers import convert_pali_gemma
 from keras_hub.src.utils.transformers import convert_qwen
 from keras_hub.src.utils.transformers import convert_qwen3
 from keras_hub.src.utils.transformers import convert_qwen3_moe
+from keras_hub.src.utils.transformers import convert_qwen3_omni
 from keras_hub.src.utils.transformers import convert_qwen_moe
 from keras_hub.src.utils.transformers import convert_sam3
 from keras_hub.src.utils.transformers import convert_smollm3
@@ -77,6 +78,8 @@ class TransformersPresetLoader(PresetLoader):
             self.converter = convert_qwen_moe
         elif model_type == "qwen3_moe":
             self.converter = convert_qwen3_moe
+        elif model_type == "qwen3_omni_moe":
+            self.converter = convert_qwen3_omni
         elif model_type == "qwen3":
             self.converter = convert_qwen3
         elif model_type == "sam3_video":
