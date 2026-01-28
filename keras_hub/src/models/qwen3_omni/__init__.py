ba04dc12781ad7b595fc60ed2053cf24dab5a167
Samudraneel Sarkar <samudraneel05@gmail.com>
2026-01-28 21:54:14 +0530

    init aligned with repo standard

--- keras_hub/src/models/qwen3_omni/__init__.py
+++ keras_hub/src/models/qwen3_omni/__init__.py
@@ -1,10 +1,4 @@
-# Qwen3-Omni exports
 from keras_hub.src.models.qwen3_omni.qwen3_omni_backbone import Qwen3OmniBackbone
-from keras_hub.src.models.qwen3_omni.qwen3_omni_causal_lm import Qwen3OmniCausalLM
-from keras_hub.src.models.qwen3_omni.qwen3_omni_causal_lm_preprocessor import (
-    Qwen3OmniCausalLMPreprocessor,
-)
-from keras_hub.src.models.qwen3_omni.qwen3_omni_tokenizer import Qwen3OmniTokenizer
 from keras_hub.src.models.qwen3_omni.qwen3_omni_presets import backbone_presets
 from keras_hub.src.utils.preset_utils import register_presets
 
