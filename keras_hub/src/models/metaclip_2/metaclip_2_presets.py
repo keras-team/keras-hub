53692d16509e5eebc545d8b70b21cf0f3221d23a
Sachin Prasad <sachinprasad@google.com>
2026-03-12 14:26:09 -0700

    fix description

--- keras_hub/src/models/metaclip_2/metaclip_2_presets.py
+++ keras_hub/src/models/metaclip_2/metaclip_2_presets.py
@@ -4,12 +4,12 @@ backbone_presets = {
     "metaclip_2_vit_huge_patch14_224": {
         "metadata": {
             "description": (
-                "986 million parameter, 32-layer for vision and 24-layer for "
+                "2 billion parameter, 32-layer for vision and 24-layer for "
                 "text, patch size of 14, image resolution 224x224. MetaCLIP 2 "
                 "worldwide huge model (ViT-H-14-quickgelu-worldwide) trained on "  # noqa
                 "29B seen pairs with QuickGELU activation."
             ),
-            "params": 1858784002,
+            "params": 1858783745,
             "path": "metaclip_2",
         },
         "kaggle_handle": "kaggle://keras/metaclip2/keras/metaclip_2_vit_huge_patch14_224/1",
@@ -17,7 +17,7 @@ backbone_presets = {
     "metaclip_2_vit_huge_patch14_378": {
         "metadata": {
             "description": (
-                "986 million parameter, 32-layer for vision and 24-layer for "
+                "2 billion parameter, 32-layer for vision and 24-layer for "
                 "text, patch size of 14, image resolution 378x378. MetaCLIP 2 "
                 "worldwide huge model (ViT-H-14-378-worldwide) trained on "
                 "29B seen pairs."
@@ -30,7 +30,7 @@ backbone_presets = {
     "metaclip_2_vit_giant_patch14_224": {
         "metadata": {
             "description": (
-                "1.4 billion parameter, 40-layer for vision and 24-layer for "
+                "4 billion parameter, 40-layer for vision and 24-layer for "
                 "text, patch size of 14, image resolution 224x224. MetaCLIP 2 "
                 "worldwide giant model (ViT-bigG-14-worldwide) trained on "
                 "29B seen pairs."
@@ -43,7 +43,7 @@ backbone_presets = {
     "metaclip_2_vit_giant_patch14_378": {
         "metadata": {
             "description": (
-                "1.4 billion parameter, 40-layer for vision and 24-layer for "
+                "4 billion parameter, 40-layer for vision and 24-layer for "
                 "text, patch size of 14, image resolution 378x378. MetaCLIP 2 "
                 "worldwide giant model (ViT-bigG-14-378-worldwide) trained on "
                 "29B seen pairs."
