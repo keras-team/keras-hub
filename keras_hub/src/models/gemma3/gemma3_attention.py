f84c6788093f834159c1c38f2621f09a7ca02544
Gaurav Gaonkar <ggaonkar@google.com>
2026-06-12 18:08:05 +0000

    applying fix only for torch backend

--- keras_hub/src/models/gemma3/gemma3_attention.py
+++ keras_hub/src/models/gemma3/gemma3_attention.py
@@ -198,7 +198,10 @@ class CachedGemma3Attention(keras.layers.Layer):
                 kwargs = {"attn_logits_soft_cap": self.logit_soft_cap}
             else:
                 kwargs = {}
-            if self.num_key_value_groups > 1:
+            if (
+                self.num_key_value_groups > 1
+                and keras.config.backend() != "jax"
+            ):
                 k = ops.repeat(k, self.num_key_value_groups, axis=-2)
                 v = ops.repeat(v, self.num_key_value_groups, axis=-2)
             return ops.dot_product_attention(
