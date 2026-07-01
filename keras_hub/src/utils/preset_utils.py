336b3b302e524a85df9440923e6f8097e94c1847
Laxma Reddy Patlolla <laxmareddyp@google.com>
2026-07-01 15:21:07 -0700

    Pin kagglesdk==0.1.30 and surface kagglehub import errors

--- keras_hub/src/utils/preset_utils.py
+++ keras_hub/src/utils/preset_utils.py
@@ -18,8 +18,15 @@ from keras_hub.src.utils.tensor_utils import get_tensor_size_in_bits
 try:
     import kagglehub
     from kagglehub.exceptions import KaggleApiHTTPError
-except ImportError:
+except ModuleNotFoundError:
     kagglehub = None
+except ImportError as e:
+    raise ImportError(
+        f"The `kagglehub` package is installed but failed to import "
+        f"due to a dependency error: {e}. This is likely caused by an "
+        f"incompatible version of `kagglesdk`. Please try: "
+        f"`pip install --upgrade kagglehub kagglesdk`."
+    ) from e
 
 try:
     import huggingface_hub
