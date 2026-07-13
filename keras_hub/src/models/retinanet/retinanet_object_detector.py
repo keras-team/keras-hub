191299f46b7a588060b73115132a5f7cab823478
Pawel <p.zarzycki@versamind.com>
2026-07-12 22:41:41 -0700

    Simplify RetinaNet normalizer clamping

--- keras_hub/src/models/retinanet/retinanet_object_detector.py
+++ keras_hub/src/models/retinanet/retinanet_object_detector.py
@@ -246,8 +246,8 @@ class RetinaNetObjectDetector(ObjectDetector):
         )
         positive_mask = ops.cast(ops.greater(labels, -1.0), dtype="float32")
         normalizer = ops.sum(positive_mask)
-        # avoid dividing by zero below, normalizer is integer value here
-        safe_normalizer = ops.maximum(normalizer, ops.ones_like(normalizer))
+        # Avoid division by zero below.
+        safe_normalizer = ops.maximum(normalizer, 1.0)
         cls_weights = ops.cast(ops.not_equal(labels, -2.0), dtype="float32")
         cls_weights /= safe_normalizer
         box_weights = positive_mask / safe_normalizer
