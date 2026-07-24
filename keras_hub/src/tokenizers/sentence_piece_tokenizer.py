c17dd9f877c07f29c247b9189ed2018ee435bc22
Nikhil Raj Yammani <yammani@google.com>
2026-07-23 21:09:39 -0700

    Fix SentencePiece version compatibility via hasattr check

--- keras_hub/src/tokenizers/sentence_piece_tokenizer.py
+++ keras_hub/src/tokenizers/sentence_piece_tokenizer.py
@@ -149,22 +149,25 @@ class SentencePieceTokenizer(tokenizer.Tokenizer):
 
     def _set_proto_spm(self, proto):
         out_type = str if is_string_dtype(self.compute_dtype) else int
-        try:
-            # Newer PyBind11 wrapper (>=0.2.2)
-            self._sentence_piece_spm = spm.SentencePieceProcessor(
-                model_proto=proto,
-                out_type=out_type,
-                add_bos=self.add_bos,
-                add_eos=self.add_eos,
-            )
-        except TypeError:
-            # Older SWIG wrapper (<0.2.2)
+
+        if hasattr(spm.SentencePieceProcessor(), "Init"):
+            # Old SWIG wrapper (sentencepiece <=0.1.99)
             self._sentence_piece_spm = spm.SentencePieceProcessor()
             self._sentence_piece_spm.Init(
                 model_proto=proto,
                 out_type=out_type,
                 add_bos=self.add_bos,
                 add_eos=self.add_eos,
+                alpha=1.0,
+            )
+        else:
+            # New pybind11 wrapper (sentencepiece >=0.2.0)
+            self._sentence_piece_spm = spm.SentencePieceProcessor(
+                model_proto=proto,
+                out_type=out_type,
+                add_bos=self.add_bos,
+                add_eos=self.add_eos,
+                alpha=1.0,
             )
 
     def set_proto(self, proto):
