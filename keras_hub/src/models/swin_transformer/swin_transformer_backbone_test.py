c745eb579c9f534470ec83406f76e1e3ecdf7450
tacl202 <binhaitcl@163.com>
2026-09-09 07:50:27 +0800

    Remove keras.ops.random patch; use keras.random as intended

--- keras_hub/src/models/swin_transformer/swin_transformer_backbone_test.py
+++ keras_hub/src/models/swin_transformer/swin_transformer_backbone_test.py
@@ -1,10 +1,6 @@
 import keras
 
-# Patch: keras.ops.random not exported in Keras 3.15.1 (keras-team/keras#23575)
-import keras.src.ops as _src_ops
 
-if not hasattr(keras.ops, "random"):
-    keras.ops.random = _src_ops.random
 import numpy as np
 import pytest
 from keras import ops
