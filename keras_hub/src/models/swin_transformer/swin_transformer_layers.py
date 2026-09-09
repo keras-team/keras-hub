91e8e17ea5bc1c32dd8560c054feeeaf0b2a5e12
tacl202 <binhaitcl@163.com>
2026-09-09 11:47:27 +0800

    Remove commented code for random_tensor calculation

--- keras_hub/src/models/swin_transformer/swin_transformer_layers.py
+++ keras_hub/src/models/swin_transformer/swin_transformer_layers.py
@@ -126,7 +126,6 @@ class DropPath(layers.Layer):
         keep_prob = 1.0 - self.drop_prob
 
         batch_size = ops.shape(x)[0]
-        # random_tensor = keep_prob + ops.random.uniform((batch_size, 1, 1, 1))
         mask_shape = (batch_size,) + (1,) * (len(x.shape) - 1)
         random_tensor = keep_prob + keras.random.uniform(
             mask_shape, seed=self.seed_generator
